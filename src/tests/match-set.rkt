c2f259353d9f2a32a4ead25b6b8939d3d0d8d56e
Joe Gibbs Politz <joe@cs.brown.edu>
2012-11-25 14:52:54 -0500

    use the right predicate

--- src/tests/match-set.rkt
+++ src/tests/match-set.rkt
@@ -7,18 +7,18 @@
     (syntax-case stx ()
       [(_ elts ...)
        #`(? (lambda (s)
+              ;; Struct is for handy debugging (keep syntax around)
+              (struct set-matcher (fun stx) #:transparent)
               ;; A list of matchers for each pattern
-              (define matchers (list (match-lambda [elts #t] [else #f]) ...))
+              (define matchers (list (set-matcher (match-lambda [elts #t] [else #f]) 'elts) ...))
               ;; for each element of the set, find all the matchers that match it
               (define (get-matchers set-elt matcher-list)
-                (filter (lambda (f) (f set-elt)) matcher-list))
+                (filter (lambda (f) ((set-matcher-fun f) set-elt)) matcher-list))
               ;; for each of those matchers m, copy the matchers list with m removed
               (define (get-next-matchers to-remove current-list)
-                (map (lambda (m) (remove m current-list)) to-remove))
+                (map (lambda (m) (remove m current-list eq?)) to-remove))
               ;; the-set should be the same size as matchers before calling set-matches
               (define (set-matches the-set matchers)
-                (display (length matchers))
-                (newline)
                 (cond
                   [(empty? the-set) #t]
                   [else
@@ -26,11 +26,11 @@
                    (cond
                      [(empty? matches-this-element) #f]
                      [(cons? matches-this-element)
-                      (empty? (filter false? (map (lambda (next-matchers)
-                                                    (set-matches (rest the-set) 
-                                                                 next-matchers))
-                                                  (get-next-matchers matches-this-element
-                                                                     matchers))))])]))
+                      (cons? (filter (negate false?) (map (lambda (next-matchers)
+                                                            (set-matches (rest the-set) 
+                                                                         next-matchers))
+                                                          (get-next-matchers matches-this-element
+                                                                             matchers))))])]))
               (and (set? s)
                    (equal? (set-count s) #,(length (syntax->datum #'(elts ...))))
                    (set-matches (set->list s) matchers))))])))
@@ -38,6 +38,10 @@
 (check-true
  (match (set 1 2 3)
   [(mset _ _ 3) #t]))
+
+(check-true
+ (match (set 4 5)
+   [(mset 4 _) #t]))
 
 (check-exn
  #rx"match"
@@ -52,7 +56,12 @@
 (check-true (match (set 1 2 (set 3 4) (set (set 2) (set 3)))
               [(mset (mset _ (mset 3)) _ (mset 4 _) _) #t]))
 
+(check-true (match (set 1 2 (set 5 4) (set (set 2) (set 3)))
+              [(mset (mset _ (mset 2)) _ (mset 4 _) _) #t]))
+
 ;; why doesn't this one work?
-(match (set 1 2 (set 3 4) (set (set 2) (set 3)))
-  [(mset (mset _ (mset 3)) _ (mset 3 _) _) #t])
+(check-true (match (set 1 2 (set 5 4) (set (set 2) (set 3)))
+              [(mset (mset _ (mset 3)) _ (mset 5 _) _) #t]))
                               
+(check-true (match (set 1 2 (set 3 4) (set (set 2) (set 3)))
+              [(mset (mset _ (mset 3)) _ (mset 3 _) _) #t]))
