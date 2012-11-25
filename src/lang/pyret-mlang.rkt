a5e37d0bec54978bde5c8ae0610f90ed60e787ab
Joe Gibbs Politz <joe@cs.brown.edu>
2012-11-25 18:11:54 -0500

    make the module language (sort of) work

--- src/lang/pyret-mlang.rkt
+++ src/lang/pyret-mlang.rkt
@@ -3,11 +3,19 @@
 (provide (rename-out [my-read read]
                      [my-read-syntax read-syntax]))
 
-(require "../parser.rkt")
+(require "parser.rkt" "compile-pyret.rkt")
+(require (for-syntax "pyret.rkt"))
 (require syntax/strip-context racket/pretty racket/port)
+
+(dynamic-require "../src/lang/pyret.rkt" 0)
+(define ns (module->namespace "../src/lang/pyret.rkt"))
 
 (define (my-read in)
   (syntax->datum (my-read-syntax #f in)))
 
 (define (my-read-syntax src in)
-  (get-syntax src in))
+  (with-syntax ([stx (compile-pyret (eval (get-syntax src in) ns))])
+    #'(module src racket
+        (require "../src/lang/values.rkt")
+        stx)))
+        
