aba103453bb14ec8f6fa7b996b3fbf85fe5ae9b6
Joe Gibbs Politz <joe@cs.brown.edu>
2012-12-04 15:51:02 -0500

    enforcing some style

--- src/lang/grammar.rkt
+++ src/lang/grammar.rkt
@@ -8,32 +8,43 @@ stmt: def-expr | fun-expr | data-expr | expr
     | assign-expr | dot-assign-expr | bracket-assign-expr
 
 expr: obj-expr | list-expr | app-expr | id-expr | prim-expr
-      | dot-expr | bracket-expr | dot-method-expr | bracket-method-expr
-      | cond-expr | lambda-expr
+    | dot-expr | bracket-expr | dot-method-expr | bracket-method-expr
+    | cond-expr | lambda-expr
 
 id-expr: NAME
 
 assign-expr: NAME "=" expr
 
-prim-expr: num-expr | bool-expr | string-expr
+prim-expr:
+   num-expr
+ | bool-expr
+ | string-expr
 num-expr: NUMBER
 bool-expr: "true" | "false"
 string-expr: STRING
                     
-def-expr: "def" NAME "::" ann ":" expr | "def" NAME ":" expr
+def-expr:
+   "def" NAME "::" ann ":" expr
+ | "def" NAME ":" expr
 
 app-arg-elt: expr ","
-app-args: "(" app-arg-elt* expr ")" | "(" ")"
+app-args:
+   "(" app-arg-elt* expr ")"
+ | "(" ")"
 app-expr: expr app-args
 
 arg-elt: NAME ","
 ann-arg-elt: NAME "::" ann ","
 last-arg-elt: NAME
 ann-last-arg-elt: NAME "::" ann
-args: "(" (arg-elt|ann-arg-elt)* (last-arg-elt|ann-last-arg-elt) ")" | "(" ")"
+args:
+   "(" (arg-elt|ann-arg-elt)* (last-arg-elt|ann-last-arg-elt) ")"
+ | "(" ")"
 fun-expr:
-   "fun" NAME args ":" block "end" |
-   "fun" NAME args "->" ann ":" block "end"
+   "fun" NAME args ":" block "end"
+ | "fun" NAME args "->" ann ":" block "end"
+ | "fun" NAME args ":" "(" block ")"
+ | "fun" NAME args "->" ann ":" "(" block ")"
 
 lambda-args: (arg-elt|ann-arg-elt)* (last-arg-elt|ann-last-arg-elt)
 lambda-expr:
@@ -45,14 +56,18 @@ lambda-expr:
 cond-branch: "|" expr "=>" block
 cond-expr: "cond" ":" cond-branch* "end"
    
-field: NAME ":" expr | NAME args ":" stmt | NAME args ":" stmt "end"
+field:
+   NAME ":" expr
+ | NAME args ":" stmt
+ | NAME args ":" stmt "end"
 list-field: field ","
 
 # list-field is here because it works better with syntax-matching -
 # there's a syntax sub-list for list-field that we can grab hold of
 obj-expr:
-    "{" "extend" expr "with" list-field* field "}" | "{" "extend" expr "}"
-  | "{" list-field* field "}" | "{" "}"
+   "{" "extend" expr "with" list-field* field "}"
+ | "{" "extend" expr "}"
+ | "{" list-field* field "}" | "{" "}"
 
 list-elt: expr ","
 list-expr: "[" list-elt* expr "]" | "[" "]"
@@ -68,12 +83,14 @@ bracket-method-expr: expr ":" "[" expr "]" app-args
 
 data-member: NAME | NAME "::" ann
 data-member-elt: data-member ","
-data-variant: "|" NAME ":" data-member-elt* data-member
-       | "|" NAME
+data-variant:
+   "|" NAME ":" data-member-elt* data-member
+ | "|" NAME
 data-param-elt: NAME ","
 data-params: "(" data-param-elt* NAME ")"
-data-expr: "data" NAME data-variant+ "end"
-         | "data" NAME data-params data-variant+ "end"
+data-expr:
+   "data" NAME data-variant+ "end"
+ | "data" NAME data-params data-variant+ "end"
 
            
 ann: name-ann | record-ann | arrow-ann | app-ann
