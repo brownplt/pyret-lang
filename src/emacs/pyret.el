012651337038f04e0430c6e021a234b759820602
Daniel Patterson <dbp@riseup.net>
2013-01-10 23:17:07 -0500

    update emacs mode with a few more keywords and builtins

--- src/emacs/pyret.el
+++ src/emacs/pyret.el
@@ -4,7 +4,8 @@
   'pyret-mode
   '("#")                                  ;; comments start with #
   '("fun" "def" "cond" "import" "provide"
-    "data" "end" "true" "false" "do" )    ;; keywords
+    "data" "end" "true" "false" "do"
+    "as" "with" "sharing")    ;; keywords
   '(("=" .  'font-lock-operator)
     (":" .  'font-lock-builtin)
     ("::" . 'font-lock-bultin)
@@ -14,7 +15,8 @@
     ("}" .  'font-lock-builtin)
     ("." .  'font-lock-builtin)
     ("\\" . 'font-lock-builtin)
-    (";" .  'font-lock-builtin))
+    (";" .  'font-lock-builtin)
+    ("|" .  'font-lock-builtin))
   '("\\.arr$")                            ;; files for which to activate this mode 
    (list (lambda () (set (make-local-variable 'comment-start) "#"))
 	 (lambda () (set (make-local-variable 'tab-width) 2))
