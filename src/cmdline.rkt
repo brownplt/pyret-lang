6b9647a71cd356ec6885d7443fce5e5c06812fd6
Joe Gibbs Politz <joe@cs.brown.edu>
2013-05-08 20:46:07 -0400

    useful utilities go here; for now just getting the racket of a pyret file

---
+++ src/cmdline.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+
+(require
+  pyret/lang/reader
+  racket/cmdline
+  racket/pretty)
+
+(command-line
+  #:once-each
+  ("--get-racket" pyret-file "Compile pyret-file to Racket and print it on stdout"
+   (pretty-write (syntax->datum (read-syntax 'cmdline (open-input-file pyret-file))))))
+
