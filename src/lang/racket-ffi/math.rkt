3ad4b5102a876bb5f6eac5467be0adff94cbd4b4
Ben Lerner <blerner@cs.brown.edu>
2013-05-29 21:59:43 +0100

    adding binding for random

--- src/lang/racket-ffi/math.rkt
+++ src/lang/racket-ffi/math.rkt
@@ -9,6 +9,7 @@
   (make-immutable-hash
     (list
       (cons "uniform-dist" (wrap-racket-fun uniform-dist))
+      (cons "random" (wrap-racket-fun random))
       (cons "sample" (wrap-racket-fun sample)))))
 
 (define math-obj (p:mk-object math-dict))
