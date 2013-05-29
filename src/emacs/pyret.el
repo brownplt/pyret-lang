42b2ecc120e2f79317f24594bfc5ff6f054b067f
Ben Lerner <blerner@cs.brown.edu>
2013-05-29 11:36:15 -0400

    forgot to include the definition of smart-tab...

--- src/emacs/pyret.el
+++ src/emacs/pyret.el
@@ -1,12 +1,26 @@
 (defvar pyret-mode-hook nil)
-(defconst pyret-mode-map
+(defun pyret-smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+        (indent-for-tab-command))))
+(defvar pyret-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "RET") 'newline-and-indent)
     (define-key map (kbd "|") 
       (function (lambda (&optional N) 
                   (interactive "^p")
                   (or N (setq N 1))
-                  (self-insert-command N) (smart-tab))))
+                  (self-insert-command N)
+                  (pyret-smart-tab))))
     map)
   "Keymap for Pyret major mode")
 
