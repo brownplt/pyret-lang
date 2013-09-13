6b43145f7bd79ed8e4803c6b1a0cc6f2fa9ec0b3
Joe Gibbs Politz <joe@cs.brown.edu>
2013-09-12 20:46:27 -0400

    cps

--- spec/go.rkt
+++ spec/go.rkt
@@ -11,7 +11,7 @@
 
   [v+ø v ø]
 
-  [e (make-chan e) (e e) (go e e) (seq e e) (e >! e e) (<! e e) x v]
+  [e (make-chan) (e e) (go e) (seq e e) (e >! e) (e >! e e) (<! e) (<! e e) x v]
   [(x y z c) (variable-except λ seq go make-chan <! >! qread qwrite qgo)]
 
   [q (qread number (λ (x) e))
@@ -21,9 +21,7 @@
   [Q [q ...]]
   [C [(number v+ø) ...]]
 
-  [E hole (E e) (v E) (go E e) (go v E) (seq E e) (seq v E)
-          (E >! e e) (v >! E e) (v >! v E)
-          (<! E e) (<! v E)]
+  [E hole (E e) (v E) (go1 E) (go E e) (go v E) (seq E e) (seq v E)]
 
   [p {Q C e+⊥}])
 
@@ -33,15 +31,57 @@
   [(subst x any (λ (y) e)) (λ (y) (subst x any e))]
   [(subst x any x) any]
   [(subst x any y) y]
-  [(subst x any (make-chan e)) (make-chan (subst x any e))]
+  [(subst x any (make-chan)) (make-chan)]
   [(subst x any (num number)) (num number)]
   [(subst x any (chan number)) (chan number)]
   [(subst x any (seq e_1 e_2)) (seq (subst x any e_1) (subst x any e_2))]
-  [(subst x any (go e_1 e_2)) (go (subst x any e_1) (subst x any e_2))]
+  [(subst x any (go e)) (go (subst x any e))]
   [(subst x any (e_1 >! e_2 e_3))
    ((subst x any e_1) >! (subst x any e_2) (subst x any e_3))]
   [(subst x any (<! e_1 e_2)) (<! (subst x any e_1) (subst x any e_2))]
   [(subst x any (e_1 e_2)) ((subst x any e_1) (subst x any e_2))])
+
+(define-metafunction πsync
+  cps : e -> e
+  [(cps (seq e_1 e_2))
+   (λ (k)
+    ((cps e_1)
+     (λ (v1)
+      ((cps e_2)
+       (λ (v2)
+        (k v2))))))]
+  [(cps (<! e))
+   (λ (k)
+    ((cps e)
+     (λ (c)
+      (<! c k))))]
+  [(cps (e_1 >! e_2))
+   (λ (k)
+    ((cps e_1)
+     (λ (val)
+      ((cps e_2)
+       (λ (c)
+        (val >! c k))))))]
+  [(cps (e_1 e_2))
+   (λ (k)
+    ((cps e_1)
+     (λ (v1)
+      ((cps e_2)
+       (λ (v2)
+        (k (v1 v2)))))))]
+  [(cps e) (λ (k) (k e))])
+
+(define-metafunction πsync
+  cps-gos : e -> e
+  [(cps-gos (go e)) (go (cps e))]
+  [(cps-gos (seq e_1 e_2))
+   (seq (cps-gos e_1) (cps-gos e_2))]
+  [(cps-gos (λ (x) e))
+   (λ (x) (cps-gos e))]
+  [(cps-gos (e_1 e_2)) ((cps-gos e_1) (cps-gos e_2))]
+  [(cps-gos (<! e)) (err "<! outside go")]
+  [(cps-gos (e >! e)) (err ">! outside go")]
+  [(cps-gos e) e])
 
 (define →πsync
   (reduction-relation
@@ -50,9 +90,10 @@
 
     (==> ((λ (x) e) v) (subst x v e))
     (==> (seq v e) e)
+    (--> {Q C v} {Q C ⊥})
 
-    (--> {Q [(number v+ø) ...] (make-chan (λ (x) e))}
-         {Q [(number_0 ø) (number v+ø) ...] ((λ (x) e) (chan number_0))}
+    (--> {Q [(number v+ø) ...] (in-hole E (make-chan))}
+         {Q [(number_0 ø) (number v+ø) ...] (in-hole E (chan number_0))}
          (where number_0 ,(+ 1 (length (term (number ...))))))
 
     (--> {[q ...] C (v >! (chan number_this) (λ (x) e))}
@@ -66,35 +107,23 @@
          {[q_0 ... q ...]
           [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
           (subst x v e)})
-    #;(--> {[(qread number_this (λ (x) e)) q ...]
-          [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
-          ⊥}
-         {[q ... (qread number_this (λ (x) e))]
-          [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
-          ⊥})
     (--> {[q_0 ... (qwrite number_this v (λ (x) e)) q ...]
           [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
           ⊥}
          {[q_0 ... q ...]
           [(number_1 v+ø_1) ...  (number_this v) (number_n v+ø_n) ...]
           (subst x v e)})
-    #;(--> {[(qwrite number_this v_0 (λ (x) e)) q ...]
-          [(number_1 v+ø_1) ...  (number_this v) (number_n v+ø_n) ...]
-          ⊥}
-         {[q ... (qwrite number_this v_0 (λ (x) e))]
-          [(number_1 v+ø_1) ...  (number_this v) (number_n v+ø_n) ...]
-          ⊥})
     (--> {[q_0 ... (qgo e) q ...] C ⊥}
          {[q_0 ... q ...] C e})
 
-    (--> {[q ...] [(number v+ø) ...] (go e_1 (λ (c) e_2))}
-         {[q ... (qgo (e_1 (λ (z) (z >! (chan number_new) (λ (x_done) ⊥)))))]
+    (--> {[q ...] [(number v+ø) ...] (in-hole E (go e))}
+         {[q ... (qgo (e (λ (z) (z >! (chan number_new) (λ (x_done) ⊥)))))]
           [(number_new ø) (number v+ø) ...]
-          ((λ (c) e_2) (chan number_new))}
+          (in-hole E (chan number_new))}
          (where number_new ,(+ 1 (length (term (number ...))))))
 
    with 
-   [(--> {Q C e_1} {Q C e_2})
+   [(--> {Q C (in-hole E e_1)} {Q C (in-hole E e_2)})
     (==> e_1 e_2)]))
 
 ;(traces →πsync (term ([] [] (make-chan))))
@@ -117,11 +146,71 @@
 
 (define g1-matches (is-e g1))
 (define g2-matches (is-e g2))
-(printf "~a, ~a" g1-matches g2-matches)
-(flush-output)
+
+
+#;(printf "~a, ~a" g1-matches g2-matches)
+#;(flush-output)
 
 (define gotest
   (term ([] []
           (make-chan
             (λ (c) ,g1)))))
-(traces →πsync gotest)
+;(traces →πsync gotest)
+
+(define loop2
+  (term
+    (go
+      (λ (k)
+        ((λ (y)
+            (y y))
+          (λ (y)
+            (<! c (λ (v) (y y))))))
+      (λ (x) x))))
+(define loop1
+  (term (go
+    (λ (k)
+      ((λ (y)
+          (y y))
+        (λ (y)
+          ((num 5) >! c (λ (v) (y y))))))
+    (λ (c2) ,loop2)))) 
+(define looptest
+  (term ([] []
+          (make-chan
+            (λ (c) ,loop1)))))
+;(traces →πsync looptest)
+
+(define justgo
+  (term
+    (go (num 5))))
+;(traces →πsync (term ([] [] (cps-gos ,justgo))))
+
+(define gosendtwice
+  (term
+    ((λ (c)
+      (seq
+        (go
+          (seq
+            ((num 5) >! c)
+            ((num 6) >! c)))
+        (go
+          (seq
+            (<! c)
+            (<! c)))))
+      (make-chan))))
+(traces →πsync (term ([] [] (cps-gos ,gosendtwice))))
+
+
+
+    #;(--> {[(qread number_this (λ (x) e)) q ...]
+          [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
+          ⊥}
+         {[q ... (qread number_this (λ (x) e))]
+          [(number_1 v+ø_1) ...  (number_this ø) (number_n v+ø_n) ...]
+          ⊥})
+    #;(--> {[(qwrite number_this v_0 (λ (x) e)) q ...]
+          [(number_1 v+ø_1) ...  (number_this v) (number_n v+ø_n) ...]
+          ⊥}
+         {[q ... (qwrite number_this v_0 (λ (x) e))]
+          [(number_1 v+ø_1) ...  (number_this v) (number_n v+ø_n) ...]
+          ⊥})
