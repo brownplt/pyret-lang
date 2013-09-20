0ad2ba0d6ede21aebc1fe8c472d152b551886ac0
Joe Gibbs Politz <joe@cs.brown.edu>
2013-09-19 20:31:08 -0400

    slightly larger compiled code, way better stack traces

--- src/lang/compile.rkt
+++ src/lang/compile.rkt
@@ -207,10 +207,10 @@
                     [(arg ...) (map (curryr compile-expr env) args)]
                     [(argid ...) (map (λ (_) (format-id #'obj "~a" #`#,(gensym 'arg))) args)]
                     [field (compile-string-literal l2 field env)])
-          #`(r:let* ([%obj obj]
+          (mark l #`(r:let* ([%obj obj]
                      [%field (p:get-raw-field #,(loc-stx l) %obj field)]
                      [argid arg] ...)
-              ((p:p-base-method %field) %obj argid ...)))]
+              ((p:p-base-method %field) %obj argid ...))))]
 
 
     [(s-app l fun args)
@@ -222,10 +222,11 @@
          (with-syntax ([(arg ...) (args-stx l args)])
            #`(p:arity-catcher (arg ...) #,(compile-expr/internal body env)))]
         [_ #`(p:p-base-app #,(compile-expr fun env))]))
+     (mark l
      (attach l
         (with-syntax ([fun (compile-fun-expr fun)]
                       [(arg ...) (map (curryr compile-expr env) args)])
-          #'(fun arg ...)))]
+          #'(fun arg ...))))]
 
     [(s-obj l fields)
      (attach l
