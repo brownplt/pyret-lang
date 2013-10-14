b39b2328c7880dc5bef5b61a8c5e7495687e7916
Joe Gibbs Politz <joe@cs.brown.edu>
2013-10-14 11:59:41 -0400

    check language

--- src/check/lang/check.rkt
+++ src/check/lang/check.rkt
@@ -10,13 +10,13 @@
   racket/runtime-path
   syntax/strip-context
   (only-in rnrs/io/ports-6 port-eof?)
-  "../parameters.rkt"
-  "../color-tokenizer.rkt"
-  "type-env.rkt"
-  "compile.rkt"
-  "desugar.rkt"
-  "typecheck.rkt"
-  "eval.rkt")
+  "../../parameters.rkt"
+  "../../lang/type-env.rkt"
+  "../../lang/compile.rkt"
+  "../../lang/desugar.rkt"
+  "../../lang/typecheck.rkt"
+  "../../lang/eval.rkt"
+  "../../color-tokenizer.rkt")
 
 (define (get-info in mod line col pos)
   (lambda (key default)
@@ -24,17 +24,10 @@
       [(color-lexer) get-token]
       [else default])))
 
-(define-runtime-module-path pyret-lang-racket "pyret-lang-racket.rkt")
+(define-runtime-module-path pyret-lang-racket "../../lang/pyret-lang-racket.rkt")
 
 (define (my-read in)
   (syntax->datum (my-read-syntax #f in)))
-
-; NOTE(joe): Only the first file gets compiled in check mode, so this helper
-; returns true the first time, and sets the parameter to be false after
-(define (test-check-mode)
-  (define old-value (current-check-mode))
-  (current-check-mode #f)
-  old-value)
 
 (define (bare-read-syntax src in #:check [check #f] #:type-env [env DEFAULT-ENV])
   (cond
@@ -53,6 +46,6 @@
               #`(module src-syntax (file pyret-lang-racket-stx)
                   (r:require (r:only-in racket/base current-read-interaction current-print void))
                   (void (current-read-interaction repl-eval-pyret))
-                  (void (current-print (print-pyret #,(current-check-mode))))
-                  #,(pyret->racket src in #:type-env WHALESONG-ENV #:toplevel #t #:check (test-check-mode)))))]))
+                  (void (current-print (print-pyret #t)))
+                  #,(pyret->racket src in #:type-env WHALESONG-ENV #:toplevel #t #:check #t))))]))
 
