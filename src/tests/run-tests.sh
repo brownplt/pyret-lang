4703a806c7dbcf0e9d6661dd636311abf99d1d8d
Eric Caruso <eric@cs.brown.edu>
2013-10-25 20:37:39 -0400

    modifying to use make test

--- src/tests/run-tests.sh
+++ src/tests/run-tests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# make sure we are in the test directory before running!
-cd $(dirname $(realpath $0))
-# run all tests
-raco make compile-tests.rkt parse-tests.rkt type-tests.rkt well-formed-tests.rkt indentation-tests.rkt \
-  && racket parse-tests.rkt \
-  && racket compile-tests.rkt \
-  && racket type-tests.rkt \
-  && racket well-formed-tests.rkt \
-  && racket indentation-tests.rkt
+raco make compile-tests.rkt parse-tests.rkt && racket parse-tests.rkt \
+  && racket compile-tests.rkt && echo 'OK'
+
