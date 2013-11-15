0a186a40d69ab824d13c8874d100c5c09a4af04c
Ben Lerner <blerner@cs.brown.edu>
2013-11-15 17:50:57 -0500

    Checking provides blocks for unbound identifiers, by collecting the imported identifiers, the file's definitions, and then checking the provides

--- src/lang/typecheck.rkt
+++ src/lang/typecheck.rkt
@@ -407,9 +407,15 @@
       [(s-import l f n)
        (update n (binding l (a-blank) #f) env)]
       [_ env]))
+  (define (check-exports imp env)
+    (match imp
+      [(s-provide l e) (s-provide l (cc-env e env))]
+      [_ imp]))
   (match ast
-    ;; TODO(joe): typechecking provides expressions?
     [(s-prog s imps ast)
      (define imported-env (foldr bind-imports env imps))
-     (s-prog s imps (cc-env ast imported-env))]
+     (define new-body (cc-env ast imported-env))
+     (define body-env (cc-block-env (s-block-stmts new-body) imported-env))
+     (define new-imps (map (λ(i) (check-exports i body-env)) imps))
+     (s-prog s new-imps new-body)]
     [else (cc-env ast env)]))
