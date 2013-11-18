990eda34798938720f77717e9aabcb219d9eef2e
Eric Caruso <eric@cs.brown.edu>
2013-11-17 23:22:14 -0500

    Implementation of binomial heaps

--- src/tests/compile-tests.rkt
+++ src/tests/compile-tests.rkt
@@ -1117,6 +1117,7 @@ o2.m().called" true)
     (private-run (example-path "queue.arr") 9)
     (private-run (example-path "point.arr") 5)
     (private-run (example-path "ralist.arr") 110)
+    (private-run (example-path "heap.arr") 31)
 
     ;; NOTE(dbp): just syntax checking, no tests, for now.
     (private-run (example-path "htdp/arithmetic.arr") 0)
