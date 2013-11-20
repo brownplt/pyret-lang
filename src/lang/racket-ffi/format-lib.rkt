62f5f932b0b79be5208dfa8cd4286038b27ecc05
Joe Politz <joe@cs.brown.edu>
2013-11-20 18:33:25 -0500

    format

--- src/lang/racket-ffi/format-lib.rkt
+++ src/lang/racket-ffi/format-lib.rkt
@@ -2,10 +2,18 @@
 
 (require
  (except-in "../runtime.rkt" raise)
+ racket/format
  "../string-map.rkt"
  "../ffi-helpers.rkt")
 
-(provide (rename-out [pyret-format %PYRET-PROVIDE]))
+(provide (rename-out [export %PYRET-PROVIDE]))
 
 (define pyret-format (ffi-wrap (lambda (template lst)
   (apply format (cons template lst)))))
+(define fix-width (ffi-wrap (lambda (str width)
+  (~a str #:width width))))
+
+(define export (p:mk-object
+  (make-string-map (list
+    (cons "format" pyret-format)
+    (cons "fix-width" fix-width)))))
