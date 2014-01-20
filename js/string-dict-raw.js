ae6d19f401a6c4e41b10ca489ce7c9e6f704e105
Joe Gibbs Politz <joe@cs.brown.edu>
2014-01-19 22:15:01 -0500

    this seemed to be missing its fixes for requirejs

--- js/string-dict-raw.js
+++ js/string-dict-raw.js
@@ -1,194 +1,195 @@
 var jsnums = require('./js-numbers/src/js-numbers.js');
+define(["./js-numbers/src/js-numbers"], function(jsnums) {
 
-function getBaseStringDict(rt) {
-    //Grab the stuff we need from the runtime
-    var makeMethod = rt['makeMethod'];
-    var makeBoolean = rt['makeBoolean'];
-    var makeNothing = rt['makeNothing'];
-    var makeNumber = rt['makeNumber'];
-    var makeString = rt['makeString'];
+  function getBaseStringDict(rt) {
+      //Grab the stuff we need from the runtime
+      var makeMethod = rt['makeMethod'];
+      var makeBoolean = rt['makeBoolean'];
+      var makeNothing = rt['makeNothing'];
+      var makeNumber = rt['makeNumber'];
+      var makeString = rt['makeString'];
 
-    var checkIf = rt['checkIf'];
-    var makeNumberBig = rt['makeNumberBig'];
-    var isNumber = rt['isNumber'];
-    var isString = rt['isString'];
-    var makeMessageException = rt['makeMessageException'];
+      var checkIf = rt['checkIf'];
+      var makeNumberBig = rt['makeNumberBig'];
+      var isNumber = rt['isNumber'];
+      var isString = rt['isString'];
+      var makeMessageException = rt['makeMessageException'];
 
-    //Make the dictionary
-    return {
-        /**@type {PMethod}*/
-        '_plus' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PString}
-        */
-        method(left, right) {
-            checkIf(left, isString);
-            checkIf(right, isString);
+      //Make the dictionary
+      return {
+          /**@type {PMethod}*/
+          '_plus' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PString}
+          */
+          method(left, right) {
+              checkIf(left, isString);
+              checkIf(right, isString);
 
-            return makeString(left.s.concat(right.s));
-        },
+              return makeString(left.s.concat(right.s));
+          },
 
-        /**@type {PMethod}*/
-        'append' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PString}
-        */
-        method(left, right) {
-            checkIf(left, isString);
-            checkIf(right, isString);
+          /**@type {PMethod}*/
+          'append' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PString}
+          */
+          method(left, right) {
+              checkIf(left, isString);
+              checkIf(right, isString);
 
-            return makeString(left.s.concat(right.s));
-        },
+              return makeString(left.s.concat(right.s));
+          },
 
-        /**@type {PMethod}*/
-        'contains' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PBoolean}
-        */
-        method(left, right) {
-            checkIf(left, isString);
-            checkIf(right, isString);
+          /**@type {PMethod}*/
+          'contains' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PBoolean}
+          */
+          method(left, right) {
+              checkIf(left, isString);
+              checkIf(right, isString);
 
-            return makeBoolean(left.s.indexOf(right.s) !== -1);
-        },
+              return makeBoolean(left.s.indexOf(right.s) !== -1);
+          },
 
-        /**@type {PMethod}*/
-        'char-at' : 
-        /**
-          @param {!PString} str
-          @param {!PNumber} n
-          @return {!PString}
-        */
-        method(str, n) {
-            checkIf(str, isString);
-            checkIf(n, isNumber);
-        
-            //TODO: Handle bignums that are beyond javascript
-            return makeString(String(str.s.charAt(n.n.toFixnum())));
-        },
+          /**@type {PMethod}*/
+          'char-at' : 
+          /**
+            @param {!PString} str
+            @param {!PNumber} n
+            @return {!PString}
+          */
+          method(str, n) {
+              checkIf(str, isString);
+              checkIf(n, isNumber);
+          
+              //TODO: Handle bignums that are beyond javascript
+              return makeString(String(str.s.charAt(n.n.toFixnum())));
+          },
 
 
-        /**@type {PMethod}*/
-        'tostring' : 
-        /**
-          @param {!PString} me
-          @return {!PString}
-        */
-        method(me) {
-            checkIf(me, isString);
-            return makeString(me.s);
-        },
+          /**@type {PMethod}*/
+          'tostring' : 
+          /**
+            @param {!PString} me
+            @return {!PString}
+          */
+          method(me) {
+              checkIf(me, isString);
+              return makeString(me.s);
+          },
 
-        /**@type {PMethod}*/
-        'to-upper' : 
-        /**
-          @param {!PString} me
-          @return {!PString}
-        */
-        method(me) {
-            checkIf(me, isString);
-            return makeString(me.s.toUpperCase());
-        },
+          /**@type {PMethod}*/
+          'to-upper' : 
+          /**
+            @param {!PString} me
+            @return {!PString}
+          */
+          method(me) {
+              checkIf(me, isString);
+              return makeString(me.s.toUpperCase());
+          },
 
-        /**@type {PMethod}*/
-        'to-lower' : 
-        /**
-          @param {!PString} me
-          @return {!PString}
-        */
-        method(me) {
-            checkIf(me, isString);
-            return makeString(me.s.toLowerCase());
-        },
+          /**@type {PMethod}*/
+          'to-lower' : 
+          /**
+            @param {!PString} me
+            @return {!PString}
+          */
+          method(me) {
+              checkIf(me, isString);
+              return makeString(me.s.toLowerCase());
+          },
 
-        /**@type {PMethod}*/
-        'tonumber' : 
-        /**
-          @param {!PString} me
-          @return {!PNumber | !PNothing}
-        */
-        method(me) {
-            checkIf(me, isString);
-            var num = jsnums.fromString(me.s);
-            if(num !== false) {
-                return makeNumberBig(/**@type {Bignum}*/ (num));
-            }
-            else {
-                return makeNothing();
-            }
-        },
+          /**@type {PMethod}*/
+          'tonumber' : 
+          /**
+            @param {!PString} me
+            @return {!PNumber | !PNothing}
+          */
+          method(me) {
+              checkIf(me, isString);
+              var num = jsnums.fromString(me.s);
+              if(num !== false) {
+                  return makeNumberBig(/**@type {Bignum}*/ (num));
+              }
+              else {
+                  return makeNothing();
+              }
+          },
 
-        /**@type {PMethod}*/
-        'length' : 
-        /**
-          @param {!PString} me
-          @return {!PNumber}
-        */
-        method(me) {
-            checkIf(me, isString);
-            return makeNumber(me.s.length);
-        }, 
+          /**@type {PMethod}*/
+          'length' : 
+          /**
+            @param {!PString} me
+            @return {!PNumber}
+          */
+          method(me) {
+              checkIf(me, isString);
+              return makeNumber(me.s.length);
+          }, 
 
-        /**@type {PMethod}*/
-        '_lessthan' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PBoolean}
-        */
-        method(left, right) {
-            checkIf(left, isNumber);
-            checkIf(right, isNumber);
+          /**@type {PMethod}*/
+          '_lessthan' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PBoolean}
+          */
+          method(left, right) {
+              checkIf(left, isNumber);
+              checkIf(right, isNumber);
 
-            return makeBoolean(left.s < right.s);
-        },
+              return makeBoolean(left.s < right.s);
+          },
 
-        /**@type {PMethod}*/
-        '_greaterthan' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PBoolean}
-        */
-        method(left, right) {
-            checkIf(left, isNumber);
-            checkIf(right, isNumber);
+          /**@type {PMethod}*/
+          '_greaterthan' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PBoolean}
+          */
+          method(left, right) {
+              checkIf(left, isNumber);
+              checkIf(right, isNumber);
 
-            return makeBoolean(left.s > right.s);
-        },
-        /**@type {PMethod}*/
-        '_lessequal' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PBoolean}
-        */
-        method(left, right) {
-            checkIf(left, isNumber);
-            checkIf(right, isNumber);
+              return makeBoolean(left.s > right.s);
+          },
+          /**@type {PMethod}*/
+          '_lessequal' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PBoolean}
+          */
+          method(left, right) {
+              checkIf(left, isNumber);
+              checkIf(right, isNumber);
 
-            return makeBoolean(left.s <= right.s);
-        },
-        /**@type {PMethod}*/
-        '_greaterequal' : 
-        /**
-          @param {!PString} left
-          @param {!PString} right
-          @return {!PBoolean}
-        */
-        method(left, right) {
-            checkIf(left, isNumber);
-            checkIf(right, isNumber);
+              return makeBoolean(left.s <= right.s);
+          },
+          /**@type {PMethod}*/
+          '_greaterequal' : 
+          /**
+            @param {!PString} left
+            @param {!PString} right
+            @return {!PBoolean}
+          */
+          method(left, right) {
+              checkIf(left, isNumber);
+              checkIf(right, isNumber);
 
-            return makeBoolean(left.s >= right.s);
-        }
-    };
-}
-
-exports['getBaseStringDict'] = getBaseStringDict;
+              return makeBoolean(left.s >= right.s);
+          }
+      };
+  }
+  return {getBaseStringDict: getBaseStringDict};
+});
