295f03ed2f84ce2dd2b6dd9f18a29b71b2e0136a
Joe Gibbs Politz <joe@cs.brown.edu>
2014-01-27 10:12:43 -0500

    web-compile should be compiled

--- js/compile-compiler.sh
+++ js/compile-compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-to_compile=("compile.arr" "pyret.arr" "js-of-pyret.arr" "anf.arr" "ast-anf.arr" "anf-simple-compile.arr" "compile-structs.arr" "ast-split.arr" "desugar.arr" "js-ast.arr")
+to_compile=("compile.arr" "web-compile.arr" "pyret.arr" "js-of-pyret.arr" "anf.arr" "ast-anf.arr" "anf-simple-compile.arr" "compile-structs.arr" "ast-split.arr" "desugar.arr" "js-ast.arr")
 
 for i in ${to_compile[@]}; do
   echo "Compiling $i"
