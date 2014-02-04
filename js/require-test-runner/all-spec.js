ad3003b41ba057fef9a556fce2cecde5c000d00b
Joe Gibbs Politz <joe@cs.brown.edu>
2014-02-04 10:05:46 -0500

    tests for namespaces

--- js/require-test-runner/all-spec.js
+++ js/require-test-runner/all-spec.js
@@ -1,7 +1,7 @@
 var r = require("requirejs");
 
-r(["../test/runtime-test", "../test/runtime-numbers", "../test/runtime-strings", "../test/runtime-booleans", "../test/runtime-brands"],
-       function (base, numbers, strings, booleans, brands) {
+r(["../test/runtime-test", "../test/runtime-numbers", "../test/runtime-strings", "../test/runtime-booleans", "../test/runtime-brands", "../test/test-namespaces"],
+       function (base, numbers, strings, booleans, brands, namespaces) {
 	   var USE_COMPILED = false;
 
 	   base.performTest(USE_COMPILED);
@@ -9,4 +9,5 @@ r(["../test/runtime-test", "../test/runtime-numbers", "../test/runtime-strings",
 	   strings.performTest(USE_COMPILED);
 	   booleans.performTest(USE_COMPILED);
 	   brands.performTest(USE_COMPILED);
+	   namespaces.performTest(USE_COMPILED);
        });
