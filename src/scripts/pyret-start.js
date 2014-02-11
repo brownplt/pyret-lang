10a8404c5e9336ea41e48d768faf3070724d4322
Ben Lerner <blerner@cs.brown.edu>
2014-02-11 00:11:16 -0500

    making Pyret stack print a bit better

--- src/scripts/pyret-start.js
+++ src/scripts/pyret-start.js
@@ -1,16 +1,16 @@
 define(["js/runtime-anf", "arr/compiler/pyret.arr"], function(RT, pyret) {
   var rt = RT.makeRuntime({
-      initialGas: 500,
-      stdout: function(str) { process.stdout.write(str); }
-    });
+    initialGas: 500,
+    stdout: function(str) { process.stdout.write(str); }
+  });
   rt.setParam("command-line-arguments", process.argv.slice(1));
   rt.run(pyret, rt.namespace, function(result) {
-     if(rt.isSuccessResult(result)) {
-        process.exit(0);
-     } else if (rt.isFailureResult(result)) {
-        console.error('Pyret terminated with an error' + String(result) + "\nStack:\n" 
-                      + String(result.exn.stack) + "\nPyret stack:\n" + String(result.exn.pyretStack));
-        process.exit(1);
-     }
+    if(rt.isSuccessResult(result)) {
+      process.exit(0);
+    } else if (rt.isFailureResult(result)) {
+      console.error('Pyret terminated with an error' + String(result) + "\nStack:\n" 
+                    + String(result.exn.stack) + "\nPyret stack:\n" + JSON.stringify(result.exn.pyretStack, null, "  "));
+      process.exit(1);
+    }
   });
 });
