878e40f051863c84e689e4f63a63cf1f3e18485e
Ben Lerner <blerner@cs.brown.edu>
2014-02-11 00:51:13 -0500

    printing error message only

--- src/scripts/pyret-start.js
+++ src/scripts/pyret-start.js
@@ -8,7 +8,7 @@ define(["js/runtime-anf", "arr/compiler/pyret.arr"], function(RT, pyret) {
     if(rt.isSuccessResult(result)) {
       process.exit(0);
     } else if (rt.isFailureResult(result)) {
-      console.error('Pyret terminated with an error' + String(result) + "\nStack:\n" 
+      console.error('Pyret terminated with an error:\n' + result.exn.exn.s + "\nStack:\n" 
                     + String(result.exn.stack) + "\nPyret stack:\n" + JSON.stringify(result.exn.pyretStack, null, "  "));
       process.exit(1);
     }
