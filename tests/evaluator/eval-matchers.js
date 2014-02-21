44c6e22a707dcba110bcabf076734bdf597feaa7
Joe Gibbs Politz <joe@cs.brown.edu>
2014-02-21 07:54:19 -0500

    worklist rather than statelist to avoid racy test behavior

--- tests/evaluator/eval-matchers.js
+++ tests/evaluator/eval-matchers.js
@@ -1,51 +1,66 @@
 define(["./eval", "../runtime/matchers", "js/ffi-helpers"], function(e, matchers, ffiLib) {
-  console.log("eval-matchers body");
   var count = 0;
   function makeEvalCheckers(jasmine, runtime) {
     matchers.addPyretMatchers(jasmine);
     function gf(o, s) { return runtime.getField(o, s); }
-    var finished = [];
+    var tests = [];
     var ffi = ffiLib(runtime, runtime.namespace);
 
+    function pushTest(test) {
+      tests.push(test);
+    }
+    function wait(done) {
+      if (tests.length === 0) {
+        done();
+      }
+      else if (tests.length > 0) {
+        tests.pop()(function() { wait(done); });
+      }
+    }
+
     function checkEvalsTo(str, answer) {
-      var i = finished.push(false);
-      e.evalPyret(runtime, str, {}, function(result) {
-        expect(result).toBeSuccess(runtime);
-        var actual = gf(result.result, "answer");
-        expect(actual).toBeSameAs(runtime, answer);
-        finished[i - 1] = true;
+      pushTest(function(after) {
+        e.evalPyret(runtime, str, {}, function(result) {
+          expect(result).toBeSuccess(runtime);
+          var actual = gf(result.result, "answer");
+          expect(actual).toBeSameAs(runtime, answer);
+          after();
+        });
       });
     }
     function checkEvalTests(str, pred) {
-      var i = finished.push(false);
-      e.evalPyret(runtime, str, {}, function(result) {
-        expect(result).toBeSuccess(runtime);
-        console.log("Result in checks: ", result);
-        var checks = gf(result.result, "checks");
-        expect(checks).toPassPredicate(pred);
-        finished[i - 1] = true;
+      pushTest(function(after) {
+        e.evalPyret(runtime, str, {}, function(result) {
+          expect(result).toBeSuccess(runtime);
+          console.log("Result in checks: ", result);
+          var checks = gf(result.result, "checks");
+          expect(checks).toPassPredicate(pred);
+          after();
+        });
       });
     }
     function checkEvalPred(str, pred) {
-      var i = finished.push(false);
-      e.evalPyret(runtime, str, {}, function(result) {
-        expect(result).toBeSuccess(runtime);
-        if (runtime.isSuccessResult(result)) {
-          var actual = gf(result.result, "answer");
-          expect(actual).toPassPredicate(pred);
-        }
-        finished[i - 1] = true;
+      pushTest(function(after) {
+        e.evalPyret(runtime, str, {}, function(result) {
+          expect(result).toBeSuccess(runtime);
+          if (runtime.isSuccessResult(result)) {
+            var actual = gf(result.result, "answer");
+            expect(actual).toPassPredicate(pred);
+          }
+          after();
+        });
       });
     }
     function checkError(str, exnPred) {
-      var i = finished.push(false);
-      e.evalPyret(runtime, str, {}, function(result) {
-        expect(result).toBeFailure(runtime);
-        if (runtime.isFailureResult(result)) {
-          var exn = result.exn;
-          expect(exn).toPassPredicate(exnPred);
-        }
-        finished[i - 1] = true;
+      pushTest(function(after) {
+        e.evalPyret(runtime, str, {}, function(result) {
+          expect(result).toBeFailure(runtime);
+          if (runtime.isFailureResult(result)) {
+            var exn = result.exn;
+            expect(exn).toPassPredicate(exnPred);
+          }
+          after();
+        });
       });
     }
     function checkCompileErrorMsg(str, exnMsg) {
@@ -62,29 +77,19 @@ define(["./eval", "../runtime/matchers", "js/ffi-helpers"], function(e, matchers
       return checkCompileError(str, findInArray);
     }
     function checkCompileError(str, exnPred) {
-      var i = finished.push(false);
-      e.compilePyret(runtime, str, {}, function(result) {
-        expect(result).toBeFailure(runtime);
-        var problems = result.exn;
-        // Compiling returns a string or an array of 
-        // Pyret objects detailing problems
-        expect(problems).toBeInstanceOf(Array);
-        if (problems instanceof Array) {
-          expect(problems).toPassPredicate(exnPred);
-        }
-        finished[i - 1] = true;
+      pushTest(function(after) {
+        e.compilePyret(runtime, str, {}, function(result) {
+          expect(result).toBeFailure(runtime);
+          var problems = result.exn;
+          // Compiling returns a string or an array of 
+          // Pyret objects detailing problems
+          expect(problems).toBeInstanceOf(Array);
+          if (problems instanceof Array) {
+            expect(problems).toPassPredicate(exnPred);
+          }
+          after();
+        });
       });
-    }
-    function wait(done) {
-      setTimeout(function() {
-          if(finished.filter(function(p) { return p === false; }).length === 0) {
-            done();
-          }
-          else {
-            setTimeout(function() { wait(done); });
-          }
-        },
-        100);
     }
     return {
       checkCompileError: checkCompileError,
