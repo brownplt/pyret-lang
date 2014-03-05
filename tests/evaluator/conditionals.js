70bd234126be0937b6092f93a6b93ea418362609
joseph <joe@cs.brown.edu>
2014-03-05 17:42:34 -0500

    short-circuiting tests

--- tests/evaluator/conditionals.js
+++ tests/evaluator/conditionals.js
@@ -54,37 +54,93 @@ xfg("false", "true") +
 
     });
 
-    describe("Booleans", function() {
-      it("Correct and", function(done) {
-        same("true and true", rt.pyretTrue);
-        same("true and false", rt.pyretFalse);
-        same("false and true", rt.pyretFalse);
+    describe("and", function() {
+      it("should have the right binary truth table", function(done) {
         same("false and false", rt.pyretFalse);
+        same("false and true", rt.pyretFalse);
+        same("true and false", rt.pyretFalse);
+        same("true and true", rt.pyretTrue);
         P.wait(done);
       });
-      it("Correct or", function(done) {
-        same("true or true", rt.pyretTrue);
-        same("true or false", rt.pyretTrue);
-        same("false or true", rt.pyretTrue);
+
+      it("should work for long sequences", function(done) {
+        same("false and false and false and false and false and false and false and false", rt.pyretFalse);
+        same("false and false and false and false and false and false and false and true", rt.pyretFalse);
+        same("false and false and false and true and false and false and false and false", rt.pyretFalse);
+        same("true and true and true and true and true and true and true and true", rt.pyretTrue);
+        same("true and true and true and true and true and true and true and false", rt.pyretFalse);
+        P.wait(done);
+      });
+
+      it("should short-circuit at the right place in a sequence", function(done) {
+        var xf = "var x = 0\n" +
+                 "fun f(): x := x + 1 true end\n";
+
+        same("true and true and false and raise('explosions')", rt.pyretFalse);
+        same(xf +
+             "f() and f() and false and f()\n" +
+             "x", rt.makeNumber(2));
+        same(xf +
+             "false and f() and f() and f()\n" +
+             "x", rt.makeNumber(0));
+        P.wait(done);
+
+      });
+
+      it("should error if given a non-boolean, but still short-circuit", function(done) {
+        var pte = function(e) {
+          return rt.unwrap(e.exn).indexOf("Pyret Type Error") !== -1;
+        };
+        err("true and 5", pte);
+        err("true and 'foo'", pte);
+        same("false and 'foo'", rt.pyretFalse);
+        same("false and block: raise('do not get here') end", rt.pyretFalse);
+        P.wait(done);
+      });
+    });
+
+
+    describe("or", function() {
+      it("should have the right binary truth table", function(done) {
         same("false or false", rt.pyretFalse);
+        same("false or true", rt.pyretTrue);
+        same("true or false", rt.pyretTrue);
+        same("true or true", rt.pyretTrue);
         P.wait(done);
       });
+
+      it("should work for long sequences", function(done) {
+        same("false or false or false or false or false or false or false or false", rt.pyretFalse);
+        same("false or false or false or false or false or false or false or true", rt.pyretTrue);
+        same("false or false or false or true or false or false or false or false", rt.pyretTrue);
+        same("true or true or true or true or true or true or true or true", rt.pyretTrue);
+        same("true or true or true or true or true or true or true or false", rt.pyretTrue);
+        P.wait(done);
+      });
+
+      it("should short-circuit at the right place in a sequence", function(done) {
+        var xf = "var x = 0\n" +
+                 "fun f(): x := x + 1 false end\n";
+
+        same("false or false or true or raise('explosions')", rt.pyretTrue);
+        same(xf +
+             "f() or f() or true or f()\n" +
+             "x", rt.makeNumber(2));
+        same(xf +
+             "true or f() or f() or f()\n" +
+             "x", rt.makeNumber(0));
+        P.wait(done);
+
+      });
+
       it("should error if given a non-boolean, but still short-circuit", function(done) {
         var pte = function(e) {
           return rt.unwrap(e.exn).indexOf("Pyret Type Error") !== -1;
         };
         err("false or 5", pte);
-        err("5 or true", pte);
         err("false or 'foo'", pte);
         same("true or 'foo'", rt.pyretTrue);
         same("true or block: raise('do not get here') end", rt.pyretTrue)
-
-        err("true and 5", pte);
-        err("5 and true", pte);
-        err("true and 'foo'", pte);
-        same("false and 'foo'", rt.pyretFalse);
-        same("false and block: raise('do not get here') end", rt.pyretFalse)
-
         P.wait(done);
       });
     });
