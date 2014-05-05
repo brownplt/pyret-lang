9c1aa2e2d505c9f9d0630eeb390fb065e0fcfcf5
Ben Lerner <blerner@cs.brown.edu>
2014-05-05 17:09:20 -0400

    whups, merge-conflict of some sort

--- src/js/trove/world.js
+++ src/js/trove/world.js
@@ -360,8 +360,6 @@ define(["js/runtime-util", "trove/image-lib", "trove/world-lib", "js/ffi-helpers
 
     var p = function(f) { return function(v) { return runtime.checkIf(v, f); }; };
     var checkHandler = p(isWorldConfigOption);
-    var checkNum = p(runtime.isNumber);
-    var checkString = p(runtime.isString);
     var checkNum = runtime.checkNumber;
     var checkString = runtime.checkString;
 
