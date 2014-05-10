b7fbca4b95ff7ab38e9feb912f4e076ed2240aeb
Ben Lerner <blerner@cs.brown.edu>
2014-05-09 23:27:58 -0400

    tersifying the Pyret stack

--- src/scripts/pyret-start.js
+++ src/scripts/pyret-start.js
@@ -5,6 +5,16 @@ define(["requirejs", "js/runtime-anf", "compiler/pyret.arr"], function(r, RT, py
     stderr: function(str) { process.stderr.write(str); }
   });
   rt.setParam("command-line-arguments", process.argv.slice(1));
+  function printStack(stack) {
+    stackStr = stack.map(function(val) {
+      if (val instanceof Array && val.length == 7) {
+        return (val[0] + ": line " + val[1] + ", column " + val[2]);
+      } else {
+        return JSON.stringify(val);
+      }
+    });
+    return "  " + stackStr.join("\n  ");
+  }
   rt.run(pyret, rt.namespace, {sync: true}, function(result) {
      if(rt.isSuccessResult(result)) {
         process.exit(0);
@@ -12,7 +22,7 @@ define(["requirejs", "js/runtime-anf", "compiler/pyret.arr"], function(r, RT, py
         var exnStack = result.exn.stack; result.exn.stack = undefined;
         var pyretStack = result.exn.pyretStack; result.exn.pyretStack = undefined;
         console.error('Pyret terminated with an error:\n' + String(result).substring(0, 500) + "\nStack:\n" 
-                      + String(exnStack) + "\nPyret stack:\n" + JSON.stringify(pyretStack, null, "  "));
+                      + String(exnStack) + "\nPyret stack:\n" + printStack(pyretStack));
         process.exit(1);
      }
   });
