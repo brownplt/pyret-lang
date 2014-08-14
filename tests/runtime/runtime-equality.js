b18f27aace05cbbac6535ec05adafd33fab46750
Stevie Strickland <sstrickl@racket-lang.org>
2014-08-14 15:41:02 -0400

    Adding more tests for the runtime.

--- tests/runtime/runtime-equality.js
+++ tests/runtime/runtime-equality.js
@@ -27,6 +27,7 @@ define(["js/runtime-anf", "js/ffi-helpers", "./matchers"], function(rtLib, ffiLi
           var zero = rt.makeNumber(0);
           var f = rt.makeFunction(function() { return true; });
           var t = rt.makeBoolean(true);
+          var a = rt.makeString("a");
 
           expect(rt.identical(zero, f)).toBe(false);
           expect(rt.equal_always(zero, f)).toBe(false);
@@ -34,6 +35,9 @@ define(["js/runtime-anf", "js/ffi-helpers", "./matchers"], function(rtLib, ffiLi
           expect(rt.identical(zero, t)).toBe(false);
           expect(rt.equal_always(zero, t)).toBe(false);
           expect(rt.equal_now(zero, t)).toBe(false);
+          expect(rt.identical(zero, a)).toBe(false);
+          expect(rt.equal_always(zero, a)).toBe(false);
+          expect(rt.equal_now(zero, a)).toBe(false);
           expect(rt.identical(f, t)).toBe(false);
           expect(rt.equal_always(f, t)).toBe(false);
           expect(rt.equal_now(f, t)).toBe(false);
@@ -48,6 +52,34 @@ define(["js/runtime-anf", "js/ffi-helpers", "./matchers"], function(rtLib, ffiLi
           expect(rt.equal_always(zero, one)).toBe(false);
           expect(rt.equal_now(zero, zero)).toBe(true);
           expect(rt.equal_now(zero, one)).toBe(false);
+        });
+
+        it("should distinguish strings", function() {
+          var a = rt.makeString("a");
+          var b = rt.makeString("b");
+          var c = rt.makeString("a");
+          expect(rt.identical(a, a)).toBe(true);
+          expect(rt.identical(a, b)).toBe(false);
+          expect(rt.identical(a, c)).toBe(true);
+          expect(rt.equal_always(a, a)).toBe(true);
+          expect(rt.equal_always(a, b)).toBe(false);
+          expect(rt.equal_always(a, c)).toBe(true);
+          expect(rt.equal_now(a, a)).toBe(true);
+          expect(rt.equal_now(a, b)).toBe(false);
+          expect(rt.equal_now(a, c)).toBe(true);
+        });
+
+        it("should distinguish refs", function() {
+          var a = rt.makeRef(rt.Number);
+          var b = rt.makeRef(rt.Number);
+          rt.setRef(a, rt.makeNumber(3));
+          rt.setRef(b, rt.makeNumber(3));
+          expect(rt.identical(a, a)).toBe(true);
+          expect(rt.identical(a, b)).toBe(false);
+          expect(rt.equal_always(a, a)).toBe(true);
+          expect(rt.equal_always(a, b)).toBe(false);
+          expect(rt.equal_now(a, a)).toBe(true);
+          expect(rt.equal_now(a, b)).toBe(true);
         });
 
         it("should throw on functions", function() {
