51dc108823ee4b31ca5dc038fcefb4cc5298887a
Ben Lerner <blerner@cs.brown.edu>
2014-09-13 09:11:53 -0400

    more hardening of the parser against unexpected tokens

--- src/scripts/just-lex.js
+++ src/scripts/just-lex.js
@@ -4,7 +4,6 @@ R(["../../build/phase1/js/pyret-tokenizer", "../../build/phase1/js/pyret-parser"
   const data = fs.readFileSync(process.argv[2], {encoding: "utf-8"});
   const toks = T.Tokenizer;
   toks.tokenizeFrom(data);
-  toks.tokenizeFrom("");
   while (toks.hasNext()) {
     var tok = toks.next();
     console.log(tok.toString(true), tok.pos.toString(true));
