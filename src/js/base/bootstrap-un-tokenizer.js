8262647bd4664ed8f99b9d8c1797df2a08fc167a
Dorai Sitaram <ds26gte@yahoo.com>
2015-01-10 23:55:36 -0500

    allow an optional exponent in exact number literals

--- src/js/base/bootstrap-un-tokenizer.js
+++ src/js/base/bootstrap-un-tokenizer.js
@@ -78,7 +78,7 @@ define(["../../../lib/jglr/jglr"], function(E) {
                                     "<", ">", "is", "raises"]);
   const name = new RegExp("^[_a-zA-Z][-_a-zA-Z0-9]*", STICKY_REGEXP);
   const roughnum = new RegExp("^~[-+]?[0-9]+(?:\\.[0-9]+)?(?:[eE][-+]?[0-9]+)?", STICKY_REGEXP);
-  const number = new RegExp("^[-+]?[0-9]+(?:\\.[0-9]+)?", STICKY_REGEXP);
+  const number = new RegExp("^[-+]?[0-9]+(?:\\.[0-9]+)?(?:[eE][-+]?[0-9]+)?", STICKY_REGEXP);
   const rational = new RegExp("^[-+]?[0-9]+/[0-9]+", STICKY_REGEXP);
   const parenparen = new RegExp("^\\((?=\\()", STICKY_REGEXP); // NOTE: Don't include the following paren
   const opparen = new RegExp("^" + operator_regex_str + "(?=\\()", STICKY_REGEXP); // NOTE: likewise
