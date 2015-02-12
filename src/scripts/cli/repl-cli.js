4f67cbd0bff4887f8626f1a17c326d703046a688
Joe Politz <jpolitz@cs.swarthmore.edu>
2015-02-11 19:45:11 -0500

    move stuff to a subdirectory

--- src/scripts/cli/repl-cli.js
+++ src/scripts/cli/repl-cli.js
@@ -5,9 +5,9 @@ var r = require("requirejs");
 
 r.config({
   paths: {
-    trove: "../../build/phase1/trove",
-    js: "../../build/phase1/js",
-    compiler: "../../build/phase1/arr/compiler"
+    trove: "../../../build/phase1/trove",
+    js: "../../../build/phase1/js",
+    compiler: "../../../build/phase1/arr/compiler"
   }
 });
 
