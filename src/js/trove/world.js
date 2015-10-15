191359dba7043793a8274c6c39490c02e16e129d
Stevie Strickland <sstrickl@racket-lang.org>
2015-10-15 11:05:30 -0400

    Initial move of Particle-related things into their own file.

--- src/js/trove/world.js
+++ src/js/trove/world.js
@@ -452,81 +452,6 @@ define(["js/runtime-util", "trove/image-lib", "trove/world-lib", "js/ffi-helpers
         var checkHandler = runtime.makeCheckType(isOpaqueWorldConfigOption, "WorldConfigOption");
         //////////////////////////////////////////////////////////////////////
 
-        var sd_to_js = function(sd) {
-            var ret = {};
-            var arr = ffi.toArray(runtime.getField(sd, "keys-list").app());
-            for(var i in arr) {
-                var k = arr[i];
-                var v = runtime.getField(sd, "get-value").app(k);
-                if(typeof(v) === "string") {
-                    ret[k] = v;
-                } else {
-                    throw new Error('unimplemented value conversion for StringDict');
-                }
-            }
-            return ret;
-        };
-
-        var OnParticle = function(handler, name, sdict) {
-            WorldConfigOption.call(this, 'on-particle');
-            this.handler = handler;
-            this.event = name;
-            this.options = sd_to_js(sdict);
-        };
-
-        OnParticle.prototype = Object.create(WorldConfigOption.prototype);
-
-        OnParticle.prototype.toRawHandler = function(toplevelNode) {
-            var that = this;
-            var worldFunction = adaptWorldFunction(that.handler);
-            return rawJsworld.on_particle(worldFunction, that.event, that.options);
-        };
-
-
-        //////////////////////////////////////////////////////////////////////
-
-        var ToParticle = function(handler, ename, sdict) {
-            WorldConfigOption.call(this, 'to-particle');
-            this.handler = handler;
-            this.event = ename;
-            this.options = sd_to_js(sdict);
-        };
-
-        ToParticle.prototype = Object.create(WorldConfigOption.prototype);
-
-        ToParticle.prototype.toRawHandler = function(toplevelNode) {
-            var that = this;
-            var worldFunction = adaptWorldFunction(that.handler);
-            var eventGen = function(w, k) {
-                worldFunction(w, function(v) {
-                    if(ffi.isSome(v)) {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("POST","https://api.particle.io/v1/devices/events");
-                        xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-                        xhr.send("access_token=" + that.options.acc + "&name=_event&data=" + that.event + ":" + runtime.getField(v, "value") + "&private=true&ttl=60");
-                    }
-                    k(w);
-                });
-            };
-            return rawJsworld.on_world_change(eventGen);
-        };
-
-
-        //////////////////////////////////////////////////////////////////////
-
-        var configCore = function(coreid, acc, configs) {
-          var config_str = configs.map(function(c){
-              return runtime.getField(c, "_shim-convert").app();}).join("");
-          var xhr = new XMLHttpRequest();
-          xhr.open("POST","https://api.particle.io/v1/devices/events");
-          xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-          xhr.send("access_token=" + acc + "&name=_config&data=" + config_str + "&private=true&ttl=60");
-        }
-
-        var pStruct_vals = runtime.getField(pStruct, "values");
-
-        //////////////////////////////////////////////////////////////////////
-
 
         // The default tick delay is 28 times a second.
         var DEFAULT_TICK_DELAY = 1/28;
@@ -535,107 +460,67 @@ define(["js/runtime-util", "trove/image-lib", "trove/world-lib", "js/ffi-helpers
         var makeFunction = runtime.makeFunction;
 
         return makeObject({
-          "provide": makeObject({
-            "big-bang": makeFunction(function(init, handlers) {
-              ffi.checkArity(2, arguments, "big-bang");
-              runtime.checkList(handlers);
-              var arr = ffi.toArray(handlers);
-              var initialWorldValue = init;
-              arr.map(function(h) { checkHandler(h); });
-              bigBang(initialWorldValue, arr);
-              ffi.throwMessageException("Internal error in bigBang: stack not properly paused and stored.");
+          "provide-plus-types": makeObject({
+            "values": makeObject({
+              "big-bang": makeFunction(function(init, handlers) {
+                ffi.checkArity(2, arguments, "big-bang");
+                runtime.checkList(handlers);
+                var arr = ffi.toArray(handlers);
+                var initialWorldValue = init;
+                arr.map(function(h) { checkHandler(h); });
+                bigBang(initialWorldValue, arr);
+                ffi.throwMessageException("Internal error in bigBang: stack not properly paused and stored.");
+              }),
+              "on-tick": makeFunction(function(handler) {
+                ffi.checkArity(1, arguments, "on-tick");
+                runtime.checkFunction(handler);
+                return runtime.makeOpaque(new OnTick(handler, Math.floor(DEFAULT_TICK_DELAY * 1000)));
+              }),
+              "on-tick-n": makeFunction(function(handler, n) {
+                ffi.checkArity(2, arguments, "on-tick-n");
+                runtime.checkFunction(handler);
+                runtime.checkNumber(n);
+                var fixN = typeof n === "number" ? fixN : n.toFixnum();
+                return runtime.makeOpaque(new OnTick(handler, fixN * 1000));
+              }),
+              "to-draw": makeFunction(function(drawer) {
+                ffi.checkArity(1, arguments, "to-draw");
+                runtime.checkFunction(drawer);
+                return runtime.makeOpaque(new ToDraw(drawer));
+              }),
+              "stop-when": makeFunction(function(stopper) {
+                ffi.checkArity(1, arguments, "stop-when");
+                runtime.checkFunction(stopper);
+                return runtime.makeOpaque(new StopWhen(stopper));
+              }),
+              "on-key": makeFunction(function(onKey) {
+                ffi.checkArity(1, arguments, "on-key");
+                runtime.checkFunction(onKey);
+                return runtime.makeOpaque(new OnKey(onKey));
+              }),
+              "on-mouse": makeFunction(function(onMouse) {
+                ffi.checkArity(1, arguments, "on-mouse");
+                runtime.checkFunction(onMouse);
+                return runtime.makeOpaque(new OnMouse(onMouse));
+              }),
+              "is-world-config": makeFunction(function(v) {
+                ffi.checkArity(1, arguments, "is-world-config");
+                if(!runtime.isOpaque(v)) { return runtime.pyretFalse; }
+                return runtime.makeBoolean(isWorldConfigOption(v.val));
+              }),
+              "is-key-equal": makeFunction(function(key1, key2) {
+                ffi.checkArity(2, arguments, "is-key-equal");
+                runtime.checkString(key1);
+                runtime.checkString(key2);
+                return key1.toString().toLowerCase() === key2.toString().toLowerCase();
+              })
             }),
-            "on-tick": makeFunction(function(handler) {
-              ffi.checkArity(1, arguments, "on-tick");
-              runtime.checkFunction(handler);
-              return runtime.makeOpaque(new OnTick(handler, Math.floor(DEFAULT_TICK_DELAY * 1000)));
+            "types": makeObject({
             }),
-            "on-tick-n": makeFunction(function(handler, n) {
-              ffi.checkArity(2, arguments, "on-tick-n");
-              runtime.checkFunction(handler);
-              runtime.checkNumber(n);
-              var fixN = typeof n === "number" ? fixN : n.toFixnum();
-              return runtime.makeOpaque(new OnTick(handler, fixN * 1000));
-            }),
-            "to-draw": makeFunction(function(drawer) {
-              ffi.checkArity(1, arguments, "to-draw");
-              runtime.checkFunction(drawer);
-              return runtime.makeOpaque(new ToDraw(drawer));
-            }),
-            "stop-when": makeFunction(function(stopper) {
-              ffi.checkArity(1, arguments, "stop-when");
-              runtime.checkFunction(stopper);
-              return runtime.makeOpaque(new StopWhen(stopper));
-            }),
-            "on-key": makeFunction(function(onKey) {
-              ffi.checkArity(1, arguments, "on-key");
-              runtime.checkFunction(onKey);
-              return runtime.makeOpaque(new OnKey(onKey));
-            }),
-            "on-mouse": makeFunction(function(onMouse) {
-              ffi.checkArity(1, arguments, "on-mouse");
-              runtime.checkFunction(onMouse);
-              return runtime.makeOpaque(new OnMouse(onMouse));
-            }),
-            "on-particle": makeFunction(function(onEvent,eName,sd) {
-              ffi.checkArity(3, arguments, "on-particle");
-              runtime.checkFunction(onEvent);
-              runtime.checkString(eName);
-              return runtime.makeOpaque(new OnParticle(onEvent,eName,sd));
-            }),
-            "to-particle": makeFunction(function(toEvent,eName,sd) {
-              ffi.checkArity(3, arguments, "to-particle");
-              runtime.checkFunction(toEvent);
-              runtime.checkString(eName);
-              return runtime.makeOpaque(new ToParticle(toEvent,eName,sd));
-            }),
-            "is-world-config": makeFunction(function(v) {
-              ffi.checkArity(1, arguments, "is-world-config");
-              if(!runtime.isOpaque(v)) { return runtime.pyretFalse; }
-              return runtime.makeBoolean(isWorldConfigOption(v.val));
-            }),
-            "is-key-equal": makeFunction(function(key1, key2) {
-              ffi.checkArity(2, arguments, "is-key-equal");
-              runtime.checkString(key1);
-              runtime.checkString(key2);
-              return key1.toString().toLowerCase() === key2.toString().toLowerCase();
-            }),
-            // core configuration
-            "configure-core": makeFunction(function(coreid, acc, config) {
-              ffi.checkArity(3, arguments, "configure-core");
-              runtime.checkString(coreid);
-              runtime.checkString(acc);
-              runtime.checkList(config);
-              configCore(coreid, acc, ffi.toArray(config));
-            }),
-            "ait-enters": runtime.getField(pStruct_vals, "ait-enters"),
-            "ait-exits": runtime.getField(pStruct_vals, "ait-exits"),
-            "ait-crosses": runtime.getField(pStruct_vals, "ait-crosses"),
-            "is-ait-enters": runtime.getField(pStruct_vals, "is-ait-enters"),
-            "is-ait-exits": runtime.getField(pStruct_vals, "is-ait-exits"),
-            "is-ait-crosses": runtime.getField(pStruct_vals, "is-ait-crosses"),
-            "pc-write": runtime.getField(pStruct_vals, "pc-write"),
-            "pc-digital-read": runtime.getField(pStruct_vals, "pc-digital-read"),
-            "pc-analog-read": runtime.getField(pStruct_vals, "pc-analog-read"),
-            "is-pc-write": runtime.getField(pStruct_vals, "is-pc-write"),
-            "is-pc-digital-read": runtime.getField(pStruct_vals, "is-pc-digital-read"),
-            "is-pc-analog-read": runtime.getField(pStruct_vals, "is-pc-analog-read"),
-            "A0": runtime.getField(pStruct_vals, "A0"),
-            "A1": runtime.getField(pStruct_vals, "A1"),
-            "A2": runtime.getField(pStruct_vals, "A2"),
-            "A3": runtime.getField(pStruct_vals, "A3"),
-            "A4": runtime.getField(pStruct_vals, "A4"),
-            "A5": runtime.getField(pStruct_vals, "A5"),
-            "A6": runtime.getField(pStruct_vals, "A6"),
-            "A7": runtime.getField(pStruct_vals, "A7"),
-            "D0": runtime.getField(pStruct_vals, "D0"),
-            "D1": runtime.getField(pStruct_vals, "D1"),
-            "D2": runtime.getField(pStruct_vals, "D2"),
-            "D3": runtime.getField(pStruct_vals, "D3"),
-            "D4": runtime.getField(pStruct_vals, "D4"),
-            "D5": runtime.getField(pStruct_vals, "D5"),
-            "D6": runtime.getField(pStruct_vals, "D6"),
-            "D7": runtime.getField(pStruct_vals, "D7")
+            "internal": {
+              WorldConfigOption: WorldConfigOption,
+              adaptWorldFunction: adaptWorldFunction
+            }
           })
         });
       });
