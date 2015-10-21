brownplt/pyret-lang

commit 9a6ca6a2984ab6b95b5e97829c39b50d1258ee53

Check unbound ids before type-check, and don't erase s-instantiate
I don't see a good reason for erasing instantiations post-type-checking, since
we may want to give them semantics at some point, and it will just cause
confusion in analyses when we are suddenly missing expressions.

Also, this combined with checking for unbounds ids _after_ type-checking made
it so unbound type ids inside instantiations were completely missed, which
is really baffling to students trying to understand the binding structure of
instantations, even if they aren't explicitly checked.

tests/pyret/tests/test-compile-errors.arr
@@ -49,4 +49,17 @@ check:
     c("data D: _() sharing: m(self): 5 end end") satisfies CS.is-wf-err
     c("data _: d() sharing: m(self): 5 end end") satisfies CS.is-wf-err
   end
+
+  check "unbound type ids":
+    c("link<NotDefined>(1, empty)") satisfies CS.is-unbound-type-id
+    c(```
+fun test<A>(a :: A):
+ a
+end
+
+check:
+ test<A>(1) is 1
+end    
+```) satisfies CS.is-unbound-type-id
+  end
 end

src/arr/compiler/compile.arr
@@ -72,6 +72,7 @@ fun compile-js-ast(phases, ast, name, env, libs, options) -> CompilationPhase:
       cases(C.CompileResult) type-checked:
         | ok(_) =>
           var tc-ast = type-checked.code
+          any-errors = named-errors + U.check-unbound(env, tc-ast) + U.bad-assignments(env, tc-ast)
           type-checked := nothing
           var dp-ast = DP.desugar-post-tc(tc-ast, env)
           tc-ast := nothing
@@ -84,7 +85,6 @@ fun compile-js-ast(phases, ast, name, env, libs, options) -> CompilationPhase:
           inlined = cleaned.visit(U.inline-lams)
           cleaned := nothing
           when options.collect-all: ret := phase("Inlined lambdas", inlined, ret) end
-          any-errors = named-errors + U.check-unbound(env, inlined) + U.bad-assignments(env, inlined)
           if is-empty(any-errors):
             if options.collect-all: P.trace-make-compiled-pyret(ret, phase, inlined, env, options)
             else: phase("Result", C.ok(P.make-compiled-pyret(inlined, env, options)), ret)

src/arr/compiler/desugar-post-tc.arr
@@ -32,9 +32,6 @@ desugar-visitor = A.default-map-visitor.{
       A.s-cases-else(l, A.a-blank, val-id, branches.map(_.visit(self)),
         A.s-block(l, [list: no-cases-exn(l, val-id)])))
   end,
-  s-instantiate(self, l, body, args):
-    body.visit(self)
-  end,
   s-check(self, l, name, body, keyword-check):
     A.s-id(l, A.s-global("nothing"))
   end

commit 0e3253b7d45d46a32f519355d5221a268aa50a32

Fix a lurking bug that manifested as a regression after 9a6ca6a2
It turns out that some .reverse() calls were missed after folding over lists
of type bindings.  Since one of them was in default-env-map-visitor, every
time this helper was used, it would flip-flop the order of type bindings in
the list.  This made it so checking unbound ids only worked after "cleaning"
the AST, which amusingly caused the required (even) number of flip-flops to
happen.  Moving the call to check-unbound broke the pattern and caused the
issue on programs like:

  type N = Number
  type N2 = N

src/arr/compiler/anf-loop-compiler.arr
@@ -1396,6 +1396,8 @@ fun compile-program(self, l, imports-in, prog, freevars, env):
           n.toname()
         else if import-keys.ts.has-key(n.key()):
           type-name(n.toname())
+        else:
+          raise("Unaware of imported name: " + n.key())
         end
     end
     j-var(js-id-of(n), j-method(NAMESPACE, "get", [clist: j-str(bind-name)]))

src/arr/compiler/ast-util.arr
@@ -193,7 +193,7 @@ fun default-env-map-visitor<a, c>(
         new-bind = b.visit(visit-envs)
         updated.{ bs: link(new-bind, acc.bs) }
       end
-      A.s-type-let-expr(l, bound-env.bs, body.visit(self.{ env: bound-env.val-env, type-env: bound-env.type-env }))
+      A.s-type-let-expr(l, bound-env.bs.reverse(), body.visit(self.{ env: bound-env.val-env, type-env: bound-env.type-env }))
     end,
     s-let-expr(self, l, binds, body):
       bound-env = for fold(acc from { e: self.env, bs : [list: ] }, b from binds):

src/arr/compiler/resolve-scope.arr
@@ -653,7 +653,7 @@ fun resolve-names(p :: A.Program, initial-env :: C.CompileEnvironment):
         end
       end
       visit-body = body.visit(self.{env: bound-env.e, type-env: bound-env.te})
-      A.s-type-let-expr(l, bound-env.bs, visit-body)
+      A.s-type-let-expr(l, bound-env.bs.reverse(), visit-body)
     end,
     s-let-expr(self, l, binds, body):
       bound-env = for fold(acc from { e: self.env, bs : [list: ] }, b from binds):

tests/pyret/tests/test-compile-errors.arr
@@ -4,7 +4,6 @@ import "../test-compile-helper.arr" as C
 check:
   fun c(str):
     result = C.compile-str(str)
-    result satisfies CS.is-err
     cases(CS.CompileResult) result:
       | ok(_) => "No error for " + str
       | err(probs) => probs.first
@@ -61,5 +60,20 @@ check:
  test<A>(1) is 1
 end    
 ```) satisfies CS.is-unbound-type-id
+  end
+
+  check "bound type aliases":
+    c(```
+type N = Number
+type N2 = N
+
+fun test<A>(a :: A):
+ a
+end
+
+check:
+ test<N>(1) is 1
+end    
+```) satisfies string-contains(_, "No error")
   end
 end