1dbd3787985f2ae3ed1e4dc679ee3d81c1863c51
Ben Lerner <blerner@cs.brown.edu>
2016-02-02 16:00:39 -0500

    legacy-path converted

--- src/js/trove/legacy-path.js
+++ src/js/trove/legacy-path.js
@@ -1,38 +1,42 @@
-define([], function() {
-  return function(runtime, ns) {
-    var F = runtime.makeFunction;
-    function getLegacyPath(name) {
-      runtime.pauseStack(function(restarter) {
-        // NOTE(joe): This is a bit of requireJS hackery that assumes a
-        // certain layout for builtin modules
-        require([name], function(m) {
-          restarter.resume(runtime.makeObject({
-            "get-raw-dependencies":
-              F(function() {
-                return [];
-              }),
-            "get-raw-provides":
-              F(function() {
-                return [];
-              }),
-            "get-raw-compiled":
-              F(function() {
-                return runtime.makeOpaque(function() { return m; });
-              })
-          }));
-        });
+define(["js/runtime-util"], function(util) {
+  return util.definePyretModule("legacy-path",
+    [],
+    [],
+    {},
+    function(RUNTIME, NAMESPACE) {
+      var F = RUNTIME.makeFunction;
+      function getLegacyPath(name) {
+        RUNTIME.pauseStack(function(restarter) {
+          // NOTE(joe): This is a bit of requireJS hackery that assumes a
+          // certain layout for builtin modules
+          require([name], function(m) {
+            restarter.resume(RUNTIME.makeObject({
+              "get-raw-dependencies":
+                F(function() {
+                  return [];
+                }),
+              "get-raw-provides":
+                F(function() {
+                  return [];
+                }),
+              "get-raw-compiled":
+                F(function() {
+                  return RUNTIME.makeOpaque(function() { return m; });
+                })
+            }));
+          });
 
+        });
+      }
+      var O = RUNTIME.makeObject;
+      return O({
+        "provide-plus-types": O({
+          types: { },
+          values: O({
+            "legacy-path-raw-locator": RUNTIME.makeFunction(getLegacyPath)
+          })
+        }),
+        "aNAMESPACEwer": RUNTIME.nothing
       });
-    }
-    var O = runtime.makeObject;
-    return O({
-      "provide-plus-types": O({
-        types: { },
-        values: O({
-          "legacy-path-raw-locator": runtime.makeFunction(getLegacyPath)
-        })
-      }),
-      "answer": runtime.nothing
     });
-  };
-});
+})
