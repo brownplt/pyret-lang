34c36ac0f4fe063711330e5bc6babcdaa3554219
Ben Lerner <blerner@cs.brown.edu>
2016-03-27 17:47:03 -0400

    Cleaning up the make-standalone script so that it sends far less data to requirejs than before, and therefore hopefully the dependency-tracing process should be much faster.

--- make-standalone.sh
+++ make-standalone.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-node build/phase1/main-wrapper.js --builtin-js-dir src/js/troveA/ \
+TARGET=tests/pyret/standalone/importer
+
+node build/phase1/main-wrapper.js \
+              --builtin-js-dir src/js/troveA/ \
               --builtin-arr-dir src/arr/troveA/ \
               -no-check-mode \
-              --build-standalone tests/pyret/standalone/importer.arr \
+              --build-standalone $TARGET.arr \
               > build/phaseA/program.js
 
-node node_modules/requirejs/bin/r.js -o src/scripts/require-build.js \
-  baseUrl=build/phaseA/ name=handalone \
-  out=tests/pyret/standalone/importer.arr.js
+# keep only the initial call to define, with its dependency list...
+sed -e '/]/,$d' build/phaseA/program.js > build/phaseA/program-require.js
+# and close it off...
+cat <<END >> build/phaseA/program-require.js
+  ]);
+requirejs(["runtime"]);
+END
+# then rename the call from define to requirejs.
+sed -i -- 's/define/requirejs/' build/phaseA/program-require.js
 
-cat <<END > tests/pyret/standalone/importer.jarr
+# Use requirejs to process that minimal file
+node node_modules/requirejs/bin/r.js -o src/scripts/require-build.js \
+  baseUrl=build/phaseA/ name=program-require \
+  out=$TARGET-require.js
+
+# Construct the working standalone by initializing requirejs,
+cat <<END > $TARGET.jarr
 requirejs = require("requirejs");
 define = requirejs.define;
 END
-  
-
-cat tests/pyret/standalone/importer.arr.js >> tests/pyret/standalone/importer.jarr
+# including the native requires,
+cat $TARGET-require.js >> $TARGET.jarr
+# the program itself,
+cat build/phaseA/program.js | sed -e 's/define(\[/define("program", \[/' >> $TARGET.jarr
+# and the handalone driver
+cat build/phaseA/handalone.js >> $TARGET.jarr
