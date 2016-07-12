ae2d766cd0d2b9fcea2ab0b4aea005c6ccdef1bd
mkolosick <mskolosick@gmail.com>
2016-07-12 12:07:15 -0400

    Added ffi fixes for tuples

--- src/js/base/runtime.js
+++ src/js/base/runtime.js
@@ -2691,9 +2691,9 @@ function (Namespace, jsnums, codePoint, seedrandom, util) {
     PTupleAnn.prototype.check = function(compilerLoc, val) {
       var that = this;
       if(!isTuple(val)) {
-        return ffi.contractFail(
+        return thisRuntime.ffi.contractFail(
           makeSrcloc(compilerLoc),
-          ffi.makeTypeMismatch(val, "Tuple")
+          thisRuntime.ffi.makeTypeMismatch(val, "Tuple")
         );
       }
       if(that.anns.length != val.vals.length) {
@@ -2708,22 +2708,22 @@ function (Namespace, jsnums, codePoint, seedrandom, util) {
           thisAnn = thisChecker;
           return thisChecker.check(that.locs[that.locs.length - remainingAnns.length], val.vals[remainingAnns.length]);
         }, function(result) {
-          if(ffi.isOk(result)) {
-            if(remainingAnns.length === 0) { return ffi.contractOk; }
+          if(thisRuntime.ffi.isOk(result)) {
+            if(remainingAnns.length === 0) { return thisRuntime.ffi.contractOk; }
             else { return deepCheckFields(remainingAnns); }
           }
-          else if(ffi.isFail(result)) {
+          else if(thisRuntime.ffi.isFail(result)) {
             return that.createTupleFailureError(compilerLoc, val, thisAnn, result);
             //return ffi.throwMessageException("types are wrong");
           }
         },
         "PTupleAnn:deepCheckFields");
       }
-      if(that.anns.length === 0) { return ffi.contractOk; }
+      if(that.anns.length === 0) { return thisRuntime.ffi.contractOk; }
       else { return deepCheckFields(that.anns.slice()); }
     }
     PTupleAnn.prototype.createTupleLengthMismatch = function(compilerLoc, val, annLength, tupLength) {
-      return ffi.contractFail(compilerLoc, ffi.makeTupleLengthMismatch(compilerLoc, val, annLength, tupLength));
+      return thisRuntime.ffi.contractFail(compilerLoc, thisRuntime.ffi.makeTupleLengthMismatch(compilerLoc, val, annLength, tupLength));
     };
     PTupleAnn.prototype.createTupleFailureError = function(compilerLoc, val, ann, result) {
       var that = this;
@@ -2731,10 +2731,10 @@ function (Namespace, jsnums, codePoint, seedrandom, util) {
       for(var i = 0; i < that.anns.length; i++) {
         if(that.anns[i] === ann) { loc = that.locs[i]; }
       }
-      return ffi.contractFail(
+      return thisRuntime.ffi.contractFail(
         makeSrcloc(compilerLoc),
-        ffi.makeTupleAnnsFail(val, ffi.makeList([
-          ffi.makeAnnFailure(
+        thisRuntime.ffi.makeTupleAnnsFail(val, thisRuntime.ffi.makeList([
+          thisRuntime.ffi.makeAnnFailure(
             makeSrcloc(loc),
             ann,
             getField(result, "reason")
