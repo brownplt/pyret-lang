297ffa7d3c7fcbdd2bc077234bf61d3c0eacc2db
Joe Gibbs Politz <joe.politz@gmail.com>
2016-07-13 10:53:52 -0400

    docs are now at github.com/brownplt/pyret-docs, no need for them to be here

--- docs/written/arr/compiler/gensym.arr.js.rkt
+++
@@ -1,8 +0,0 @@
-#lang scribble/base
-@(require "../../../scribble-api.rkt")
-@docmodule["\"compiler/gensym.arr\""]{
-  @; Unknown: PLEASE DOCUMENT
-  @ignore[(list "gensym-counter")]
-  @section[#:tag "\"compiler/gensym.arr\"_Functions"]{Functions}
-  @function["make-name"]
-}
--- docs/written/arr/compiler/well-formed.arr.js.rkt
+++
@@ -1,6 +0,0 @@
-#lang scribble/base
-@(require "../../../scribble-api.rkt")
-@docmodule["\"compiler/well-formed.arr\""]{
-  @section[#:tag "\"compiler/well-formed.arr\"_Functions"]{Functions}
-  @function["check-well-formed"]
-}
