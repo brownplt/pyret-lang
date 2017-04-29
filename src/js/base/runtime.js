aeed549081d74cb3f228a44322b9a631abddb4ff
ian boros <puppyofkosh@gmail.com>
2017-04-29 14:46:54 -0400

    Fix some tests. Have allowBounce flag inherit from parent runtime

--- src/js/base/runtime.js
+++ src/js/base/runtime.js
@@ -5877,6 +5877,12 @@ function (Namespace, jsnums, codePoint, seedrandom, util) {
       thisRuntime.bounceAllowed = theOutsideWorld.options.bounceAllowed;
     }
 
+    if (theOutsideWorld.parentRuntime) {
+      thisRuntime.bounceAllowed &= theOutsideWorld.parentRuntime.bounceAllowed;
+    }
+
+    console.log("New runtime. Is bounce allowed: " + thisRuntime.bounceAllowed);
+
     return thisRuntime;
   }
 
