65f7fc48ca95c141b74dd9e0ee5be8d813d18a2c
ian boros <puppyofkosh@gmail.com>
2017-06-09 10:58:52 -0400

    more setting environment up

--- build.js
+++ build.js
@@ -1,8 +1,11 @@
 ({
     baseUrl: './optimizer',
-    name: "all",
+    name: "main",
+    //include: ["requireLib"],
     optimize: 'none',
     out: 'compiled.js',
+    cjsTranslate: true,
+    //wrap: true,
     skipSemiColonInsertion: true,
     paths: {
         "source-map": "../node_modules/source-map/dist/source-map",
@@ -11,8 +14,7 @@
         "s-expression": "../node_modules/s-expression/index",
         "path": "../node_modules/path/path",
         "fs": "empty:",
-        "requirejs": "../node_modules/requirejs/require",
-        "js-sha256": "../node_modules/js-sha256/src/sha256"
+        "requireLib": "../node_modules/requirejs/require",
+        "js-sha256": "../node_modules/js-sha256/src/sha256",
     }
 })
-// Run with node node_modules/requirejs/bin/r.js -o build.js
