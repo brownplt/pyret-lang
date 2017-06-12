e9b57746ae15431a11960031e3021725af21b81b
ian boros <puppyofkosh@gmail.com>
2017-06-12 15:11:10 -0400

    working on getting test suite to work

--- browserify/browserify-require-combined.js
+++ browserify/browserify-require-combined.js
@@ -9,3 +9,13 @@ define("seedrandom", [], function() {return seedrandom;});
 
 sourcemap = require("source-map");
 define("source-map", [], function () { return sourcemap; });
+
+jssha256 = require("js-sha256");
+define("js-sha256", [], function () { return jssha256; });
+
+// This one is different since we need to ask for browserify's copy of fs
+fs = require("fs");
+define("fs", [], function () { return fs; });
+
+path = require("path");
+define("path", [], function () { return path; });
