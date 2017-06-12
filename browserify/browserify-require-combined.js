08c192df3942ac8df9b24090e76a9bb689136322
ian boros <puppyofkosh@gmail.com>
2017-06-12 14:05:42 -0400

    Get browserify working with hello world

---
+++ browserify/browserify-require-combined.js
@@ -0,0 +1,11 @@
+sexpr = require("s-expression");
+define("s-expression", [], function() {return sexpr;});
+
+q = require("q");
+define("q", [], function() {return q;});
+
+seedrandom = require("seedrandom");
+define("seedrandom", [], function() {return seedrandom;});
+
+sourcemap = require("source-map");
+define("source-map", [], function () { return sourcemap; });
---
+++ browserify/header.js
@@ -0,0 +1,4 @@
+if(typeof window === 'undefined') {
+var requirejs = require("requirejs");
+var define = requirejs.define;
+}
