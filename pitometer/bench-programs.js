031757ab9c0c62449c3db46d3ee660c46ccd9072
Joe Gibbs Politz <joe.politz@gmail.com>
2017-09-04 12:20:03 -0700

    make sure this runs with appropriate stack size, since otherwise errors result

--- pitometer/bench-programs.js
+++ pitometer/bench-programs.js
@@ -20,7 +20,8 @@ define(["child_process", "time-helpers", "fs", "path"], function(childProcess, t
       const [compileSuccess, , ] = maybeTime(true, () => echoRun(
         `env EF='--flatness-threshold -1' make ${toBuild}`, {stdio: [0, 1, 2]}));
 
-      return maybeTime(compileSuccess, () => echoRun(`node ${toBuild}`));
+      echoRun(`ulimit -s 65532`);
+      return maybeTime(compileSuccess, () => echoRun(`node --stack-size=65532 ${toBuild}`));
     }
 
     let paths = fs.readdirSync(programsPath);
