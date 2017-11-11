37453771a2a869e29154060009f0b2c860f6e11e
Rachit Nigam <rachit.nigam12@gmail.com>
2017-11-11 18:34:11 -0500

    Revert "use HOF in runtime"
    This reverts commit 829bb4975679decf7a6d3db5d94f172a30083a3c.

--- src/js/base/vhull-runtime.js
+++ src/js/base/vhull-runtime.js
@@ -23,6 +23,237 @@ define("pyret-base/js/runtime",
   var require = requirejs;
   var AsciiTable;
 
+  function map(obj, callback/*, thisArg*/) {
+
+    var T, A, k;
+
+    if (obj == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this|
+    //    value as the argument.
+    var O = Object(obj);
+
+    // 2. Let lenValue be the result of calling the Get internal
+    //    method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let A be a new array created as if by the expression new Array(len)
+    //    where Array is the standard built-in constructor with that name and
+    //    len is the value of len.
+    A = new Array(len);
+
+    // 7. Let k be 0
+    k = 0;
+
+    // 8. Repeat, while k < len
+    while (k < len) {
+
+      var kValue, mappedValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal
+      //    method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        //    method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Let mappedValue be the result of calling the Call internal
+        //     method of callback with T as the this value and argument
+        //     list containing kValue, k, and O.
+        mappedValue = callback.call(T, kValue, k, O);
+
+        // iii. Call the DefineOwnProperty internal method of A with arguments
+        // Pk, Property Descriptor
+        // { Value: mappedValue,
+        //   Writable: true,
+        //   Enumerable: true,
+        //   Configurable: true },
+        // and false.
+
+        // In browsers that support Object.defineProperty, use the following:
+        // Object.defineProperty(A, k, {
+        //   value: mappedValue,
+        //   writable: true,
+        //   enumerable: true,
+        //   configurable: true
+        // });
+
+        // For best browser support, use the following:
+        A[k] = mappedValue;
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+
+    // 9. return A
+    return A;
+  };
+  function filter(obj, fun/*, thisArg*/) {
+    'use strict';
+
+    if (obj === void 0 || obj === null) {
+      throw new TypeError();
+    }
+
+    var t = Object(obj);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+
+        // NOTE: Technically this should Object.defineProperty at
+        //       the next index, as push can be affected by
+        //       properties on Object.prototype and Array.prototype.
+        //       But that method's new, and collisions should be
+        //       rare, so use the more-compatible alternative.
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+  function reduce(obj, callback /*, initialValue*/) {
+    if (obj === null) {
+      throw new TypeError( 'Array.prototype.reduce ' +
+        'called on null or undefined' );
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError( callback +
+        ' is not a function');
+    }
+
+    // 1. Let O be ? ToObject(this value).
+    var o = Object(obj);
+
+    // 2. Let len be ? ToLength(? Get(O, "length")).
+    var len = o.length >>> 0;
+
+    // Steps 3, 4, 5, 6, 7
+    var k = 0;
+    var value;
+
+    if (arguments.length >= 2) {
+      value = arguments[1];
+    } else {
+      while (k < len && !(k in o)) {
+        k++;
+      }
+
+      // 3. If len is 0 and initialValue is not present,
+      //    throw a TypeError exception.
+      if (k >= len) {
+        throw new TypeError( 'Reduce of empty array ' +
+          'with no initial value' );
+      }
+      value = o[k++];
+    }
+
+    // 8. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ! ToString(k).
+      // b. Let kPresent be ? HasProperty(O, Pk).
+      // c. If kPresent is true, then
+      //    i.  Let kValue be ? Get(O, Pk).
+      //    ii. Let accumulator be ? Call(
+      //          callbackfn, undefined,
+      //          « accumulator, kValue, k, O »).
+      if (k in o) {
+        value = callback(value, o[k], k, o);
+      }
+
+      // d. Increase k by 1.
+      k++;
+    }
+
+    // 9. Return accumulator.
+    return value;
+  }
+  function forEach(obj, callback/*, thisArg*/) {
+
+    var T, k;
+
+    if (obj == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling toObject() passing the
+    // |this| value as the argument.
+    var O = Object(obj);
+
+    // 2. Let lenValue be the result of calling the Get() internal
+    // method of O with the argument "length".
+    // 3. Let len be toUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If isCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let
+    // T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let k be 0.
+    k = 0;
+
+    // 7. Repeat while k < len.
+    while (k < len) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //    This is implicit for LHS operands of the in operator.
+      // b. Let kPresent be the result of calling the HasProperty
+      //    internal method of O with argument Pk.
+      //    This step can be combined with c.
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        // method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Call the Call internal method of callback with T as
+        // the this value and argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined.
+  };
+
   function copyArgs(args) {
     return Array.prototype.slice.call(args);
   }
@@ -270,7 +501,7 @@ define("pyret-base/js/runtime",
         }
         var headers = thisRuntime.getField(val, "headers");
         var rowSkel = thisRuntime.getField(val, "rows");
-        headers = headers.map(function(h){ return renderValueSkeleton(h, []); });
+        headers = map(headers, function(h){ return renderValueSkeleton(h, []); });
         var rows = [];
         for (var i = 0; i < rowSkel.length; i++) {
           var curRow = [];
@@ -1814,7 +2045,7 @@ define("pyret-base/js/runtime",
     function isPyretException(val) { return val instanceof PyretFailException; }
     PyretFailException.prototype.toString = function() {
       var stackStr = this.pyretStack && this.pyretStack.length > 0 ?
-        this.getStack().map(function(s) {
+        map(this.getStack(), function(s) {
           var g = getField;
           if(s && hasField(s, "source")) {
             return g(s, "source") +
@@ -1832,7 +2063,7 @@ define("pyret-base/js/runtime",
       return toReprJS(this.exn, ReprMethods._tostring) + "\n" + stackStr;
     };
     PyretFailException.prototype.getStack = function() {
-      return this.pyretStack.map(makeSrcloc);
+      return map(this.pyretStack, makeSrcloc);
     };
 
     function makeSrcloc(arr) {
@@ -2548,7 +2779,7 @@ define("pyret-base/js/runtime",
     }
 
     function checkArgsInternal(moduleName, funName, args, anns) {
-      anns.forEach(function(ann, i) {
+      forEach(anns, function(ann, i) {
         if (!isCheapAnnotation(ann)) {
           thisRuntime.ffi.throwMessageException("Internal error: non-stacksafe annotation given to checkArgsInternal");
         }
@@ -3067,7 +3298,7 @@ define("pyret-base/js/runtime",
     Cont.prototype._toString = function() {
       var stack = this.stack;
       var stackStr = stack && stack.length > 0 ?
-        stack.map(function(s) {
+        map(stack, function(s) {
           if(!s && s.from) { return "<blank frame>"; }
           else {
             if(typeof s.from === "string") { return s; }
@@ -3140,10 +3371,9 @@ define("pyret-base/js/runtime",
     function printPyretStack(stack, verbose) {
       if (stack === undefined) return "  undefined";
       if (!verbose) {
-        stack = stack.filter(
-          function(val) { return val instanceof Array && val.length == 7; });
+        stack = filter(stack, function(val) { return val instanceof Array && val.length == 7; });
       }
-      var stackStr = stack.map(function(val) {
+      var stackStr = map(stack, function(val) {
         if (val instanceof Array && val.length == 7) {
           return (val[0] + ": line " + val[1] + ", column " + val[2]);
         } else if (val) {
@@ -3571,8 +3801,7 @@ define("pyret-base/js/runtime",
       thisRuntime.checkString(s);
       thisRuntime.checkString(splitstr);
 
-      return thisRuntime.ffi.makeList(
-        s.split(splitstr).map(thisRuntime.makeString));
+      return map(thisRuntime.ffi.makeList(s.split(splitstr), thisRuntime.makeString));
     }
     var string_split = function(s, splitstr) {
       if (arguments.length !== 2) { var $a=new Array(arguments.length); for (var $i=0;$i<arguments.length;$i++) { $a[$i]=arguments[$i]; } throw thisRuntime.ffi.throwArityErrorC(["string-split"], 2, $a); }
@@ -3611,7 +3840,7 @@ define("pyret-base/js/runtime",
     var string_explode = function(s) {
       if (arguments.length !== 1) { var $a=new Array(arguments.length); for (var $i=0;$i<arguments.length;$i++) { $a[$i]=arguments[$i]; } throw thisRuntime.ffi.throwArityErrorC(["string-explode"], 1, $a); }
       thisRuntime.checkString(s);
-      return thisRuntime.ffi.makeList(s.split("").map(thisRuntime.makeString))
+      return map(thisRuntime.ffi.makeList(s.split(""),thisRuntime.makeString));
     }
     var string_indexOf = function(s, find) {
       if (arguments.length !== 2) { var $a=new Array(arguments.length); for (var $i=0;$i<arguments.length;$i++) { $a[$i]=arguments[$i]; } throw thisRuntime.ffi.throwArityErrorC(["string-index-of"], 2, $a); }
@@ -3928,11 +4157,10 @@ define("pyret-base/js/runtime",
     function loadBuiltinModules(modules, startName, withModules) {
       function loadWorklist(startMod) {
         function addMod(curMod, curPath, curName) {
-          if (curPath.filter(
-            function(b) { return b.name === curMod.name; }).length > 0) {
-              CONSOLE.error("Module cycle: ", curMod, curPath);
-              throw new Error("Module cycle in loadBuiltinModules");
-            }
+          if (filter(curPath, function(b) { return b.name === curMod.name; }).length > 0) {
+            CONSOLE.error("Module cycle: ", curMod, curPath);
+            throw new Error("Module cycle in loadBuiltinModules");
+          }
           if (typeof curMod === "function") {
             return [{mod: {
                 theModule: curMod,
@@ -3943,7 +4171,7 @@ define("pyret-base/js/runtime",
             }];
           }
           var curDeps = curMod.dependencies;
-          var depMods = curDeps.map(function(d) {
+          var depMods = map(curDeps, function(d) {
             //CONSOLE.error("Going to load: ", d);
             if(d.protocol === "legacy-path") {
               return { dname: d.args[0], modinfo: require(d.args[0]) };
@@ -3957,7 +4185,7 @@ define("pyret-base/js/runtime",
             }
           });
           var tocomp = {mod: curMod, name: curName, path: curPath};
-          return depMods.reduce(function(acc, elt) {
+          return reduce(depMods, function(acc, elt) {
             //CONSOLE.error("elt to reduce on: ", elt);
             return addMod(elt.modinfo, curPath.concat([tocomp]), elt.dname).concat(acc);
           }, [tocomp])
@@ -3980,21 +4208,21 @@ define("pyret-base/js/runtime",
         var funNoSpace = String(moduleFun).indexOf("function(R") === 0;
         return len2 && (funSpace || funNoSpace);
       }
-      var rawModules = wl.forEach(function(m) {
+      var rawModules = forEach(wl, function(m) {
         if(m.mod.name === startName) { return; }
         // NOTE(joe): yes this is depressing.  I know.
         if(isProbablyOldStyleRNSFunction(m.mod.theModule)) { // Already a runtime/namespace function
           var thisRawMod = m.mod.theModule;
         }
         else {
-          var rawDeps = m.mod.dependencies.map(function(d) {
+          var rawDeps = map(m.mod.dependencies, function(d) {
             return finalModMap[getName(d)];
           });
           var thisRawMod = m.mod.theModule.apply(null, rawDeps);
         }
         finalModMap[m.name] = thisRawMod;
       });
-      var originalOrderRawModules = modules.map(function(m) {
+      var originalOrderRawModules = map(modules, function(m) {
         var mod = finalModMap[getName(m)];
         if(typeof mod === "undefined") {
           //CONSOLE.error("FinalModMap: ", finalModMap);
@@ -4098,7 +4326,7 @@ define("pyret-base/js/runtime",
                             JSON.stringify(m, null, "  "));
           }
         };
-        var wrappedMods = ms.map(wrapMod);
+        var wrappedMods = map(ms, wrapMod);
         return withModules.apply(null, wrappedMods);
       });
     }
@@ -4107,8 +4335,7 @@ define("pyret-base/js/runtime",
         var ms = new Array(arguments.length);
         for (var i = 0; i < arguments.length; i++) ms[i] = arguments[i];
         return safeTail(function() {
-          return withModules.apply(null,
-            ms.map(function(m) { return getField(m, "values"); }));
+          return withModules.apply(null, map(ms, function(m) { return getField(m, "values"); }));
         });
       });
     }
@@ -4175,7 +4402,7 @@ define("pyret-base/js/runtime",
       }
 
       function quote(s) { if (typeof s === "string") { return "'" + s + "'"; } else { return s; } }
-      function constArr(arr) { return "[" + arr.map(quote).join(",") + "]"; }
+      function constArr(arr) { return "[" + map(arr, quote).join(",") + "]"; }
 
       function makeConstructor() {
         var argNames = constructor.$fieldNames;
@@ -4183,14 +4410,14 @@ define("pyret-base/js/runtime",
         return thisRuntime.safeCall(function () {
           return checkAnnsThunk();
         }, function (checkAnns) {
-          checkAnns.forEach(function(a) {
+          forEach(checkAnns, function(a) {
             if(!isCheapAnnotation(a)) {
               hasRefinement = true;
             }
           });
           var constructorBody =
             "var dict = thisRuntime.create(base);\n";
-          allArgs.forEach(function(a, i) {
+          forEach(allArgs, function(a, i) {
             if(allMuts[i]) {
               var checkIndex = checkArgs.indexOf(a);
               if(checkIndex >= 0) {
@@ -4216,7 +4443,7 @@ define("pyret-base/js/runtime",
               "});";
           }
           else {
-            checkArgs.forEach(function(a, i) {
+            forEach(checkArgs, function(a, i) {
               if(checkMuts[i]) {
                 checksPlusBody += "var checkAns = thisRuntime.isGraphableRef(" + checkArgs[i] + ") || thisRuntime._checkAnn(checkLocs[" + i + "], checkAnns[" + i + "], " + checkArgs[i] + ");";
               }
@@ -4279,7 +4506,7 @@ define("pyret-base/js/runtime",
 
     function addModuleToNamespace(namespace, valFields, typeFields, moduleObj) {
       var newns = Namespace.namespace({});
-      valFields.forEach(function(vf) {
+      forEach(valFields, function(vf) {
         if(hasField(moduleObj, "defined-values")) {
           newns = newns.set(vf, getField(moduleObj, "defined-values")[vf]);
         }
@@ -4287,7 +4514,7 @@ define("pyret-base/js/runtime",
           newns = newns.set(vf, getField(getField(moduleObj, "values"), vf));
         }
       });
-      typeFields.forEach(function(tf) {
+      forEach(typeFields, function(tf) {
         newns = newns.setType(tf, getField(moduleObj, "types")[tf]);
       });
       return namespace.merge(newns);
