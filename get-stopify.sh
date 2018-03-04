6e451d3e9bf4e48886634548b439698cd3e5f9bb
Rachit Nigam <rachit.nigam12@gmail.com>
2018-03-04 14:57:53 -0500

    fix travis

---
+++ get-stopify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+BRANCH=deep-stacks-dev
+
+# Remove any previous stopify versions
+rm -rf node_modules/{stopify,stopify-continuations}
+
+# Clone the stopify repo
+git clone https://github.com/plasma-umass/Stopify.git --branch $BRANCH stopify
+
+cd stopify
+# Build the stopify version
+yarn install && yarn run build
+
+# copy the stopify version
+cd ..
+cp -R stopify/stopify-continuations node_modules/
+cp -R stopify/stopify node_modules/
+
+rm -rf stopify
