2310012a22dfc8a243e008f1ac454be64b5c3d24
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2018-03-06 16:42:57 -0500

    also fix gensym

--- src/js/trove/global.js
+++ src/js/trove/global.js
@@ -255,7 +255,7 @@
 
       // String functions
 
-      "gensym":{'bind': 'fun', 'flatness': 0, 'name': 'gensym', 'typ': ['arrow', [], 'String']},
+      "gensym":{'bind': 'fun', 'flatness': 0, 'name': 'gensym', 'typ': ['arrow', ['String'], 'String']},
       "string-repeat":{'bind': 'fun',
                        'flatness': 0,
                        'name': 'string-repeat',
