6902103a328f6ed3b48c8c79f90425179f665dc7
Ben Lerner <blerner@ccs.neu.edu>
2018-04-30 00:00:48 -0400

    today's experiments in trying to improve the parser's performance: may not have shrunk runtime, but shrunk generated parser file size

--- src/scripts/just-parse.js
+++ src/scripts/just-parse.js
@@ -7,22 +7,45 @@ R.config({
   }
 });
 R(["pyret-base/js/pyret-tokenizer", "pyret-base/js/pyret-parser", "fs"], function(T, G, fs) {
+  var start = process.hrtime();
   const data = fs.readFileSync(process.argv[2], {encoding: "utf-8"});
+  var readingTime = process.hrtime(start);
   const toks = T.Tokenizer;
   toks.tokenizeFrom(data);
   // while (toks.hasNext())
   //   console.log(toks.next().toString(true));
+  start = process.hrtime();
   var parsed = G.PyretGrammar.parse(toks);
+  var parsedTime = process.hrtime(start);
   if (parsed) {
     console.log("Result:");
+    start = process.hrtime();
     var countParses = G.PyretGrammar.countAllParses(parsed);
+    var countTime = process.hrtime(start);
     console.log("There are " + countParses + " potential parses");
     if (countParses === 1) {
+      start = process.hrtime();
       var ast = G.PyretGrammar.constructUniqueParse(parsed);
+      var astTime = process.hrtime(start);
       console.log("AST constructed");
+      start = process.hrtime()
       var astStr = ast.toString(true);
+      var tostrTime = process.hrtime(start);
+
+      const NS_PER_SEC = 1e9;
+      [{"reading  ": readingTime},
+       {"parsing  ": parsedTime},
+       {"counting ": countTime},
+       {"ast      ": astTime},
+       {"toString ": tostrTime}].forEach(function(entry) {
+         for (var k in entry) {
+           entry[k] = ((entry[k][0] * NS_PER_SEC + entry[k][1]) / NS_PER_SEC).toFixed(10);
+         }
+         console.log(entry);
+       });
+
       console.log("ast.toString().length = " + astStr.length)
-      console.log(astStr);
+      // console.log(astStr);
       return ast;
     } else {
       var asts = G.PyretGrammar.constructAllParses(parsed);
