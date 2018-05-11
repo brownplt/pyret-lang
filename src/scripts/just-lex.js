897b77638d2c01eb7439cff9d9cdf6964b55afb5
Ben Lerner <blerner@ccs.neu.edu>
2018-05-11 12:32:49 -0400

    enabling running both tokenizers side-by-side, and adding some histogram information to show which tokens are the most expensive (cumulative and per-token) to lex.  Also optimizing whitespace tokens, to not even bother pulling out their strings or constructing token objects, since we ignore them anyway/

--- src/scripts/just-lex.js
+++ src/scripts/just-lex.js
@@ -2,21 +2,25 @@ const R = require("requirejs");
 
 R.config({
   paths: {
-    'jglr': "../../lib/jglr/",
+    'jglr': "../../lib/jglr",
     'pyret-base': "../../build/phaseA",
+    'src-base': "../../src/js/base"
   }
 });
-R(["pyret-base/js/pyret-tokenizer", "pyret-base/js/pyret-parser", "fs", "tokenize"], function(T, G, fs, Tok) {
+R(["pyret-base/js/pyret-tokenizer", "pyret-base/js/pyret-parser", "fs", "src-base/pyret-tokenizer2"], function(T, G, fs, Tok) {
   const data = fs.readFileSync(process.argv[2], {encoding: "utf-8"});
   console.log(process.argv[2]);
-
-
-  var t = new Tok.Tokenizer(data, Tok.spec);
+  
+  var t = new Tok.Tokenizer(Tok.spec);
+  t.tokenizeFrom(data);
   var startNew = process.hrtime();
   var newAns = [];
+  var tokHist = {};
   while (t.hasNext()) {
     var tok = t.next();
     newAns.push(tok);
+    if (tokHist[tok.name] === undefined) tokHist[tok.name] = 0;
+    tokHist[tok.name]++;
   }
   var endNew = process.hrtime(startNew);
 
@@ -33,13 +37,37 @@ R(["pyret-base/js/pyret-tokenizer", "pyret-base/js/pyret-parser", "fs", "tokeniz
   console.log("=========================================================");
 
   const NS_PER_SEC = 1e9;
-  [{"original  ": endTrue},
-   {"new lexer ": endNew}].forEach(function(entry) {
-     for (var k in entry) {
-       entry[k] = ((entry[k][0] * NS_PER_SEC + entry[k][1]) / NS_PER_SEC).toFixed(10);
-     }
-     console.log(entry);
-   });
+  var stats =
+      [{"original  ": endTrue},
+       {"new lexer ": endNew}];
+  stats.forEach(function(entry) {
+    for (var k in entry) {
+      entry[k] = ((entry[k][0] * NS_PER_SEC + entry[k][1]) / NS_PER_SEC).toFixed(10);
+    }
+  });
+  console.log({"RATIO     ": (stats[0]["original  "] / stats[1]["new lexer "]).toFixed(10)});
+  console.log({"num NAMES ": tokHist["NAME"]});
+  console.log({"total toks": newAns.length});
+  var stats = [];
+  for (var tok in t.times) {
+    var sum = t.times[tok].reduce((a, b) => a + b);
+    var avg = sum / t.times[tok].length;
+    stats.push({name: tok, sum, num: t.times[tok].length, avg});
+  }
+  stats.sort((a, b) => (a.sum - b.sum));
+  stats.forEach((stat) => 
+                console.log(stat.name.padEnd(30), ":",
+                            "sum:", stat.sum.toFixed(10),
+                            "num:", stat.num.toString().padEnd(5),
+                            "avg:", stat.avg.toFixed(10))
+               );
+  
+  // stats.forEach(function(entry) {
+  //   for (var k in entry) {
+  //     console.log(entry);
+  //   }
+  // });
+  // console.log(tokHist);
 
 
   for (var i = 0; i < Math.max(trueAns.length, newAns.length); i++) {
