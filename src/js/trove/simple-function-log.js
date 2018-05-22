42ae36e5aa87e4f788d14e0fc8dd8ceeed841472
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2018-05-22 17:51:49 -0400

    remove table specific code for JSmodule return
    do I need to have anything in here?

--- src/js/trove/simple-function-log.js
+++ src/js/trove/simple-function-log.js
@@ -23,18 +23,13 @@
       return Array(len).join("  ")
     }
 
+    var my_token = ffi.subscribeToFunctionTraces(onFunctionPush, onFunctionPop);
+
     // do we actually want to make a module?
     // Really, I just want to hook into ffi's subscribe
     // and then print things from there...
     return runtime.makeJSModuleReturn({
-      TableAnn : annTable,
-      RowAnn : annRow,
-      makeTable: makeTable,
-      makeRow: makeRow,
-      makeRowFromArray: makeRowFromArray,
-      openTable: openTable,
-      isTable: isTable,
-      isRow: isRow
+      
     },
       {});
   }
