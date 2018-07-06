cebd16f887e68137413df5a9d9882e45a6b041db
Ben Lerner <blerner@ccs.neu.edu>
2018-07-05 23:08:11 -0400

    Trying to delay process.exit until process.stdout and process.stderr have been drained, by using node callbacks on stream.write

--- src/js/base/handalone.js
+++ src/js/base/handalone.js
@@ -15,6 +15,7 @@ requirejs(["pyret-base/js/runtime", "pyret-base/js/post-load-hooks", "pyret-base
   var toLoad = program.toLoad;
   var uris = program.uris;
   var realm = { instantiated: {}, static: {}};
+  var util = require('util'); // This lets us mimic console.log of fancy objects, while using process.stderr.write
 
   var main = toLoad[toLoad.length - 1];
 
@@ -55,18 +56,21 @@ requirejs(["pyret-base/js/runtime", "pyret-base/js/post-load-hooks", "pyret-base
     return runtime.safeCall(function() {
       return toCall.app(checks, getStackP);
     }, function(summary) {
-      if(runtime.isObject(summary)) {
-        process.stdout.write(runtime.getField(summary, "message"));
-        process.stdout.write("\n");
-        var errs = runtime.getField(summary, "errored");
-        var failed = runtime.getField(summary, "failed");
-        if(errs !== 0 || failed !== 0) {
-          process.exit(EXIT_ERROR_CHECK_FAILURES);
+      // We're technically on the Pyret stack right now, but don't need to be.
+      // So, pause the stack and switch off Pyret stack management so that the
+      // use of the callbacks to write (and therefore lack of Pyret return value)
+      // doesn't screw up Pyret's runtime.
+      runtime.pauseStack(function(resumer) {
+        if(runtime.isObject(summary)) {
+          var errs = runtime.getField(summary, "errored");
+          var failed = runtime.getField(summary, "failed");
+          var exitCode = (errs !== 0 || failed !== 0) ? EXIT_ERROR_CHECK_FAILURES : EXIT_SUCCESS;
+          process.stdout.write(runtime.getField(summary, "message"));
+          process.stdout.write("\n",
+                               function() { process.exit(exitCode); });
         }
-        else {
-          process.exit(EXIT_SUCCESS);
-        }
-      }
+        // NOTE: Never calls resumer.resume, because there's nothing to do here beside exit
+      });
     }, "postLoadHooks[main]:render-check-results-stack");
   }
 
@@ -88,12 +92,14 @@ requirejs(["pyret-base/js/runtime", "pyret-base/js/post-load-hooks", "pyret-base
           }
         },
         function(reasonResult) {
+          // This callback is *not* on the Pyret stack, so no need to pause
           if (execRt.isFailureResult(reasonResult)) {
-            console.error("While trying to report that Pyret terminated with an error:\n" + JSON.stringify(res)
-                          + "\nPyret encountered an error rendering that error:\n" + JSON.stringify(reasonResult)
-                          + "\nStack:\n" + JSON.stringify(exnStack)
-                          + "\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack, true));
-            process.exit(EXIT_ERROR_RENDERING_ERROR);
+            process.stderr.write(
+              "While trying to report that Pyret terminated with an error:\n" + JSON.stringify(res)
+                + "\nPyret encountered an error rendering that error:\n" + JSON.stringify(reasonResult)
+                + "\nStack:\n" + JSON.stringify(exnStack)
+                + "\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack, true) + "\n",
+              function() { process.exit(EXIT_ERROR_RENDERING_ERROR); });
           } else {
             execRt.runThunk(
               function() {
@@ -106,28 +112,29 @@ requirejs(["pyret-base/js/runtime", "pyret-base/js/post-load-hooks", "pyret-base
                   execRt.ffi.makeList(res.exn.pyretStack.map(execRt.makeSrcloc)));
               },
               function(printResult) {
+                // This callback is *not* on the Pyret stack, so no need to pause
                 if(execRt.isSuccessResult(printResult)) {
-                  console.error(printResult.result);
-                  console.error("\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack));
-                  process.exit(EXIT_ERROR);
+                  process.stderr.write(util.format(printResult.result));
+                  process.stderr.write("\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack) + "\n",
+                                       function() { process.exit(EXIT_ERROR); });
                 } else {
-                  console.error(
+                  process.stderr.write(
                       "While trying to report that Pyret terminated with an error:\n" + JSON.stringify(res)
                       + "\ndisplaying that error produced another error:\n" + JSON.stringify(printResult)
                       + "\nStack:\n" + JSON.stringify(exnStack)
-                      + "\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack, true));
-                  process.exit(EXIT_ERROR_DISPLAYING_ERROR);
+                      + "\nPyret stack:\n" + execRt.printPyretStack(res.exn.pyretStack, true) + "\n",
+                    function() { process.exit(EXIT_ERROR_DISPLAYING_ERROR); });
                 }
               }, "errordisplay->to-string");
           }
         }, "error->display");
     } else if (res.exn && res.exn.stack) {
-      console.error("Abstraction breaking: Uncaught JavaScript error:\n", res.exn);
-      console.error("Stack trace:\n", res.exn.stack);
-      process.exit(EXIT_ERROR_JS);
+      process.stderr.write("Abstraction breaking: Uncaught JavaScript error:\n" + util.format(res.exn));
+      process.stderr.write("Stack trace:\n" + util.format(res.exn.stack) + "\n",
+                           function() { process.exit(EXIT_ERROR_JS); });
     } else {
-      console.error("Unknown error result: ", res.exn);
-      process.exit(EXIT_ERROR_UNKNOWN);
+      process.stderr.write("Unknown error result: " + util.format(res.exn) + "\n",
+                           function() { process.exit(EXIT_ERROR_UNKNOWN); });
     }
   }
 
@@ -142,30 +149,32 @@ requirejs(["pyret-base/js/runtime", "pyret-base/js/post-load-hooks", "pyret-base
       var message = "Exited with code " + exitCode.toString() + "\n";
       process.stdout.write(message);
     }
-    process.exit(exitCode);
+    process.stdout.write("", function() { process.exit(exitCode); });
   }
 
   function onComplete(result) {
+    // This function is *not* on the Pyret stack, so no need to pause
     if(runtime.isSuccessResult(result)) {
       //console.log("The program completed successfully");
       //console.log(result);
       process.exit(EXIT_SUCCESS);
     }
     else if (runtime.isFailureResult(result)) {
-
       if (runtime.isPyretException(result.exn) && isExit(runtime, result)) {
         processExit(runtime, result.exn.exn);
-      }
-      console.error("The run ended in error:");
-      try {
-        renderErrorMessageAndExit(runtime, result);
-      } catch(e) {
-        console.error("EXCEPTION!", e);
+      } else {
+        process.stderr.write("The run ended in error:\n");
+        try {
+          renderErrorMessageAndExit(runtime, result);
+        } catch(e) {
+          process.stderr.write("EXCEPTION!\n" + util.format(e) + "\n",
+                               function() { process.exit(EXIT_ERROR_JS); });
+        }
       }
     } else {
-      console.error("The run ended in an unknown error: ", result);
-      console.error(result.exn.stack);
-      process.exit(EXIT_ERROR_UNKNOWN);
+      process.stderr.write("The run ended in an unknown error:\n" + util.format(result) + "\n");
+      process.stderr.write(result.exn.stack,
+                           function() { process.exit(EXIT_ERROR_UNKNOWN); });
     }
   }
 
