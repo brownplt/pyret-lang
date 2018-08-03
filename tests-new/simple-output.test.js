256a811a60e9b5ce3e1864c25085d060b3683cc7
Joe Gibbs Politz <joe.politz@gmail.com>
2018-08-03 09:25:20 -0700

    add an explicit timeout to tests

--- tests-new/simple-output.test.js
+++ tests-new/simple-output.test.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
-      const compileProcess = cp.spawnSync("node",
-        [ "tests-new/run-pyret.js", "tests-new/simple-output/print1.arr"], {stdio: 'pipe'});
+const COMPILER_TIMEOUT = 10000; // ms
+const RUN_TIMEOUT = 5000; // ms
 
 describe("testing simple-output programs", () => {
   const files = glob.sync("tests-new/simple-output/*.arr", {});
@@ -13,7 +13,7 @@ describe("testing simple-output programs", () => {
     test(`${f}`, () => {
 
       const compileProcess = cp.spawnSync("node",
-        [ "tests-new/run-pyret.js", f], {stdio: 'pipe'});
+        [ "tests-new/run-pyret.js", f], {stdio: 'pipe', timeout: COMPILER_TIMEOUT});
         
       assert(compileProcess.status === 0, `${compileProcess.stdout}\n${compileProcess.stderr}`);
 
@@ -24,7 +24,7 @@ describe("testing simple-output programs", () => {
       const basename = path.basename(f);
       const dest = glob.sync(`./tests-new/.pyret/compiled/project/tests-new/simple-output/**${basename}.js`)[0];
 
-      const runProcess = cp.spawnSync("node", [dest], {stdio: 'pipe'});
+      const runProcess = cp.spawnSync("node", [dest], {stdio: 'pipe', timeout: RUN_TIMEOUT});
       assert(runProcess.status === 0, `${runProcess.stdout}\n${runProcess.stderr}`);
       assert(runProcess.stdout.indexOf(expect) !== -1, `${runProcess.stdout} should contain ${expect}`);
     });
