40fb2676b847cde63b0e89a58b0f5005d6513234
jGuanZhou <gzjosef@gmail.com>
2018-08-08 13:55:23 -0700

    Immutable version of lists and string-dict

---
+++ src/runtime/lists-immutable.arr.js
@@ -0,0 +1,69 @@
+const { List } = require( 'immutable' );
+
+module.exports = {
+  'list': {
+    'make': function( list ) {
+      return List( list );
+    }
+  },
+  'to-list': function( list ) {
+    return List( list );
+  },
+  'at': function( list, index ) {
+    return list.get( index );
+  },
+  'length': function( list ) {
+    return list.size;
+  },
+  'contains': function( list, elm ) {
+    return list.includes( elm );
+  },
+  'map': function( list, fun ) {
+    return list.map( fun );
+  },
+  'flatMap': function( list, fun ) {
+    return List( list ).flatMap( fun );
+  },
+  'slice': function( list, start, end = list.length ) {
+    return list.slice( start, end );
+  },
+  'push': function( list, elm ) {
+    return list.push( elm );
+  },
+  'filter': function( list, fun ) {
+    return list.filter( fun );
+  },
+  'reduce': function( list, fun, val ) {
+    return list.reduce( fun, val );
+  },
+  'sum': function( list ) {
+    return list.reduce( ( x, y ) => x + y, 0 );
+  },
+  'max': function( list ) {
+    return list.reduce( ( x, y ) => Math.max( x, y ), list[0] )
+  },
+  'range': function( start, end ) {
+    list = List( [] );
+
+    for ( var i = start; i < end; i++ ) {
+      list = list.push( i );
+    }
+
+    return list;
+  },
+  'empty-list': function() {
+    return List( [] );
+  },
+  'concat': function( listA, listB ) {
+    var newList = List( listA );
+
+    for ( var index = 0; index < listB.size; index++ ) {
+      newList = newList.push( listB.get( index ) );
+    }
+    
+    return newList;
+  },
+  'is-list': function( list ) {
+    return List.isList( list );
+  }
+};
---
+++ src/runtime/string-dict-immutable.arr.js
@@ -0,0 +1,56 @@
+const { Map, List } = require( 'immutable' );
+
+module.exports = {
+  'string-dict': {
+    'make': function( list ) {
+      return Map( list[0] );
+    }
+  },
+  'count': function( list ) {
+    var dict = Map();
+
+    for ( var index = 0; index < list.size; index++ ) {
+      var elm = list.get( index );
+
+      if ( dict.has( elm ) ) {
+        dict = dict.set( elm, dict.get( elm ) + 1 );
+      } else {
+        dict = dict.set( elm, 1 );
+      }
+    }
+
+    return dict;
+  },
+  'apply': function( list, fun ) {
+    var dict = Map();
+
+    for ( var index = 0; index < list.size; index++ ) {
+      var elm = list[index];
+
+      dict = dict.set( elm, fun( elm ) );
+    }
+
+    return dict;
+  },
+  'insert': function( dict, key, value ) {
+    return dict.set( key, value );
+  },
+  'size': function( dict ) {
+    return dict.size;
+  },
+  'get': function( dict, elm ) {
+    return dict[elm];
+  },
+  'has-key': function( dict, elm ) {
+    return dict.has( elm );
+  },
+  'keys': function( dict ) {
+    return List( dict ).map( lst => lst[0] );
+  },
+  'values': function( dict ) {
+    return dict.toList();
+  },
+  'is-dict': function( dict ) {
+    return Map.isMap( dict );
+  }
+};
