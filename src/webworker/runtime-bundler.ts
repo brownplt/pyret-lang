a1c62eb13c011e37bc25a23f7f613bee8bd7405e
Alexander Vo <randomcivvy0121@gmail.com>
2019-05-21 19:39:52 -0700

    Change path prefixes for runtime file

--- src/webworker/runtime-bundler.ts
+++ src/webworker/runtime-bundler.ts
@@ -28,8 +28,8 @@ var prewrittenBuiltinsDir = process.argv[2];
 var uncompiledBuiltinsDir = process.argv[3];
 var outputFile = process.argv[4];
 
-var prewrittenObject = walkSync(prewrittenBuiltinsDir, "$PREWRITTEN$");
-var uncompiledObject = walkSync(uncompiledBuiltinsDir, "$UNCOMPILED$");
+var prewrittenObject = walkSync(prewrittenBuiltinsDir, ".prewritten");
+var uncompiledObject = walkSync(uncompiledBuiltinsDir, ".uncompiled");
 
 var fileObject = prewrittenObject.concat(uncompiledObject);
 var fileString = JSON.stringify(fileObject);
