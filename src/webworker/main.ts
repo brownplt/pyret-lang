7d7ee7adace0f5ee77495e24860dbf6c2043679a
Alexander Vo <randomcivvy0121@gmail.com>
2019-06-03 21:36:34 -0700

    Setup a HTML log. NOTE: Need to clear cache + refresh (CTRL + F5) in order for HTML logs to be displayed

--- src/webworker/main.ts
+++ src/webworker/main.ts
@@ -8,6 +8,34 @@ const worker = setup.worker;
 const input = <HTMLInputElement>document.getElementById("program");
 const compile = document.getElementById("compile");
 
+// Setup HTML output
+// NOTE(alex): May need to CTRL + F5 (refresh and clear cache) in order to see HTML logs
+var consoleOutputElement = document.getElementById("consoleOut");
+var oldLog = console.log;
+var consoleOutput = "";
+console.log = function(message) {
+  var outputLine = "[LOG]"; 
+  for (let i = 0; i < arguments.length; i++) {
+    var separator = ",";
+    if (i === arguments.length - 1) {
+      separator = "";
+    }
+
+    var arg = arguments[i];
+    if (typeof arg === "object") {
+      outputLine += JSON.stringify(arg, null, 4) + separator;
+    } else {
+      outputLine += arg + separator;
+    }
+  }
+  outputLine += "\n";
+  
+  consoleOutput += outputLine;
+
+  consoleOutputElement.innerHTML = consoleOutput;
+  oldLog(message, arguments);
+};
+
 compile.onclick = function() {
   fs.writeFileSync("./projects/program.arr", input.value);
   let message = {
