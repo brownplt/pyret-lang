f148d27efc710d7de4b93e19aba9d935a51932f5
Alexander Vo <randomcivvy0121@gmail.com>
2019-06-03 21:58:30 -0700

    Move console.log fill into setup.ts. Add workerLog()

--- src/webworker/setup.ts
+++ src/webworker/setup.ts
@@ -26,3 +26,42 @@ module.exports = {
   BrowserFS: BrowserFS,
   worker: myWorker
 };
+
+// Setup HTML output
+// NOTE(alex): May need to CTRL + F5 (refresh and clear cache) in order to see HTML logs
+var consoleOutputElement = document.getElementById("consoleOut");
+var oldLog = console.log;
+var consoleOutput = "";
+
+var genericLog = function(prefix, ...args: any[]) {
+  var outputLine = prefix;
+  let logArgs = arguments[1];
+  for (let i = 0; i < logArgs.length; i++) {
+    var separator = ",";
+    if (i === logArgs.length - 1) {
+      separator = "";
+    }
+
+    var arg = logArgs[i];
+    if (typeof arg === "object") {
+      outputLine += JSON.stringify(arg, null, 4) + separator;
+    } else {
+      outputLine += arg + separator;
+    }
+  }
+  outputLine += "\n";
+  
+  consoleOutput += outputLine;
+
+  consoleOutputElement.innerHTML = consoleOutput;
+};
+
+console.log = function(message) {
+  genericLog("[LOG]", arguments);
+  oldLog(message, arguments);
+}
+
+var workerLog = function(message) {
+  genericLog("[WORKER]", arguments);
+  oldLog(message, arguments);
+};
