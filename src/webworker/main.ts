74c7738b19ec5bbaa890dab5d90abb916ba6982b
Alexander Vo <randomcivvy0121@gmail.com>
2019-07-17 09:54:58 -0700

    Web compiler no longer uses the builtin-arr-dir option to compile user code.
    Builtin arr files are no longer supported. Precompile them before bundling

--- src/webworker/main.ts
+++ src/webworker/main.ts
@@ -21,7 +21,6 @@ function compileProgram() {
       program: "program.arr",
       "base-dir": "/projects",
       "builtin-js-dir": "/prewritten/",
-      "builtin-arr-dir": "/uncompiled/",
       checks: "none",
     }
   };
