f912006732d39d4564790c5ebe20f60a3c08d835
Michael MacLeod <michaelmmacleod@gmail.com>
2019-07-18 15:56:11 -0700

    Add an interactive file system browser to page.html

--- src/webworker/setup.ts
+++ src/webworker/setup.ts
@@ -21,6 +21,94 @@ BrowserFS.configure({
   }
 });
 
+const theFs = BrowserFS.BFSRequire("fs");
+
+// createFileNode : string -> dom element
+//
+// Creates a list item for the file system viewer. If the
+// item is clicked on it expands to either a list of
+// files (if path refers to a directory), or the contents
+// of a file (if path refers to a file).
+const createFileNode = (path) => {
+  const fileNode = document.createElement("li");
+
+  theFs.stat(path, (err, stats) => {
+    if (err) {
+      throw err;
+    }
+
+    if (stats.isDirectory()) {
+      if (path !== "/") {
+        path = path + "/";
+      }
+
+      fileNode.onclick = (e) => {
+        e.stopPropagation();
+
+        if (fileNode.children.length === 0) {
+          const children = document.createElement("ul");
+          fileNode.appendChild(children);
+
+          theFs.readdir(path, (err, files) => {
+            if (err) {
+              throw err;
+            }
+
+            files.forEach((file) => {
+              const child = createFileNode(path + file);
+              children.appendChild(child);
+            });
+          });
+        } else {
+          while (fileNode.firstChild) {
+            fileNode.removeChild(fileNode.firstChild);
+          }
+          fileNode.innerHTML = path;
+        }
+      };
+    } else if (stats.isFile()) {
+      fileNode.onclick = (e) => {
+        e.stopPropagation();
+
+        if (fileNode.children.length === 0) {
+          const readFileOptions = {
+            encoding: "utf-8",
+          };
+          theFs.readFile(path, readFileOptions, (err, contents) => {
+            if (err) {
+              throw err;
+            }
+
+            const overflowDiv = document.createElement("div");
+            fileNode.appendChild(overflowDiv);
+            overflowDiv.style["overflow-y"] = "scroll";
+            overflowDiv.style["max-height"] = "200px";
+            overflowDiv.style["border"] = "2px solid black";
+            overflowDiv.style["padding"] = "5px";
+            const codeElement = document.createElement("p");
+            overflowDiv.appendChild(codeElement);
+            codeElement.style["white-space"] = "pre-wrap";
+            codeElement.innerHTML = contents;
+          });
+        } else {
+          while (fileNode.firstChild) {
+            fileNode.removeChild(fileNode.firstChild);
+          }
+          fileNode.innerHTML = path;
+        }
+      };
+    }
+  });
+
+  fileNode.innerHTML = path;
+
+  return fileNode;
+};
+
+// create root node for the file system browser.
+const filesystemBrowser = document.getElementById('filesystemBrowser');
+filesystemBrowser.appendChild(createFileNode("/"));
+
 var styler = document.createElement('style');
 styler.type = 'text/css';
 styler.innerHTML = 
