f8e13d3407b0aaf121e7189ace0c8069c20a855e
Michael MacLeod <michaelmmacleod@gmail.com>
2019-07-22 12:15:49 -0700

    Tests: make type checker not run on certain tests
    Tables can't currently be type checked, but we'd like to run tests
    on them. The current testing framework forces all tests to be
    type checked, meaning that any table tests we write would fail. This
    commit makes the type checker ignore test files that have the string
    /no-type-check/ in their filename. For example, the file
    "no-type-check-table-basic-syntax.arr" will not have type checks run
    on it, but the file "paren.arr" will have type checks run on it.

---
+++ tests-new/run-pyret-no-type-check.js
@@ -0,0 +1,22 @@
+const client = require('../src/server/client-lib.js');
+const path = require('path');
+
+client.start({
+  client: {
+    port: "tests-new/pyret-tests.sock",
+    compiler: "build/phaseA/pyret.jarr",
+  },
+  _all: {
+    'local-parley': "tests-new/.pyret"
+  },
+  meta: {
+    norun: true
+  },
+  "pyret-options": {
+    'type-check': false,
+    'checks': 'none',
+    'program': process.argv[2],
+    'builtin-js-dir': 'src/runtime',
+    'base-dir': path.resolve(path.join(__dirname, "/../"))
+  }
+});
