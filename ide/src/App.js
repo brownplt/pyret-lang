e91ae8d77af386f69375493fd10d2f1442f70cc3
Michael MacLeod <michaelmmacleod@gmail.com>
2019-08-13 12:41:27 -0700

    Replace default react project code with ide-specific code

--- ide/src/App.js
+++ ide/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="outer-box">
+          <div id="header">
+              <button id="run" className="prose">
+                  Run
+              </button>
+          </div>
+          <div id="main">
+              <div id="edit-box">
+                  <div id="definitions-container">
+                      <textarea id="definitions-area" className="code"></textarea>
+                  </div>
+                  <div id="separator"></div>
+                  <div id="interactions-container">
+                      <pre id="interactions-area" className="code"></pre>
+                  </div>
+              </div>
+          </div>
+          <div id="footer"> </div>
+      </div>
   );
 }
 
