b682609bce43ce6e4a17ace1bfad58769b0045da
Michael MacLeod <michaelmmacleod@gmail.com>
2019-08-13 15:48:40 -0700

    Create mock pyretCompile and pyretRun functions

--- ide/src/App.js
+++ ide/src/App.js
@@ -19,6 +19,23 @@ const fs = BrowserFS.BFSRequire('fs');
 
 const programCacheFile = '/program-cache.arr';
 
+function pyretCompile(path, callback) {
+
+    // We don't have the infrastructure in place to compile or run Pyret programs at the
+    // moment, so just echo back the path of the file as a placeholder.
+
+    callback({path: path});
+}
+
+function pyretRun(compileSuccess, callback) {
+    const contents = fs.readFileSync(programCacheFile, {encoding: 'utf-8'});
+
+    // We don't have the infrastructure in place to compile or run Pyret programs at the
+    // moment, so just echo back the contents of the file as a placeholder.
+
+    callback({answer: {stringContents: contents}});
+}
+
 class DefinitionsArea extends React.Component {
     constructor(props) {
         super(props);
@@ -48,6 +65,22 @@ class DefinitionsArea extends React.Component {
     };
 }
 
+function isCompileSuccess(x) {
+    if (x.path) {
+        return true;
+    }
+
+    return false;
+}
+
+function isRunSuccess(x) {
+    if (x.answer) {
+        return true;
+    }
+
+    return false;
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -57,11 +90,27 @@ class App extends React.Component {
     };
 
     run = () => {
-        this.setState(
-            {
-                interactions: fs.readFileSync(programCacheFile, {encoding: 'utf-8'})
-            }
-        );
+        pyretCompile(
+            programCacheFile,
+            (compileResult) => {
+                if (isCompileSuccess(compileResult)) {
+                    pyretRun(
+                        compileResult,
+                        (runResult) => {
+                            if (isRunSuccess(runResult)) {
+                                this.setState(
+                                    {
+                                        interactions: runResult.answer.stringContents
+                                    }
+                                );
+                            } else {
+                                // there was an issue at run time
+                            }
+                        })
+                } else {
+                    // there was an issue at compile time.
+                }
+            });
     }
 
     render() {
@@ -79,7 +128,8 @@ class App extends React.Component {
                         <div id="definitions-container">
                             <DefinitionsArea />
                         </div>
-                        <div id="separator"></div>
+                        <div id="separator" >
+                        </div>
                         <div id="interactions-container">
                             <pre id="interactions-area"
                                  className="code">
