09b92044bc1beff98082d60090ebc81b24d8e9d1
Michael MacLeod <michaelmmacleod@gmail.com>
2019-08-13 14:33:05 -0700

    Autosave definitions-area contents upon edit

--- ide/src/App.js
+++ ide/src/App.js
@@ -1,5 +1,50 @@
 import React from 'react';
 import './App.css';
+const BrowserFS = require('browserfs');
+
+BrowserFS.install(window);
+
+BrowserFS.configure(
+    {
+        fs: "LocalStorage"
+    },
+    function(e) {
+        if (e) {
+            throw e;
+        }
+    }
+);
+
+const fs = BrowserFS.BFSRequire('fs');
+
+class DefinitionsArea extends React.Component {
+    constructor(props) {
+        super(props);
+
+        if (!fs.existsSync('/program-cache.arr')) {
+            fs.writeFileSync('/program-cache.arr', "provide *");
+        }
+
+        this.state = {
+            value: fs.readFileSync('/program-cache.arr')
+        }
+    }
+
+    saveDefinitions = (e) => {
+        this.setState({value: e.target.value});
+        fs.writeFileSync('/program-cache.arr', e.target.value);
+    }
+
+    render() {
+        return (
+            <textarea id="definitions-area"
+                      value={this.state.value}
+                      className="code"
+                      onChange={this.saveDefinitions}>
+            </textarea>
+        );
+    };
+}
 
 function App() {
     return (
@@ -12,7 +57,7 @@ function App() {
             <div id="main">
                 <div id="edit-box">
                     <div id="definitions-container">
-                        <textarea id="definitions-area" className="code"></textarea>
+                        <DefinitionsArea/>
                     </div>
                     <div id="separator"></div>
                     <div id="interactions-container">
