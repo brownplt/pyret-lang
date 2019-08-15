c8af8188d2379b6f8a0855e2c0c636c97df12265
Michael MacLeod <michaelmmacleod@gmail.com>
2019-08-15 13:26:43 -0700

    Bundle pyret.jarr with ide build output

--- ide/config/webpack.config.js
+++ ide/config/webpack.config.js
@@ -26,6 +26,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const eslint = require('eslint');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -630,6 +631,9 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new  CopyPlugin([
+        { from: 'pyret.jarr', to: './' },
+      ])
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
