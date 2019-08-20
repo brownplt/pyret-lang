55b87b3ec0d7af4131897d99ffa5f130aadc90aa
Michael MacLeod <michaelmmacleod@gmail.com>
2019-08-20 13:35:01 -0700

    Move path.ts into src/webworker, symlink from ide/src

--- ide/src/path.ts
+++ ide/src/path.ts
@@ -1,11 +1,1 @@
-import {path} from './browserfs-setup';
-
-export const root = "/";
-export const compileBase = path.join(root, "projects");
-export const compileBuiltinJS = path.join(root, "prewritten");
-export const compileProgram = "program.arr";
-export const runBase = path.join(root, "compiled", "project");
-export const runProgram = "program.arr.js";
-export const uncompiled = path.join(root, "uncompiled");
-export const program = path.join(compileBase, compileProgram);
-export const pyretJarr = "pyret.jarr";
+../../src/webworker/path.ts
