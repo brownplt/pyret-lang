e86bcd2bc5aebf4afe5fc1d9414d95c0acfe8d3c
Liye Zhong <liyezhong@dhcp-rhodes-368.redrover.cornell.edu>
2019-09-16 12:22:45 -0400

    add basic urlSound functionn

--- src/runtime/sound.arr.ts
+++ src/runtime/sound.arr.ts
@@ -3,7 +3,7 @@ const RUNTIME = require('./runtime.js');
 
 export const juliet = "helllo world";
 
-export function getArray(path: string) {
+export function getBuffer(path: string):AudioBuffer {
     debugger;
     //@ts-ignore
     var audioCtx = AudioContext();
@@ -16,17 +16,16 @@ export function getArray(path: string) {
     request.open('GET', path, true);
 
     request.responseType = 'arraybuffer';
-    return RUNTIME.pauseStack(function (restarter){
+    return RUNTIME.pauseStack(function (restarter) {
         request.onload = function () {
             var audioData = request.response;
-
+            
             audioCtx.decodeAudioData(audioData, function (buffer) {
                 source.buffer = buffer;
-
                 source.connect(audioCtx.destination);
                 source.loop = true;
                 console.log(buffer.getChannelData(0));
-                restarter.resume(buffer.getChannelData(0));
+                restarter.resume(buffer);
             },
 
                 function (e) {
@@ -36,14 +35,32 @@ export function getArray(path: string) {
         }
 
         request.send();
-        
+
     })
 }
 
-export function makesound(path: string) {
-    return {
-        "$brand": "sound",
-        "play": () => { },
-        "getArray": () => getArray(path)
-    };
+function _makesound(sample_rate: number, data_array: number[]): Sound {
+    const sound = {
+        '$brand': "$sound",
+        'sample-rate': sample_rate,
+        'data-array': data_array,
+        'play': () => playSound()
+    }
+    return sound;
+}
+
+export function playSound() { }
+
+export function _urlSound(path: string): Sound {
+    var buffer = getBuffer(path);
+    var data_array = Array.from(buffer.getChannelData(0));   
+    var sample_rate = buffer.sampleRate;
+    return _makesound(sample_rate, data_array);
+}
+
+interface Sound {
+    '$brand': string,
+    'sample-rate': number,
+    'data-array': number[],
+    'play': () => any
 }
