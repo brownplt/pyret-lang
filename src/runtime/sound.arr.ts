0275666b1775cb008bd8fb6134c386ff89485601
Liye Zhong <liyezhong@dhcp-gs-864.redrover.cornell.edu>
2019-09-24 22:30:28 -0400

    added overlay function and other changes, play function to be updated

--- src/runtime/sound.arr.ts
+++ src/runtime/sound.arr.ts
@@ -3,7 +3,7 @@ const RUNTIME = require('./runtime.js');
 
 export const juliet = "helllo world";
 
-export function getBuffer(path: string):AudioBuffer {
+export function getBuffer(path: string): AudioBuffer {
     debugger;
     //@ts-ignore
     var audioCtx = AudioContext();
@@ -19,7 +19,6 @@ export function getBuffer(path: string):AudioBuffer {
     return RUNTIME.pauseStack(function (restarter) {
         request.onload = function () {
             var audioData = request.response;
-            
             audioCtx.decodeAudioData(audioData, function (buffer) {
                 source.buffer = buffer;
                 source.connect(audioCtx.destination);
@@ -39,28 +38,82 @@ export function getBuffer(path: string):AudioBuffer {
     })
 }
 
-function _makesound(sample_rate: number, data_array: number[]): Sound {
+export function makeSound(sample_rate: number, duration: number, data_array: number[][]): Sound {
     const sound = {
-        '$brand': "$sound",
+        '$brand': "sound",
         'sample-rate': sample_rate,
+        'duration': duration,
         'data-array': data_array,
-        'play': () => playSound()
+        'play': () => playSound(sound)
     }
     return sound;
 }
 
-export function playSound() { }
+export function playSound(sound: Sound) {
+    //@ts-ignore
+    var audioCtx = AudioContext();
+    var source;
+    source = audioCtx.createBufferSource();
+    var frameCount = sound.duration*sound['data-array'][0].length
+    source.buffer = audioCtx.createBuffer(sound['data-array'].length,frameCount,sound['sample-rate']);
+    source.connect(audioCtx.destination);
+    source.start();
+}
 
-export function _urlSound(path: string): Sound {
+export function urlSound(path: string): Sound {
     var buffer = getBuffer(path);
-    var data_array = Array.from(buffer.getChannelData(0));   
+    var numChannel = buffer.numberOfChannels;
+    var data_array = new Array(numChannel);
+    for (var channel = 0; channel < numChannel; channel++) {
+        var channel_array = Array.from(buffer.getChannelData(channel));
+        data_array[channel] = channel_array;
+    }
     var sample_rate = buffer.sampleRate;
-    return _makesound(sample_rate, data_array);
+    var duration = buffer.duration;
+    return makeSound(sample_rate, duration, data_array);
+}
+
+export function overlay(samples: Sound[]): Sound {
+    var numSamples = samples.length;
+    var maxChannels = 0;
+    var maxDuration = 0;
+    var sample_rate = samples[0]['sample-rate'];
+    for (var i = 0; i < numSamples; i++) {
+        if (samples[i]['data-array'].length > maxChannels) {
+            maxChannels = samples[i]['data-array'].length;
+        }
+        if (samples[i].duration > maxDuration) {
+            maxDuration = samples[i].duration;
+        }
+    }
+    var frameCount = maxDuration * sample_rate;
+    var mixed = new Array(maxChannels);
+    console.log(mixed);
+    console.log(frameCount);
+    for (var m = 0; m < maxChannels; m++) {
+        mixed[m] = new Array(frameCount);
+        for (var n = 0; n < frameCount; n++) {
+            mixed[m][n] = 0;
+        }
+    }
+    console.log(mixed);
+    for (var j = 0; j < numSamples; j++) {
+        for (var srcChannel = 0; srcChannel < samples[j]['data-array'].length; srcChannel++) {
+            var _in = samples[j]['data-array'][srcChannel];
+            for (var i = 0; i < _in.length; i++) {
+                console.log(mixed);
+                mixed[srcChannel][i] += _in[i];
+                console.log(mixed);
+            }
+        }
+    }
+    return makeSound(sample_rate, maxDuration, mixed);
 }
 
 interface Sound {
     '$brand': string,
     'sample-rate': number,
-    'data-array': number[],
+    'duration': number,
+    'data-array': number[][],
     'play': () => any
 }
