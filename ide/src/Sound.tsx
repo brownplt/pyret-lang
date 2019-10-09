f4b4b2be656d16d04a74531015ec3b45f64fd253
Andrew Tsakiris <art85@cornell.edu>
2019-10-09 13:58:24 -0400

    implement first iteration of zoom

--- ide/src/Sound.tsx
+++ ide/src/Sound.tsx
@@ -9,9 +9,9 @@ type SoundWidgetState = {
    isMouseDown: boolean,
    startBox: number,
    endBox: number,
-   viewStart: number,
-   viewEnd: number,
-   viewWidth: number
+   startIndex: number, //zoom related
+   endIndex: number,
+   focusDuration: number
 };
 
 export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetState> {
@@ -22,6 +22,8 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
   FPS: number = 30.0;
   source: any;
   audioCtx: any;
+  MIN_ZOOM_BOX_WIDTH: number = 5;
+  MIN_PIXELS_VIEW: number = 40;
   constructor(props : SoundWidgetProps) {
     super(props);
     this.waveformCanvas = React.createRef();
@@ -33,26 +35,26 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
       isMouseDown: false,
       startBox: 0,
       endBox: -1,
-      viewStart: 0,
-      viewEnd: this.WIDTH,
-      viewWidth: this.WIDTH
+      startIndex: 0,
+      endIndex: this.props.sound.duration * this.props.sound['sample-rate'],
+      focusDuration: this.props.sound.duration
     }
   }
 
   playSound = () => {
     const dataArray = this.props.sound['data-array'];
-    const numChannels = dataArray.length;
+   // const numChannels = dataArray.length;
     const timePassed = this.state.progress / this.FPS;
     const duration = this.props.sound.duration - timePassed;
     const sampleRate = this.props.sound['sample-rate'];
     const frameCount = duration * sampleRate;
-    let startIndex = Math.round(timePassed * sampleRate);
-
-    var myArrayBuffer = this.audioCtx.createBuffer(numChannels, frameCount, sampleRate);
+    let startIndex = Math.round((timePassed / this.state.focusDuration) * (this.state.endIndex-this.state.startIndex) + this.state.startIndex);
+    console.log("starting to play at  " + startIndex + ". Endindex=" + this.state.endIndex + " start: " + this.state.startIndex);
+    var myArrayBuffer = this.audioCtx.createBuffer(1, (this.state.focusDuration - timePassed) * sampleRate, sampleRate);
     for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
       var nowBuffering = myArrayBuffer.getChannelData(channel);
       let soundArray = dataArray[channel];
-      for (var i = startIndex; i < soundArray.length; i++) {
+      for (var i = startIndex; i < this.state.endIndex; i++) {
         nowBuffering[i-startIndex] = soundArray[i];
       }
     }
@@ -82,9 +84,27 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     if(this.state.isMouseDown) {
       var rect = e.target.getBoundingClientRect();
       let x = e.clientX - rect.left;
-      console.log("Box from: " + this.state.startBox + " to " + this.state.endBox);
-      
-      this.setState({ isMouseDown: false, viewWidth: this.state.endBox - this.state.startBox, viewStart: this.state.startBox, viewEnd: this.state.endBox, startBox: 0, endBox: -1, });
+      //console.log("Box from: " + this.state.startBox + " to " + this.state.endBox);
+      let startPixel = this.state.startBox; //starting pixel
+      let endPixel = x; //ending pixel
+      if(x < this.state.startBox) {
+        startPixel = x;
+        endPixel = this.state.startBox;
+      }
+      if(endPixel - startPixel >= this.MIN_ZOOM_BOX_WIDTH) {
+        const startIndex = Math.round(this.state.startIndex + (startPixel / this.WIDTH) * this.state.focusDuration * this.props.sound['sample-rate']);
+        const endIndex = Math.round(this.state.startIndex + (endPixel / this.WIDTH) * this.state.focusDuration * this.props.sound['sample-rate']);
+        const focusDuration = (endPixel - startPixel) / this.WIDTH * this.state.focusDuration;
+        console.log("Box:" + startIndex + " to " + endIndex + " duration: " + focusDuration);
+        if(endIndex - startIndex > this.MIN_PIXELS_VIEW) {
+          this.setState({progress: 0, startIndex, endIndex, focusDuration });
+          setTimeout(this.drawWaveForm, 100);
+        }
+
+      }
+      this.setState({ isMouseDown: false, startBox: 0, endBox: -1});
+     
+     
     }
   }
   handleMouseMove = (e:any) => {
@@ -99,7 +119,7 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     var rect = e.target.getBoundingClientRect();
     let x = e.clientX - rect.left
     //console.log(e.clientX - rect.left);
-    let maxProgress = this.FPS * this.props.sound.duration;
+    let maxProgress = this.FPS * this.state.focusDuration;
     let newProg = Math.round((x / this.WIDTH) * (maxProgress));
     
     this.setState({progress: newProg});
@@ -119,7 +139,7 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     context.strokeStyle = "#FF0000";
     context.clearRect(0, 0,this.WIDTH, this.HEIGHT);
     context.beginPath();
-    let lineX = ((this.state.progress/this.FPS / (this.props.sound.duration))) * this.WIDTH;
+    let lineX = ((this.state.progress/this.FPS / (this.state.focusDuration))) * this.WIDTH;
     context.moveTo(lineX, 0);
     context.lineTo(lineX, this.HEIGHT);
     context.stroke();
@@ -131,6 +151,7 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
   }
 
   drawWaveForm = () => {
+    console.log("drawing wave form start: " + this.state.startIndex);
     const canvas = this.waveformCanvas.current;
     const context = canvas.getContext('2d');
     context.lineWidth = 1;
@@ -140,13 +161,14 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     context.moveTo(0, this.HEIGHT / 2.0);
     const channel0 = this.props.sound['data-array'][0]; //first 100 elements for now
    // const channel0 = Array.from({length: 100}, () => Math.random() * 2 - 1);
-    const deltaX = canvas.width / channel0.length;
+    const deltaX = this.WIDTH / (this.state.endIndex - this.state.startIndex);
     let x = 0;
-    channel0.forEach((y:number) => {
-      let y_coord =  (this.HEIGHT / 2 ) - y * (this.HEIGHT / 2);
+    for(let i = this.state.startIndex; i < this.state.endIndex; i++) {
+      let y_coord =  (this.HEIGHT / 2 ) - channel0[i] * (this.HEIGHT / 2);
       context.lineTo(x,y_coord);
       x += deltaX;
-    });
+    }
+   
     context.stroke();  
   }
 
@@ -164,7 +186,7 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
   updateProgress = () => {
     if(this.state.isPlaying) {
       this.setState({progress : this.state.progress + 1});
-      if(this.state.progress / this.FPS > this.props.sound.duration) {
+      if(this.state.progress / this.FPS > this.state.focusDuration) {
         this.setState({progress: 0});
         if(this.state.isPlaying) {
           this.togglePlay();
@@ -180,12 +202,16 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     this.setState({progress: 0});
   }
 
-  
+  handleResetZoom = () => {
+    this.setState({progress: 0, startIndex: 0, endIndex: this.props.sound.duration * this.props.sound['sample-rate'], focusDuration: this.props.sound.duration });
+    setTimeout(this.drawWaveForm, 25);
+  }
   render() {
       return (
           <div style={{ border: "1px solid red"}}>
           <button onClick={this.togglePlay}>{!this.state.isPlaying ? "Play" : "Stop"}</button>
           <button onClick={this.handleReset}>{this.state.progress}</button>
+          <button onClick={this.handleResetZoom}>Reset Zoom </button>
           <div style={{position: "relative", width: this.WIDTH, height: this.HEIGHT}}
             onDoubleClick={this.handleClick}
             onMouseDown={this.handleMouseDown}
