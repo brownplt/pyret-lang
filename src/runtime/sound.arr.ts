c99d2ee09e106dae0b5d329dd31c27bf73afc8ac
soumyamahalakshmi <a.soumyamahalakshmi@gmail.com>
2019-10-21 14:58:05 -0400

    Normalize and examples

--- src/runtime/sound.arr.ts
+++ src/runtime/sound.arr.ts
@@ -1,9 +1,7 @@
-import { Howl, Howler } from 'howler';
 const RUNTIME = require('./runtime.js');
 
-export const juliet = "helllo world";
 
-export function getBuffer(path: string): AudioBuffer {
+export function getBufferFromURL(path: string): AudioBuffer {
     debugger;
     //@ts-ignore
     var audioCtx = AudioContext();
@@ -38,69 +36,22 @@ export function getBuffer(path: string): AudioBuffer {
     })
 }
 
-export function getArray(path: string): Float32Array {
-    //@ts-ignore
-    var audioCtx = AudioContext();
-    var source;
-
-    source = audioCtx.createBufferSource();
-    //@ts-ignore
-    var request = XMLHttpRequest();
-
-    request.open('GET', path, true);
-
-    request.responseType = 'arraybuffer';
-    return RUNTIME.pauseStack(function (restarter) {
-        request.onload = function () {
-            var audioData = request.response;
-            audioCtx.decodeAudioData(audioData, function (buffer) {
-                source.buffer = buffer;
-                source.connect(audioCtx.destination);
-                source.loop = true;
-                restarter.resume(buffer.getChannelData(0));
-            },
-
-                function (e) {
-                    restarter.error(new Error("Error with decoding audio data"));
-                });
-
-        }
-
-        request.send();
-
-    })
+export function getArrayFromSound(sound: Sound): number[][] {
+    return sound['data-array'];
 }
-
-export function inputArray(soundArray: Float32Array, sampleRate: number, duration: number):AudioBuffer {
-    //@ts-ignore
-    var audioCtx = AudioContext();
-    var frameCount = duration * sampleRate;
-    var numChannels = 1;
-    var myArrayBuffer = audioCtx.createBuffer(numChannels, frameCount, sampleRate);
-    console.log(myArrayBuffer);
-    var nowBuffering = myArrayBuffer.getChannelData(0);
-        for (var i = 0; i < myArrayBuffer.length; i++) {
-            nowBuffering[i] = soundArray[i];
-            console.log(nowBuffering);
-        }
-    return myArrayBuffer;
- }
 
 export function makeSound(sample_rate: number, duration: number, data_array: number[][]): Sound {
     const sound = {
         '$brand': "sound",
         'sample-rate': sample_rate,
         'duration': duration,
-        'data-array': data_array,
-        'play': () => {}
+        'data-array': data_array
     }
     return sound;
 }
 
-
-
-export function urlSound(path: string): Sound {
-    var buffer = getBuffer(path);
+export function getSoundFromURL(path: string): Sound {
+    var buffer = getBufferFromURL(path);
     var numChannel = buffer.numberOfChannels;
     var data_array = new Array(numChannel);
     for (var channel = 0; channel < numChannel; channel++) {
@@ -112,7 +63,51 @@ export function urlSound(path: string): Sound {
     return makeSound(sample_rate, duration, data_array);
 }
 
+export function getSoundFromAudioBuffer(buffer: AudioBuffer): Sound {
+    var numChannel = buffer.numberOfChannels;
+    var data_array = new Array(numChannel);
+    for (var channel = 0; channel < numChannel; channel++) {
+        var channel_array = Array.from(buffer.getChannelData(channel));
+        data_array[channel] = channel_array;
+    }
+    var sample_rate = buffer.sampleRate;
+    var duration = buffer.duration;
+    return makeSound(sample_rate, duration, data_array);
+}
+
+export function createSound(channels: number, sample_rate: number, duration: number, data_array: number[][]) {
+    //@ts-ignore
+    var audioCtx = AudioContext();
+    var myArrayBuffer = audioCtx.createBuffer(channels, duration, sample_rate);
+
+    for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+        // This gives us the actual array that contains the data
+        var nowBuffering = myArrayBuffer.getChannelData(channel);
+        for (var i = 0; i < myArrayBuffer.length; i++) {
+            nowBuffering[i] = data_array[channel][i];
+        }
+    }
+    var source = audioCtx.createBufferSource();
+    source.buffer = myArrayBuffer;
+    source.connect(audioCtx.destination);
+    return makeSound(sample_rate, duration, data_array);
+}
+
+function checkSampleRate(samples: Sound[]): boolean {
+    var i = 0
+    var sample_rate = samples[i]["sample-rate"];
+    for (var i = 1; i < samples.length; i++) {
+        if (samples[i]["sample-rate"] != sample_rate) {
+            return false;
+        }
+    }
+    return true;
+}
+
 export function overlay(samples: Sound[]): Sound {
+    if (!checkSampleRate(samples)) {
+        throw new Error("samples rates not equal for all samples");
+    }
     var numSamples = samples.length;
     var maxChannels = 0;
     var maxDuration = 0;
@@ -127,32 +122,95 @@ export function overlay(samples: Sound[]): Sound {
     }
     var frameCount = maxDuration * sample_rate;
     var mixed = new Array(maxChannels);
-    console.log(mixed);
-    console.log(frameCount);
     for (var m = 0; m < maxChannels; m++) {
         mixed[m] = new Array(frameCount);
         for (var n = 0; n < frameCount; n++) {
             mixed[m][n] = 0;
         }
     }
-    console.log(mixed);
     for (var j = 0; j < numSamples; j++) {
         for (var srcChannel = 0; srcChannel < samples[j]['data-array'].length; srcChannel++) {
             var _in = samples[j]['data-array'][srcChannel];
             for (var i = 0; i < _in.length; i++) {
-                console.log(mixed);
                 mixed[srcChannel][i] += _in[i];
-                console.log(mixed);
             }
         }
     }
-    return makeSound(sample_rate, maxDuration, mixed);
+    return createSound(mixed.length, sample_rate, maxDuration, mixed);
 }
+
+export function concat(samples: Sound[]): Sound {
+    if (!checkSampleRate(samples)) {
+        throw new Error("samples rates not equal for all samples");
+    }
+    var numSamples = samples.length;
+    var maxChannels = 0;
+    var totalDuration = 0;
+    var sample_rate = samples[0]['sample-rate'];
+    for (var i = 0; i < numSamples; i++) {
+        if (samples[i]['data-array'].length > maxChannels) {
+            maxChannels = samples[i]['data-array'].length;
+        }
+        totalDuration += samples[i].duration;
+    }
+    var frameCount = totalDuration * sample_rate;
+    var mixed = new Array(maxChannels);
+    for (var m = 0; m < maxChannels; m++) {
+        mixed[m] = new Array(frameCount);
+        for (var n = 0; n < frameCount; n++) {
+            mixed[m][n] = 0;
+        }
+    }
+    for (var j = 0; j < numSamples; j++) {
+        var fc = samples[j].duration * sample_rate;
+        for (var srcChannel = 0; srcChannel < samples[j]['data-array'].length; srcChannel++) {
+            var _in = samples[j]['data-array'][srcChannel];
+            for (var i = 0; i < _in.length; i++) {
+                mixed[srcChannel][j * fc + i] = _in[i];
+            }
+        }
+    }
+    return createSound(mixed.length, sample_rate, totalDuration, mixed);
+}
+
+export function setPlaybackSpeed(sample: Sound, rate: number): Sound {
+    var sample_rate = sample['sample-rate'];
+    var duration = sample.duration;
+    var arr = sample['data-array'];
+    var new_sample_rate = sample_rate * rate;
+    var new_dur = duration/rate;
+    return createSound(arr.length, new_sample_rate, new_dur, arr);
+}
+
+export function shorten(sample: Sound, start: number, end: number): Sound {
+    if (end > sample.duration || start > sample.duration || end < start) {
+        throw new Error("invalid start or end");
+    }
+    var sample_rate = sample['sample-rate'];
+    var dur = sample.duration;
+    var arr = sample['data-array'];
+    var new_arr = new Array(arr.length);
+    for (var channel = 0; channel < arr.length; channel++) {
+        new_arr[channel] = arr[channel].slice(start * sample_rate, end * sample_rate);
+        return makeSound(sample_rate, end - start, new_arr);
+    }
+}
+
+export function denormalizeSound(audioBuffer: AudioBuffer): Sound {
+    //@ts-ignore
+    var audioCtx = AudioContext();
+    var convolver = audioCtx.createConvolver();
+    
+    convolver.normalize = false;
+    convolver.buffer = audioBuffer;
+    
+    return getSoundFromAudioBuffer(convolver.buffer);
+}
+
 
 interface Sound {
     '$brand': string,
     'sample-rate': number,
     'duration': number,
-    'data-array': number[][],
-    'play': () => any
+    'data-array': number[][]
 }
