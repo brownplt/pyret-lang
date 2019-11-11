b98e357ba981fa42f8137a6cbf8ec4ac28dac3bf
Andrew Tsakiris <art85@cornell.edu>
2019-11-11 14:25:53 -0500

    clean up implementation of file download

--- ide/src/Sound.tsx
+++ ide/src/Sound.tsx
@@ -283,16 +283,29 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
   }
 
   createWav = () => {
+   
+
     let dataArray = this.props.sound['data-array'];
     var numOfChan = dataArray.length,
-        length = dataArray[0].length * numOfChan * 2 + 44,
-        buffer = new ArrayBuffer(length),
+        headerBytes = 44, //equal to number of bytes set below
+        totalBytes = dataArray[0].length * numOfChan * 2 + headerBytes,
+        buffer = new ArrayBuffer(totalBytes),
         view = new DataView(buffer),
-        channels = [], i, sample,
-        offset = 0,
-        pos = 0;
+        currentSampleIndex = 0,
+        currentByteOffset = 0;
+    
+    function setUint16(data: any) {
+      view.setUint16(currentByteOffset, data, true);
+      currentByteOffset += 2;
+    }
+
+    function setUint32(data: any) {
+      view.setUint32(currentByteOffset, data, true);
+      currentByteOffset += 4;
+    }
+
     setUint32(0x46464952);                         // "RIFF"
-    setUint32(length - 8);                         // file length - 8
+    setUint32(totalBytes - 8);                         // file length - 8
     setUint32(0x45564157);                         // "WAVE"
 
     setUint32(0x20746d66);                         // "fmt " chunk
@@ -305,31 +318,19 @@ export class SoundWidget extends React.Component<SoundWidgetProps, SoundWidgetSt
     setUint16(16);                                 // 16-bit (hardcoded in this demo)
 
     setUint32(0x61746164);                         // "data" - chunk
-    setUint32(length - pos - 4);                   // chunk length
+    setUint32(totalBytes - currentByteOffset - 4);                   // chunk length
 
-    for(i = 0; i < dataArray.length; i++){
-      channels.push(dataArray[i]);
-    }
-    while(pos < length) {
-      for(i = 0; i < numOfChan; i++) {             // interleave channels
-        sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
-        sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
-        view.setInt16(pos, sample, true);          // write 16-bit sample
-        pos += 2;
+    while(currentByteOffset < totalBytes) {
+      for(let i = 0; i < numOfChan; i++) {             // interleave channels
+        let sample = Math.max(-1, Math.min(1, dataArray[i][currentSampleIndex])); // clamp to [-1, 1]
+        //sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
+        sample = (sample < 0 ? sample * 32768 : sample * 32767);
+        view.setInt16(currentByteOffset, sample, true);          // write 16-bit sample
+        currentByteOffset += 2;
       }
-      offset++                                     // next source sample
+      currentSampleIndex += 1;                                     // next source sample
     }
     return new Blob([buffer], {type: "audio/wav"});
-
-    function setUint16(data: any) {
-      view.setUint16(pos, data, true);
-      pos += 2;
-    }
-
-    function setUint32(data: any) {
-      view.setUint32(pos, data, true);
-      pos += 4;
-    }
   }
 
 
