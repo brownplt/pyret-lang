812060129d6d1a8f28fec86598f475fbf2314cea
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 14:41:46 -0700

    Fix FontSize.tsx style

--- ide/src/FontSize.tsx
+++ ide/src/FontSize.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 
 type FontSizeProps = {
-    onIncrease: () => void,
-    onDecrease: () => void,
-    onReset: () => void,
-    size: number,
+  onIncrease: () => void,
+  onDecrease: () => void,
+  onReset: () => void,
+  size: number,
 };
 
-type FontSizeState = {};
-
-export class FontSize extends React.Component<FontSizeProps, FontSizeState> {
-    render() {
-        return (
-            <div className="font-size-options">
-                <button className="font-minus"
-                        onClick={this.props.onDecrease}>
-                    -
-                </button>
-                <button className="font-label"
-                        onClick={this.props.onReset}>
-                    Font ({this.props.size} px)
-                </button>
-                <button className="font-plus"
-                        onClick={this.props.onIncrease}>
-                    +
-                </button>
-            </div>
-        );
-    }
+export default function FontSize({
+  onIncrease, onDecrease, onReset, size,
+}: FontSizeProps) {
+  return (
+    <div className="font-size-options">
+      <button
+        className="font-minus"
+        onClick={onDecrease}
+        type="button"
+      >
+        -
+      </button>
+      <button
+        className="font-label"
+        onClick={onReset}
+        type="button"
+      >
+        Font (
+        {size}
+        {' '}
+        px)
+      </button>
+      <button
+        className="font-plus"
+        onClick={onIncrease}
+        type="button"
+      >
+        +
+      </button>
+    </div>
+  );
 }
