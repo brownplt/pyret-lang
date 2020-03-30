8009d0b3ee62e47e1a6aea057d138542d36bf9b4
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 13:14:22 -0700

    Fix App.tsx style

--- ide/src/App.tsx
+++ ide/src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Editor from './Editor';
+import { Editor } from './Editor';
 import { store } from './store';
 
-type AppProps = {};
-type AppState = {};
-
-class App extends React.Component<any, any> {
-    render() {
-        return (
-            <Provider store={store}>
-                <Editor/>
-            </Provider>
-        );
-    };
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Editor />
+    </Provider>
+  );
 }
-
-export default App;
