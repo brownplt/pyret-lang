8c13e0ddca788fa430d3c944fe425abc1c30a834
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 16:45:49 -0700

    Fix InteractionError.tsx style

--- ide/src/InteractionError.tsx
+++ ide/src/InteractionError.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 
 export type InteractionErrorProps = {
-    fontSize: number,
+  fontSize: number,
 };
 
-export type InteractionErrorState = {};
-
-export class InteractionError extends React.Component<InteractionErrorProps, InteractionErrorState> {
-    render() {
-        return (
-            <div className="interaction-error">
-                <p style={{
-                    fontSize: this.props.fontSize,
-                    padding: 0,
-                    margin: 0,
-                }}>
-                    {this.props.children}
-                </p>
-            </div>
-        );
-    }
+export default function InteractionError({ fontSize, children }: InteractionErrorProps) {
+  return (
+    <div className="interaction-error">
+      <p style={{
+        fontSize,
+        padding: 0,
+        margin: 0,
+      }}
+      >
+        {children}
+      </p>
+    </div>
+  );
 }
