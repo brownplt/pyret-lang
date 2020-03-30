5e7137a9bbd7b01b1bbca2eed0a1f53a2c40c27f
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 12:59:50 -0700

    Fix Interaction.tsx style

--- ide/src/Interaction.tsx
+++ ide/src/Interaction.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import {RenderedValue} from './RenderedValue';
+import RenderedValue from './RenderedValue';
 
 type InteractionProps = {
-    name: string;
-    value: any;
+  name: string;
+  value: any;
 };
 
-type InteractionState = {};
+export default function Interaction({ name, value }: InteractionProps) {
+  if (name === '$checks' || name === '$answer') {
+    return null;
+  }
 
-export class Interaction extends React.Component<InteractionProps, InteractionState> {
-    render() {
-        if (this.props.name === "$checks" || this.props.name === "$answer") {
-            return null;
-        }
-
-        return (
-            <div className="interaction">
-                {this.props.name !== "" ?
-                <pre className="interaction-identifier">
-                    {this.props.name} =&nbsp;
-                </pre> : null}
-                <RenderedValue value={this.props.value}></RenderedValue>
-            </div>
-        )
-    };
+  return (
+    <div className="interaction">
+      {name !== '' ? (
+        <pre className="interaction-identifier">
+          {name}
+          {' '}
+          =&nbsp;
+        </pre>
+      ) : null}
+      <RenderedValue value={value} />
+    </div>
+  );
 }
