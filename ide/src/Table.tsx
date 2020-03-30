c2b9dda235c135557c8dbe7c0927d62b93294f64
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 13:09:55 -0700

    Fix Table.tsx style

--- ide/src/Table.tsx
+++ ide/src/Table.tsx
@@ -9,38 +9,32 @@ type TableWidgetProps = {
   rows: any[][];
 };
 
-type TableWidgetState = {};
-
-export default class TableWidget extends React.Component<TableWidgetProps, TableWidgetState> {
-  render() {
-    const { rows } = this.props;
-    const columns = this.props.headers.map(
-      (header: string, index: number) => {
-        return {
-          id: header,
-          Header: header,
-          accessor: (row: any) => {
-            return this.props.htmlify(row[index]);
-          }
-        };
-    });
-    const maxRowsPerPage = 5;
-    const showOptions = this.props.rows.length > maxRowsPerPage;
-    const defaultPageSize = showOptions ? maxRowsPerPage : this.props.rows.length;
-    return (
-      <div className="table-container">
-        <CopyToClipboard text={rows.map((d) => d.join("\t")).join("\n")}>
-          <div className="table-copy">
-      &#128203;
-          </div>
-        </CopyToClipboard>
-        <ReactTable data={rows}
-          columns={columns}
-          showPagination={showOptions}
-          pageSize={defaultPageSize}
-          showPageSizeOptions={false}
-          filterable={showOptions}/>
-      </div>
-    );
-  }
+export default function TableWidget({ htmlify, headers, rows }: TableWidgetProps) {
+  const columns = headers.map(
+    (header: string, index: number) => ({
+      id: header,
+      Header: header,
+      accessor: (row: any) => htmlify(row[index]),
+    }),
+  );
+  const maxRowsPerPage = 5;
+  const showOptions = rows.length > maxRowsPerPage;
+  const defaultPageSize = showOptions ? maxRowsPerPage : rows.length;
+  return (
+    <div className="table-container">
+      <CopyToClipboard text={rows.map((d) => d.join('\t')).join('\n')}>
+        <div className="table-copy">
+          &#128203;
+        </div>
+      </CopyToClipboard>
+      <ReactTable
+        data={rows}
+        columns={columns}
+        showPagination={showOptions}
+        pageSize={defaultPageSize}
+        showPageSizeOptions={false}
+        filterable={showOptions}
+      />
+    </div>
+  );
 }
