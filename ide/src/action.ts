49e7e43210e0ef4d63bd43f812a849cabb92193c
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 13:12:17 -0700

    Fix action.ts style

--- ide/src/action.ts
+++ ide/src/action.ts
@@ -1,37 +1,39 @@
-import { EditorMode } from "./state";
-import { LintFailure } from "./state";
+import { EditorMode, LintFailure } from './state';
 
 export type Action =
-  ({ type: "setEditorMode", mode: EditorMode }
-   | { type: "finishSetup" }
-   | { type: "finishCreateRepl" }
-   | { type: "queueRun" }
-   | { type: "compile" }
-   | { type: "run" }
-   | { type: "finishRun" }
-   | { type: "stop" }
-   | { type: "runQueued" }
-   | { type: "compile" }
-   | { type: "compileFailure", errors: string[] }
-   | { type: "runFailure", errors: string[] }
-   | { type: "lintFailure", lintFailure: LintFailure }
-   | { type: "lintSuccess", lintSuccess: { name: string }}
-   | { type: "compileSuccess" }
-   | { type: "runFinished", result: any }
-   | { type: "updateRunner", runner: any }
-   | { type: "beginStartup" }
-   | { type: "startupCompleted" }
-   | { type: "runStarted" }
-   | { type: "updateContents", contents: string }
-   | { type: "updateChunkContents", index: number, contents: string }
-   | { type: "traverseUp", path: string }
-   | { type: "traverseDown", path: string }
-   | { type: "expandChild", path: string });
+  ({ type: 'setEditorMode', mode: EditorMode }
+  | { type: 'finishSetup' }
+  | { type: 'finishCreateRepl' }
+  | { type: 'queueRun' }
+  | { type: 'compile' }
+  | { type: 'run' }
+  | { type: 'finishRun' }
+  | { type: 'stop' }
+  | { type: 'runQueued' }
+  | { type: 'compile' }
+  | { type: 'compileFailure', errors: string[] }
+  | { type: 'runFailure', errors: string[] }
+  | { type: 'lintFailure', lintFailure: LintFailure }
+  | { type: 'lintSuccess', lintSuccess: { name: string }}
+  | { type: 'compileSuccess' }
+  | { type: 'runFinished', result: any }
+  | { type: 'updateRunner', runner: any }
+  | { type: 'beginStartup' }
+  | { type: 'startupCompleted' }
+  | { type: 'runStarted' }
+  | { type: 'updateContents', contents: string }
+  | { type: 'updateChunkContents', index: number, contents: string }
+  | { type: 'traverseUp', path: string }
+  | { type: 'traverseDown', path: string }
+  | { type: 'expandChild', path: string });
 
-export type ActionType = Action["type"];
+export type ActionType = Action['type'];
 
 export type ActionOfType<T extends ActionType> = Extract<Action, { type: T }>;
 
-export function isActionType<K extends ActionType>(actionType: K, action: Action): action is ActionOfType<K> {
+export function isActionType<K extends ActionType>(
+  actionType: K,
+  action: Action,
+): action is ActionOfType<K> {
   return actionType === action.type;
 }
