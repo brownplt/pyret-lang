b73741db398b98364749f757a79edee6fdf7a9ed
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 17:32:21 -0700

    Fix style in store.ts

--- ide/src/store.ts
+++ ide/src/store.ts
@@ -1,67 +1,69 @@
 import { createStore } from 'redux';
-import { ideApp } from './reducer';
+import ideApp from './reducer';
 import { CompileState, EditorMode, CHUNKSEP } from './state';
 import * as control from './control';
 
-export const store = createStore(
+const store = createStore(
   ideApp,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+export default store;
 
 function handleLog(message: string): void {
   console.log(message);
 }
 
 function handleSetupFinished(): void {
-  store.dispatch({ type: "finishSetup"} )
+  store.dispatch({ type: 'finishSetup' });
 }
 
 function handleCompileFailure(errors: string[]): void {
   store.dispatch({
-    type: "compileFailure",
-    errors
+    type: 'compileFailure',
+    errors,
   });
 }
 
 function handleRuntimeFailure(errors: string[]): void {
   store.dispatch({
-    type: "runFailure",
-    errors
-  })
+    type: 'runFailure',
+    errors,
+  });
 }
 
 function handleLintFailure(lintFailure: { name: string, errors: string[] }): void {
   store.dispatch({
-    type: "lintFailure",
-    lintFailure
-  })
+    type: 'lintFailure',
+    lintFailure,
+  });
 }
 
 function handleLintSuccess(lintSuccess: { name: string }): void {
   store.dispatch({
-    type: "lintSuccess",
-    lintSuccess
-  })
+    type: 'lintSuccess',
+    lintSuccess,
+  });
 }
 
 function handleCompileSuccess(): void {
   store.dispatch({
-    type: "compileSuccess"
+    type: 'compileSuccess',
   });
 }
 
 function handleCreateReplSuccess(): void {
   store.dispatch({
-    type: "finishCreateRepl"
-  })
+    type: 'finishCreateRepl',
+  });
 }
 
 function handleCompileInteractionSuccess(): void {
-  console.log("compile interaction success (nyi)");
+  console.log('compile interaction success (nyi)');
 }
 
 function handleCompileInteractionFailure(): void {
-  console.log("compile interaction failure (nyi)");
+  console.log('compile interaction failure (nyi)');
 }
 
 store.subscribe(() => {
@@ -70,19 +72,19 @@ store.subscribe(() => {
   if (state.needLoadFile && state.currentFile !== undefined) {
     if (state.editorMode === EditorMode.Text) {
       store.dispatch({
-        type: "updateContents",
-        contents: control.openOrCreateFile(state.currentFile)
+        type: 'updateContents',
+        contents: control.openOrCreateFile(state.currentFile),
       });
     } else if (state.editorMode === EditorMode.Chunks) {
       const fileContents = control.openOrCreateFile(state.currentFile);
       const chunks = fileContents.split(CHUNKSEP);
-      console.log("SPLIT CHUNKS", chunks);
-      for (let i = 0; i < chunks.length; i++) {
+      console.log('SPLIT CHUNKS', chunks);
+      for (let i = 0; i < chunks.length; i += 1) {
         store.dispatch({
-          type: "updateChunkContents",
+          type: 'updateChunkContents',
           index: i,
-          contents: chunks[i]
-        })
+          contents: chunks[i],
+        });
       }
     }
   }
@@ -101,7 +103,7 @@ store.subscribe(() => {
         handleCompileInteractionSuccess,
         handleCompileInteractionFailure,
       );
-      store.dispatch({ type: "startupCompleted" });
+      store.dispatch({ type: 'startupCompleted' });
       return;
     case CompileState.ChunkNeedsRepl:
       control.createRepl();
@@ -112,37 +114,41 @@ store.subscribe(() => {
         if (state.editorMode === EditorMode.Text) {
           control.fs.writeFileSync(
             state.currentFile,
-            state.currentFileContents);
+            state.currentFileContents,
+          );
         } else if (state.editorMode === EditorMode.Chunks) {
           if (state.chunks !== undefined) {
             control.fs.writeFileSync(
               state.currentFile,
-              state.chunks.join(CHUNKSEP));
+              state.chunks.join(CHUNKSEP),
+            );
           }
         }
         control.compile(
           parsed.dir,
           parsed.base,
-          state.typeCheck);
-        store.dispatch({ type: "compile" });
+          state.typeCheck,
+        );
+        store.dispatch({ type: 'compile' });
       }
       return;
     case CompileState.NeedsRun:
-      store.dispatch({ type: "runStarted" });
+      store.dispatch({ type: 'runStarted' });
       control.run(
         control.path.runBase,
         control.path.runProgram,
         (runResult: any) => {
-          store.dispatch({ type: "runFinished", result: runResult });
+          store.dispatch({ type: 'runFinished', result: runResult });
         },
         (runner: any) => {
-          store.dispatch({ type: "updateRunner", runner });
+          store.dispatch({ type: 'updateRunner', runner });
         },
-        control.backend.RunKind.Async);
+        control.backend.RunKind.Async,
+      );
+      break;
     default:
-      return;
   }
 });
 
-store.dispatch({ type: "beginStartup" });
-store.dispatch({ type: "expandChild", path: "/projects/program.arr" });
+store.dispatch({ type: 'beginStartup' });
+store.dispatch({ type: 'expandChild', path: '/projects/program.arr' });
