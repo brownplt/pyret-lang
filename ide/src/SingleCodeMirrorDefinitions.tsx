c18fd34224b7a67910593543f62a673325e90e7b
Michael MacLeod <michaelmmacleod@gmail.com>
2020-03-30 17:26:19 -0700

    Fix style in single code mirror defs

--- ide/src/SingleCodeMirrorDefinitions.tsx
+++ ide/src/SingleCodeMirrorDefinitions.tsx
@@ -6,63 +6,80 @@ import 'pyret-codemirror-mode/css/pyret.css';
 // pyret-codemirror-mode/mode/pyret.js expects window.CodeMirror to exist and
 // to be bound to the 'codemirror' import.
 import * as RawCodeMirror from 'codemirror';
+
 (window as any).CodeMirror = RawCodeMirror;
 require('pyret-codemirror-mode/mode/pyret');
 
 
-type SingleCodeMirrorDefinitionsProps = {
+type Props = {
   onEdit: (s: string) => void,
   highlights: number[][],
   text: string
 };
-type SingleCodeMirrorDefinitionsState = {
+type State = {
   editor: CodeMirror.Editor | null
 };
 
-export class SingleCodeMirrorDefinitions extends React.Component<SingleCodeMirrorDefinitionsProps, SingleCodeMirrorDefinitionsState> {
-
-  constructor(props : SingleCodeMirrorDefinitionsProps) { super(props); this.state = { editor: null }; }
+export default class SingleCodeMirrorDefinitions extends React.Component<Props, State> {
+  constructor(props : Props) {
+    super(props); this.state = { editor: null };
+  }
 
   componentDidUpdate() {
-    if(this.state.editor !== null) {
-      if (this.props.highlights.length > 0) {
-          for (let i = 0; i < this.props.highlights.length; i++) {
-              this.state.editor.getDoc().markText(
-                  { line: this.props.highlights[i][0] - 1,
-                      ch: this.props.highlights[i][1] },
-                  { line: this.props.highlights[i][2] - 1,
-                      ch: this.props.highlights[i][3] },
-                  { className: "styled-background-error" });
-          }
+    const { editor } = this.state;
+    const { highlights } = this.props;
+
+    if (editor !== null) {
+      if (highlights.length > 0) {
+        for (let i = 0; i < highlights.length; i += 1) {
+          editor.getDoc().markText(
+            {
+              line: highlights[i][0] - 1,
+              ch: highlights[i][1],
+            },
+            {
+              line: highlights[i][2] - 1,
+              ch: highlights[i][3],
+            },
+            { className: 'styled-background-error' },
+          );
+        }
       } else {
-          const marks = this.state.editor.getDoc().getAllMarks();
-          for (let i = 0; i < marks.length; i++) {
-              marks[i].clear();
-          }
+        const marks = editor.getDoc().getAllMarks();
+        for (let i = 0; i < marks.length; i += 1) {
+          marks[i].clear();
+        }
       }
     }
   }
-  onChange = (editor: CodeMirror.Editor, data: CodeMirror.EditorChange, value: string): void => {
-    this.setState({ editor: editor });
 
-    for ( var i = 0; i < editor.getDoc().getAllMarks().length; i++) {
-        editor.getDoc().getAllMarks()[i].clear();
+  onChange = (editor: CodeMirror.Editor, data: CodeMirror.EditorChange, value: string): void => {
+    const { onEdit } = this.props;
+
+    this.setState({ editor });
+
+    for (let i = 0; i < editor.getDoc().getAllMarks().length; i += 1) {
+      editor.getDoc().getAllMarks()[i].clear();
     }
-    this.props.onEdit(value);
-  }
+    onEdit(value);
+  };
 
 
   render() {
-    return (<CodeMirror value={this.props.text}
-      options={{
-        mode: 'pyret',
-        theme: 'default',
-        lineNumbers: true,
-        lineWrapping: true,
-      }}
-      onChange={this.onChange}
-      autoCursor={false}>
-    </CodeMirror>)
+    const { text } = this.props;
+
+    return (
+      <CodeMirror
+        value={text}
+        options={{
+          mode: 'pyret',
+          theme: 'default',
+          lineNumbers: true,
+          lineWrapping: true,
+        }}
+        onChange={this.onChange}
+        autoCursor={false}
+      />
+    );
   }
 }
-
