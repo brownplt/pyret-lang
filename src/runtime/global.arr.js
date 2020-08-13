aabbc386432fec23d1a6dff2c231f16cd2935cf1
Michael MacLeod <michaelmmacleod@gmail.com>
2020-08-13 16:13:36 -0700

    Add hack to stop pyret compiler dependency tracing on option

--- src/runtime/global.arr.js
+++ src/runtime/global.arr.js
@@ -32,13 +32,20 @@ function timeNow() {
 
 var realMakeReactor = null;
 
+
+// (Big Hack): Alias for require to stop the Pyret compiler's dependency
+// tracing. Needed because requiring option introduces a circular dependency
+// with runtime-arr-stage-1.
+const untracedRequire = require;
+
 function makeSome(v) {
-  const option = require('./option.arr.js');
+  const option = untracedRequire('./option.arr.js');
   return option.some(v);
 }
 
 function makeNone() {
-  const option = require('./option.arr.js');
+  const untracedRequire = require;
+  const option = untracedRequire('./option.arr.js');
   return option.none;
 }
 
