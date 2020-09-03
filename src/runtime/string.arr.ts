6c1e506825740563eb4d06a427385999902dd791
Alexander Vo <randomcivvy0121@gmail.com>
2020-09-02 18:26:50 -0700

    Whitespace formatting

--- src/runtime/string.arr.ts
+++ src/runtime/string.arr.ts
@@ -2,44 +2,44 @@ let NUMBER = require("./js-numbers.js");
 let OPTION = require("./option.arr.js");
 
 function stringToNumber(s: string): any {
-  var result = NUMBER['fromString'](s);
-  if (result === false) {
-    return OPTION['none'];
-  } else {
-    return OPTION['some'](result);
-  }
+    var result = NUMBER['fromString'](s);
+    if (result === false) {
+        return OPTION['none'];
+    } else {
+        return OPTION['some'](result);
+    }
 }
 
 function numToString(n) {
-  return String(n);
+    return String(n);
 }
 
 module.exports = {
-  'length': function( str ) {
-    return str.length;
-  },
-  'string-to-lower': function( str ) {
-    return str.toLowerCase();
-  },
-  'concat': function( strA, strB ) {
-    return strA.concat( strB );
-  },
-  'substring': function( str, start, end ) {
-    return str.substring( start, end );
-  },
-  'charAt': function( str, index ) {
-    return str.charAt( index );
-  },
-  'split': function( str, separator ) {
-    return str.split( separator );
-  },
-  'split-pattern': function( str, pattern ) {
-    return str.split( RegExp( pattern ) );
-  },
-  'match': function( str, pattern ) {
-    return str.match( RegExp( pattern ) );
-  },
-  "string-to-number": stringToNumber,
-  'num-to-string': numToString,
-  'num-to-string-digits': NUMBER['toStringDigits'],
+    'length': function( str ) {
+        return str.length;
+    },
+    'string-to-lower': function( str ) {
+        return str.toLowerCase();
+    },
+    'concat': function( strA, strB ) {
+        return strA.concat( strB );
+    },
+    'substring': function( str, start, end ) {
+        return str.substring( start, end );
+    },
+    'charAt': function( str, index ) {
+        return str.charAt( index );
+    },
+    'split': function( str, separator ) {
+        return str.split( separator );
+    },
+    'split-pattern': function( str, pattern ) {
+        return str.split( RegExp( pattern ) );
+    },
+    'match': function( str, pattern ) {
+        return str.match( RegExp( pattern ) );
+    },
+    "string-to-number": stringToNumber,
+    'num-to-string': numToString,
+    'num-to-string-digits': NUMBER['toStringDigits'],
 };
