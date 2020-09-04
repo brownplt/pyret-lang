78144ba34e5642879760f346feafe82244042951
Michael MacLeod <michaelmmacleod@gmail.com>
2020-09-04 15:13:07 -0700

    Animate the run button

--- ide/src/Editor.tsx
+++ ide/src/Editor.tsx
@@ -11,10 +11,9 @@ import SingleCodeMirrorDefinitions from './SingleCodeMirrorDefinitions';
 import Menu from './Menu';
 import MenuBar from './MenuBar';
 import Footer from './Footer';
-import Dropdown from './Dropdown';
-import DropdownOption from './DropdownOption';
 import Header from './Header';
 import InteractionError from './InteractionError';
+import Run from './Run';
 import * as control from './control';
 import 'react-splitter-layout/lib/index.css';
 import * as action from './action';
@@ -24,16 +23,11 @@ type StateProps = {
   currentFileContents: undefined | string,
   definitionsHighlights: number[][],
   fontSize: number,
-  stopify: boolean,
   interactionErrors: any[],
   editorMode: EditorMode,
   chunks: Chunk[],
-  running: boolean,
   compiling: boolean | 'out-of-date',
   linting: boolean,
-  dropdownVisible: boolean,
-  autoRun: boolean,
-  typeCheck: boolean,
 };
 
 function mapStateToProps(state: State.State): StateProps {
@@ -42,34 +36,21 @@ function mapStateToProps(state: State.State): StateProps {
     currentFileContents: state.currentFileContents,
     definitionsHighlights: state.definitionsHighlights,
     fontSize: state.fontSize,
-    stopify: state.runKind === control.backend.RunKind.Async,
     interactionErrors: state.interactionErrors,
     editorMode: state.editorMode,
     chunks: state.chunks,
-    running: state.running,
     compiling: state.compiling,
     linting: state.linting,
-    dropdownVisible: state.dropdownVisible,
-    autoRun: state.autoRun,
-    typeCheck: state.typeCheck,
   };
 }
 
 type DispatchProps = {
-  stop: () => void,
-  run: () => void,
   updateContents: (contents: string) => void,
   setEditorMode: (mode: EditorMode) => void,
-  setAutoRun: (autoRun: boolean) => void,
-  setStopify: (stopify: boolean) => void,
-  setTypeCheck: (typeCheck: boolean) => void,
-  setDropdownVisible: (dropdownVisible: boolean) => void,
 };
 
 function mapDispatchToProps(dispatch: (action: action.Action) => any): DispatchProps {
   return {
-    stop: () => dispatch({ type: 'enqueueEffect', effect: 'stop' }),
-    run: () => dispatch({ type: 'enqueueEffect', effect: 'saveFile' }),
     updateContents: (contents: string) => dispatch({
       type: 'update',
       key: 'currentFileContents',
@@ -77,22 +58,6 @@ function mapDispatchToProps(dispatch: (action: action.Action) => any): DispatchP
     }),
     setEditorMode: (mode: EditorMode) => {
       dispatch({ type: 'update', key: 'editorMode', value: mode });
-    },
-    setAutoRun: (autoRun: boolean) => {
-      dispatch({ type: 'update', key: 'autoRun', value: autoRun });
-    },
-    setStopify: (stopify: boolean) => {
-      if (stopify) {
-        dispatch({ type: 'update', key: 'runKind', value: control.backend.RunKind.Async });
-      } else {
-        dispatch({ type: 'update', key: 'runKind', value: control.backend.RunKind.Sync });
-      }
-    },
-    setTypeCheck: (typeCheck: boolean) => {
-      dispatch({ type: 'update', key: 'typeCheck', value: typeCheck });
-    },
-    setDropdownVisible: (dropdownVisible: boolean) => {
-      dispatch({ type: 'update', key: 'dropdownVisible', value: dropdownVisible });
     },
   };
 }
@@ -194,48 +159,12 @@ export class Editor extends React.Component<EditorProps, any> {
 
   render() {
     const {
-      compiling,
-      linting,
       fontSize,
       interactionErrors,
-      stopify,
-      stop,
-      run,
-      running,
-      dropdownVisible,
-      autoRun,
-      typeCheck,
-      setAutoRun,
-      setStopify,
-      setTypeCheck,
-      setDropdownVisible,
     } = this.props;
 
     const interactionValues = (
       <RHS />
-    );
-
-    const dropdown = dropdownVisible && (
-      <Dropdown>
-        <DropdownOption
-          enabled={autoRun}
-          onClick={() => setAutoRun(!autoRun)}
-        >
-          Auto Run
-        </DropdownOption>
-        <DropdownOption
-          enabled={stopify}
-          onClick={() => setStopify(!stopify)}
-        >
-          Stopify
-        </DropdownOption>
-        <DropdownOption
-          enabled={typeCheck}
-          onClick={() => setTypeCheck(!typeCheck)}
-        >
-          Type Check
-        </DropdownOption>
-      </Dropdown>
     );
 
     const rightHandSide = (
@@ -268,54 +197,7 @@ export class Editor extends React.Component<EditorProps, any> {
           <div>
             <MenuBar />
           </div>
-          <div
-            style={{
-              height: '100%',
-            }}
-          >
-            {stopify && running ? (
-              <button
-                className="stop-available"
-                onClick={stop}
-                type="button"
-              >
-                Stop
-              </button>
-            ) : (
-              <button
-                className="stop-unavailable"
-                type="button"
-              >
-                Stop
-              </button>
-            )}
-            <div
-              className="run-container"
-            >
-              <button
-                className="run-ready"
-                type="button"
-                onClick={run}
-                style={{
-                  background: compiling || running || linting ? 'gray' : '#317bcf',
-                }}
-              >
-                Run
-              </button>
-              <button
-                type="button"
-                className="run-options"
-                onClick={() => setDropdownVisible(!dropdownVisible)}
-                onBlur={() => setDropdownVisible(false)}
-                style={{
-                  background: compiling || running || linting ? 'gray' : '#317bcf',
-                }}
-              >
-                &#8628;
-                {dropdown}
-              </button>
-            </div>
-          </div>
+          <Run />
         </Header>
         <div className="code-container">
           <Menu />
---
+++ ide/src/Run.tsx
@@ -0,0 +1,221 @@
+import React from 'react';
+
+import {
+  connect,
+  ConnectedProps,
+} from 'react-redux';
+
+import {
+  State,
+} from './state';
+
+import {
+  Action,
+} from './action';
+
+import {
+  Chunk,
+} from './chunk';
+
+import Dropdown from './Dropdown';
+import DropdownOption from './DropdownOption';
+
+import * as control from './control';
+
+type StateProps = {
+  stopify: boolean,
+  dropdownVisible: boolean,
+  autoRun: boolean,
+  typeCheck: boolean,
+  running: boolean,
+  compiling: boolean | 'out-of-date',
+  linting: boolean,
+  chunks: Chunk[],
+};
+
+function mapStateToProps(state: State): StateProps {
+  const {
+    runKind,
+    dropdownVisible,
+    autoRun,
+    typeCheck,
+    running,
+    compiling,
+    linting,
+    chunks,
+  } = state;
+
+  return {
+    stopify: runKind === control.backend.RunKind.Async,
+    dropdownVisible,
+    autoRun,
+    typeCheck,
+    running,
+    compiling,
+    linting,
+    chunks,
+  };
+}
+
+type PropsFromReact = {
+};
+
+type DispatchProps = {
+  run: () => void,
+  stop: () => void,
+  setAutoRun: (autoRun: boolean) => void,
+  setStopify: (stopify: boolean) => void,
+  setTypeCheck: (typeCheck: boolean) => void,
+  setDropdownVisible: (dropdownVisible: boolean) => void,
+};
+
+function mapDispatchToProps(dispatch: (action: Action) => void): DispatchProps {
+  return {
+    run: () => dispatch({ type: 'enqueueEffect', effect: 'saveFile' }),
+    stop: () => dispatch({ type: 'enqueueEffect', effect: 'stop' }),
+    setAutoRun: (autoRun: boolean) => {
+      dispatch({ type: 'update', key: 'autoRun', value: autoRun });
+    },
+    setStopify: (stopify: boolean) => {
+      if (stopify) {
+        dispatch({ type: 'update', key: 'runKind', value: control.backend.RunKind.Async });
+      } else {
+        dispatch({ type: 'update', key: 'runKind', value: control.backend.RunKind.Sync });
+      }
+    },
+    setTypeCheck: (typeCheck: boolean) => {
+      dispatch({ type: 'update', key: 'typeCheck', value: typeCheck });
+    },
+    setDropdownVisible: (dropdownVisible: boolean) => {
+      dispatch({ type: 'update', key: 'dropdownVisible', value: dropdownVisible });
+    },
+  };
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = PropsFromRedux & DispatchProps & StateProps & PropsFromReact;
+
+function Run({
+  run,
+  stop,
+  setAutoRun,
+  setStopify,
+  setTypeCheck,
+  setDropdownVisible,
+  stopify,
+  dropdownVisible,
+  autoRun,
+  typeCheck,
+  running,
+  compiling,
+  linting,
+  chunks,
+}: Props) {
+  const dropdown = dropdownVisible && (
+    <Dropdown>
+      <DropdownOption
+        enabled={autoRun}
+        onClick={() => setAutoRun(!autoRun)}
+      >
+        Auto Run
+      </DropdownOption>
+      <DropdownOption
+        enabled={stopify}
+        onClick={() => setStopify(!stopify)}
+      >
+        Stopify
+      </DropdownOption>
+      <DropdownOption
+        enabled={typeCheck}
+        onClick={() => setTypeCheck(!typeCheck)}
+      >
+        Type Check
+      </DropdownOption>
+    </Dropdown>
+  );
+
+  let percent = '100%';
+
+  if (linting) {
+    let chunksToLint = 0;
+
+    chunks.forEach((chunk) => {
+      if (chunk.errorState.status === 'notLinted') {
+        chunksToLint += 1;
+      }
+    });
+
+    const n = (50 / chunks.length) * (chunks.length - chunksToLint);
+
+    percent = `${n}%`;
+  }
+
+  if (compiling) {
+    percent = '75%';
+  }
+
+  if (running) {
+    percent = '100%';
+  }
+
+  let buttonBackground = `linear-gradient(90deg, #317bcf ${percent}, gray ${percent})`;
+
+  if (running) {
+    buttonBackground = 'green';
+  }
+
+  return (
+    <div
+      style={{
+        height: '100%',
+      }}
+    >
+      {stopify && running ? (
+        <button
+          className="stop-available"
+          onClick={stop}
+          type="button"
+        >
+          Stop
+        </button>
+      ) : (
+        <button
+          className="stop-unavailable"
+          type="button"
+        >
+          Stop
+        </button>
+      )}
+      <div
+        className="run-container"
+      >
+        <button
+          className="run-ready"
+          type="button"
+          onClick={run}
+          style={{
+            background: buttonBackground,
+          }}
+        >
+          Run
+        </button>
+        <button
+          type="button"
+          className="run-options"
+          onClick={() => setDropdownVisible(!dropdownVisible)}
+          onBlur={() => setDropdownVisible(false)}
+          style={{
+            background: compiling || running || linting ? 'gray' : '#317bcf',
+          }}
+        >
+          &#8628;
+          {dropdown}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default connector(Run);
