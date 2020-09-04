5558badbbbad32827334ffb94b628064d95d51b4
Alexander Vo <randomcivvy0121@gmail.com>
2020-09-03 19:46:11 -0700

    Update builtin module numbers according to the documentation
    Remove duplicate exports

--- src/runtime/number.arr.ts
+++ src/runtime/number.arr.ts
@@ -1,4 +1,5 @@
 const NUMBERS = require('./js-numbers.js');
+const EQUALITY = require("./equality.js");
 
 
 function numToString(n) {
@@ -6,16 +7,9 @@ function numToString(n) {
 }
 
 module.exports = {
-    'num-to-string': numToString,
-    'num-to-string-digits': NUMBERS['toStringDigits'],
+
     'num-equal': NUMBERS['equals'],
-    'num-is-integer': NUMBERS['isInteger'],
-    'num-is-rational': NUMBERS['isRational'],
-    'num-is-roughnum': NUMBERS['isRoughnum'],
-    'num-is-positive': NUMBERS['isPositive'],
-    'num-is-negative': NUMBERS['isNegative'],
-    'num-is-non-positive': NUMBERS['isNonPositive'],
-    'num-is-non-negative': NUMBERS['isNonNegative'],
+
     'num-max': function(a, b) {
         if (NUMBERS['greaterThan'](a, b)) {
             return a;
@@ -23,6 +17,7 @@ module.exports = {
             return b;
         }
     },
+
     'num-min': function(a, b) {
         if (NUMBERS['greaterThan'](a, b)) {
             return b;
@@ -30,6 +25,7 @@ module.exports = {
             return a;
         }
     },
+
     'num-abs': NUMBERS['abs'],
     'num-sin': NUMBERS['sin'],
     'num-cos': NUMBERS['cos'],
@@ -38,6 +34,7 @@ module.exports = {
     'num-acos': NUMBERS['acos'],
     'num-atan': NUMBERS['atan'],
     'num-atan2': NUMBERS['atan2'],
+
     'num-modulo': NUMBERS['modulo'],
     'num-truncate': function(n) {
         if (NUMBERS['greaterThan'](n, 0)) {
@@ -48,25 +45,43 @@ module.exports = {
     },
     'num-sqrt': NUMBERS['sqrt'],
     'num-sqr': NUMBERS['sqr'],
+
     'num-ceiling': NUMBERS['ceiling'],
     'num-floor': NUMBERS['floor'],
+
     'num-round': NUMBERS['round'],
     'num-round-even': NUMBERS['round-even'],
+
     'num-log': NUMBERS['log'],
     'num-exp': NUMBERS['exp'],
     'num-expt': NUMBERS['expt'],
+
     'num-to-roughnum': NUMBERS['toRoughnum'],
+
     'num-is-integer': NUMBERS['isInteger'],
     'num-is-rational': NUMBERS['isRational'],
     'num-is-roughnum': NUMBERS['isRoughnum'],
-    'num-within-abs': function(tol) {
-        return function(l, r) {
-            return NUMBERS['roughlyEquals'](l, r, tol);
-        };
+    'num-is-positive': NUMBERS['isPositive'],
+    'num-is-negative': NUMBERS['isNegative'],
+    'num-is-non-positive': NUMBERS['isNonPositive'],
+    'num-is-non-negative': NUMBERS['isNonNegative'],
+
+
+    'num-to-string': numToString,
+    'num-to-string-digits': NUMBERS['toStringDigits'],
+
+    'num-within-abs': EQUALITY["withinAbs"],
+    'num-within-rel': EQUALITY["withinRel"],
+
+    'num-is-fixnum': function(num): boolean {
+        return NUMBERS["isFixnum"](num);
     },
-    'num-within-rel': function(relTol) {
-        return function(l, r) {
-            return NUMBERS['roughlyEqualsRel'](l, r, relTol);
-        };
+
+    'num-to-exact': function(num): number {
+        return NUMBERS["toExact"](num);
+    },
+
+    'num-to-rational': function(num): number {
+        return NUMBERS["toRational"](num);
     },
 };
