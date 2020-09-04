5cce694fc3a8420b831aa8b55586da90d4e7269d
Alexander Vo <randomcivvy0121@gmail.com>
2020-09-03 18:23:57 -0700

    Separate core equality algorithm into its own function (equalCore3).
    Implement equalNow3 and equalAlways3 in terms of equalCore3
    
    Now supports tolerance + relative checks for numbers
    Scaffolding for always checks

--- src/runtime/equality.ts
+++ src/runtime/equality.ts
@@ -63,6 +63,9 @@ export function Unknown(reason: string, value1: any, value2: any): Unknown {
   };
 }
 
+const TOL_IS_REL = true;
+const TOL_IS_ABS = false;
+
 const numericEquals: (v1: any, v2: any, callbacks: NumericErrorCallbacks) => boolean = _NUMBER["equals"];
 
 export function isEqual(val: any): boolean{
@@ -144,7 +147,7 @@ export function equalNow(v1: any, v2: any): boolean {
 }
 
 export function equalNow3(v1: any, v2: any): EqualityResult {
-    throw "Implement equalNow3";
+  return equalCore3(v1, v2, false, 0, TOL_IS_ABS, false);
 }
 
 /*
@@ -154,191 +157,8 @@ export function equalNow3(v1: any, v2: any): EqualityResult {
  * Data variants and raw (unbranded) objects are NEVER equal.
  *
  */
-export function equalAlways3(e1: any, e2: any): EqualityResult {
-  if (isEqual(identical3(e1, e2))) {
-    // Identical so must always be equal
-    return Equal();
-  }
-
-  var worklist = [[e1, e2]];
-  while (worklist.length > 0) {
-    var curr = worklist.pop();
-    var v1: any = curr[0];
-    var v2: any = curr[1];
-
-    if (isEqual(identical3(v1, v2))) {
-      // Identical so must always be equal
-      continue;
-    }
-
-    if (PRIMTIVES.isNumber(v1) && PRIMTIVES.isNumber(v2)) {
-      if (PRIMTIVES.isRoughNumber(v1) || PRIMTIVES.isRoughNumber(v2)) {
-        return Unknown("Rough Number equal-always", v1, v2);
-      } else if (numericEquals(v1, v2, NumberErrbacks)) {
-        continue;
-      } else {
-        return NotEqual("Numers", v1, v2);
-      }
-
-    } else if (PRIMTIVES.isBoolean(v1) && PRIMTIVES.isBoolean(v2)) {
-      if (v1 !== v2) { return NotEqual("Booleans", v1, v2); }
-      continue;
-
-    } else if (PRIMTIVES.isString(v1) && PRIMTIVES.isString(v2)) {
-      if (v1 !== v2) { return NotEqual("Strings", v1, v2); }
-      continue
-
-    } else if (PRIMTIVES.isFunction(v1) && PRIMTIVES.isFunction(v2)) {
-      // Cannot compare functions for equality
-      return Unknown("Functions", v1, v2);
-    } else if (PRIMTIVES.isMethod(v1) && PRIMTIVES.isMethod(v2)) {
-      return Unknown("Methods", v1, v2);
-    } else if (PRIMTIVES.isPTuple(v1) && PRIMTIVES.isPTuple(v2)) {
-      if (v1.length !== v2.length) {
-        return NotEqual("PTuple Length", v1, v2);
-      }
-
-      for (var i = 0; i < v1.length; i++) {
-        worklist.push([v1[i], v2[i]]);
-      }
-      continue;
-
-    } else if (PRIMTIVES.isArray(v1) && PRIMTIVES.isArray(v2)) {
-      if (v1.length !== v2.length) {
-        return NotEqual("Array Length", v1, v2);
-      }
-
-      for (var i = 0; i < v1.length; i++) {
-        worklist.push([v1[i], v2[i]]);
-      }
-      continue;
-
-    } else if (PRIMTIVES.isNothing(v1) && PRIMTIVES.isNothing(v2)) {
-      // Equality is defined for 'nothing'
-      // 'nothing' is always equal to 'nothing'
-      continue;
-
-    } else if (PRIMTIVES.isPRef(v1) && PRIMTIVES.isPRef(v2)) {
-      // In equal-always, non-identical refs are not equal
-      if (v1.ref !== v2.ref) {
-        return NotEqual("PRef'd Objects", v1, v2);
-      }
-        continue;
-    } else if (PRIMTIVES.isTable(v1) && PRIMTIVES.isTable(v2)) {
-        let v1_headers = v1._headers;
-        let v2_headers = v2._headers;
-
-        if (v1_headers.length !== v2_headers.length) {
-            return NotEqual(`Row Header Length (${v1_headers.length}, ${v2_headers.length})`, v1, v2);
-        }
-
-        if (v1._rows.length !== v2._rows.length) {
-            return NotEqual(`Row Length (${v1._rows.length}, ${v2._rows.length})`, v1, v2);
-        }
-
-        // Check table headers
-        for (let h = 0; h < v1_headers.length; h++) {
-            if (v1_headers[h] !== v2_headers[h]) {
-                return NotEqual(`Row headers (index ${h})`, v1, v2);
-            }
-        }
-
-        // Check row elements
-        for (let row = 0; row < v1._rows.length; row++) {
-            let v1_row = v1._rows[row];
-            let v2_row = v2._rows[row];
-
-            for (let i = 0; i < v1_row.length; i++) {
-                worklist.push([v1_row[i], v2_row[i]]);
-            }
-        }
-
-        continue;
-    } else if (PRIMTIVES.isRow(v1) && PRIMTIVES.isRow(v2)) {
-
-        let v1_headers = v1._headers;
-        let v2_headers = v2._headers;
-        if (v1_headers.length !== v2_headers.length) {
-            return NotEqual(`Row Header Length (${v1_headers.length}, ${v2_headers.length})`, v1, v2);
-        }
-
-        // TODO(alex): is this check necessary?
-        if (v1._elements.length !== v2._elements.length) {
-            return NotEqual(`Row Elements Length (${v1._elements.length}, ${v1._elements.length})`, v1, v2);
-        }
-
-        for (let i = 0; i < v1_headers.length; i++) {
-            if (v1_headers[i] !== v2_headers[i]) {
-                return NotEqual(`Row headers (index ${i})`, v1, v2);
-            }
-        }
-
-        for (let i = 0; i < v1._elements.length; i++) {
-            worklist.push([v1[i], v2[i]]);
-        }
-
-        continue;
-
-    } else if (PRIMTIVES.isDataVariant(v1) && PRIMTIVES.isDataVariant(v2)) {
-      if(v1.$brand && v1.$brand === v2.$brand) {
-        if ("_equals" in v1) {
-          // TODO(alex): Recursive callback that closes over the environment as specified by:
-          //   https://www.pyret.org/docs/latest/equality.html#%28part._s~3adatatype-defined-equality%29
-          //   NOTE(alex): CURRENTLY FAILS WITH CYCLIC STRUCTURES
-          var ans = v1["_equals"](v2, equalAlways3);
-
-          if (!isEqual(ans)) {
-            return ans;
-          } else {
-            continue;
-          }
-        }
-
-        var fields1 = v1.$brand.names;
-        var fields2 = v2.$brand.names;
-
-        if(fields1.length !== fields2.length) {
-          // Not the same brand
-          return NotEqual("Object Brands", v1, v2);
-        }
-        for(var i = 0; i < fields1.length; i += 1) {
-          if(fields1[i] != fields2[i]) {
-            // Not the same brand
-            return NotEqual("Field Brands", fields1[i], fields2[i]);
-          }
-          worklist.push([v1[fields1[i]], v2[fields2[i]]]);
-        }
-        continue;
-      } else {
-        return NotEqual("Variant Brands", v1, v2);
-      }
-    } else if (PRIMTIVES.isRawObject(v1) && PRIMTIVES.isRawObject(v2)) {
-      let keys1 = Object.keys(v1);
-      let keys2 = Object.keys(v2);
-
-      if (keys1.length !== keys2.length) {
-        return NotEqual("Raw Object Field Count", v1, v2);
-      }
-
-      // Check for matching keys and push field to worklist
-      for (var i = 0; i < keys1.length; i++) {
-        let key2Index = keys2.indexOf(keys1[i]);
-        if (key2Index === -1) {
-          // Key in v1 not found in v2
-          return NotEqual(`Raw Object Missing Field '${keys1[i]}'`, v1, v2);
-        } else {
-          // Push common field to worklist
-          worklist.push([v1[keys1[i]], v2[keys2[key2Index]]]);
-        }
-      }
-
-      continue;
-    } else {
-      return NotEqual("", e1, e2);
-    }
-  }
-
-  return Equal();
+export function equalAlways3(v1: any, v2: any): EqualityResult {
+  return equalCore3(v1, v2, true, 0, TOL_IS_ABS, false);
 }
 
 export function equalAlways(v1: any, v2: any): boolean {
@@ -467,4 +287,216 @@ export function _lessequal(lhs: any, rhs: any): boolean {
 
 export function _greaterequal(lhs: any, rhs: any): boolean {
     return equalAlways(lhs, rhs) || _greaterthan(lhs, rhs);
+}
+
+// tol and rel are PyretNumber's
+function equalCore3(left: any, right: any, alwaysFlag: boolean, tol: number, rel: boolean, fromWithin: boolean): EqualityResult {
+
+  if(tol === undefined) { // means that we aren't doing any kind of within
+    const isIdentical = identical3(left, right);
+    if (!isNotEqual(isIdentical)) {
+      return isIdentical;   // Equal or Unknown
+    }
+  }
+
+  let worklist = [[left, right]];
+
+  // Actual equality implementation on individual items
+  function equalHelp(): EqualityResult {
+    let curr = worklist.pop();
+    let v1: any = curr[0];
+    let v2: any = curr[1];
+
+    if (isEqual(identical3(v1, v2))) {
+      // Identical so must always be equal
+      return Equal();
+    }
+
+    if (PRIMTIVES.isNumber(v1) && PRIMTIVES.isNumber(v2)) {
+
+      if (tol) {
+        if (rel) {
+          if (_NUMBER["roughlyEqualsRel"](v1, v2, tol, NumberErrbacks)) {
+            return Equal();
+          } else {
+            return NotEqual("", v1, v2);
+          }
+        } else if (_NUMBER["roughlyEquals"](v1, v2, tol, NumberErrbacks)) {
+          return Equal();
+        } else {
+          return NotEqual("", v1, v2);
+        }
+      } else if (PRIMTIVES.isRoughNumber(v1) || PRIMTIVES.isRoughNumber(v2)) {
+        return Unknown("Rough Number equal-always", v1, v2);
+      } else if (numericEquals(v1, v2, NumberErrbacks)) {
+        return Equal();
+      } else {
+        return NotEqual("Numbers", v1, v2);
+      }
+
+    } else if (PRIMTIVES.isBoolean(v1) && PRIMTIVES.isBoolean(v2)) {
+      if (v1 !== v2) { return NotEqual("Booleans", v1, v2); }
+      return Equal();
+
+    } else if (PRIMTIVES.isString(v1) && PRIMTIVES.isString(v2)) {
+      if (v1 !== v2) { return NotEqual("Strings", v1, v2); }
+      return Equal();
+
+    } else if (PRIMTIVES.isFunction(v1) && PRIMTIVES.isFunction(v2)) {
+      // Cannot compare functions for equality
+      return Unknown("Functions", v1, v2);
+    } else if (PRIMTIVES.isMethod(v1) && PRIMTIVES.isMethod(v2)) {
+      return Unknown("Methods", v1, v2);
+    } else if (PRIMTIVES.isPTuple(v1) && PRIMTIVES.isPTuple(v2)) {
+      if (v1.length !== v2.length) {
+        return NotEqual("PTuple Length", v1, v2);
+      }
+
+      for (var i = 0; i < v1.length; i++) {
+        worklist.push([v1[i], v2[i]]);
+      }
+      return Equal();
+
+    } else if (PRIMTIVES.isArray(v1) && PRIMTIVES.isArray(v2)) {
+      if (alwaysFlag || v1.length !== v2.length) {
+        return NotEqual("Array Length", v1, v2);
+      }
+
+      for (var i = 0; i < v1.length; i++) {
+        worklist.push([v1[i], v2[i]]);
+      }
+      return Equal();
+
+    } else if (PRIMTIVES.isNothing(v1) && PRIMTIVES.isNothing(v2)) {
+      // Equality is defined for 'nothing'
+      // 'nothing' is always equal to 'nothing'
+      return Equal();
+
+    } else if (PRIMTIVES.isPRef(v1) && PRIMTIVES.isPRef(v2)) {
+      // In equal-always, non-identical refs are not equal
+      // TODO: handle alwaysFlag
+      if (v1.ref !== v2.ref) {
+        return NotEqual("PRef'd Objects", v1, v2);
+      }
+
+      return Equal();
+
+    } else if (PRIMTIVES.isTable(v1) && PRIMTIVES.isTable(v2)) {
+        let v1_headers = v1._headers;
+        let v2_headers = v2._headers;
+
+        if (v1_headers.length !== v2_headers.length) {
+            return NotEqual(`Row Header Length (${v1_headers.length}, ${v2_headers.length})`, v1, v2);
+        }
+
+        if (v1._rows.length !== v2._rows.length) {
+            return NotEqual(`Row Length (${v1._rows.length}, ${v2._rows.length})`, v1, v2);
+        }
+
+        // Check table headers
+        for (let h = 0; h < v1_headers.length; h++) {
+            if (v1_headers[h] !== v2_headers[h]) {
+                return NotEqual(`Row headers (index ${h})`, v1, v2);
+            }
+        }
+
+        // Check row elements
+        for (let row = 0; row < v1._rows.length; row++) {
+            let v1_row = v1._rows[row];
+            let v2_row = v2._rows[row];
+
+            for (let i = 0; i < v1_row.length; i++) {
+                worklist.push([v1_row[i], v2_row[i]]);
+            }
+        }
+
+        return Equal();
+    } else if (PRIMTIVES.isRow(v1) && PRIMTIVES.isRow(v2)) {
+
+        let v1_headers = v1._headers;
+        let v2_headers = v2._headers;
+        if (v1_headers.length !== v2_headers.length) {
+            return NotEqual(`Row Header Length (${v1_headers.length}, ${v2_headers.length})`, v1, v2);
+        }
+
+        // TODO(alex): is this check necessary?
+        if (v1._elements.length !== v2._elements.length) {
+            return NotEqual(`Row Elements Length (${v1._elements.length}, ${v1._elements.length})`, v1, v2);
+        }
+
+        for (let i = 0; i < v1_headers.length; i++) {
+            if (v1_headers[i] !== v2_headers[i]) {
+                return NotEqual(`Row headers (index ${i})`, v1, v2);
+            }
+        }
+
+        for (let i = 0; i < v1._elements.length; i++) {
+            worklist.push([v1[i], v2[i]]);
+        }
+
+        return Equal();
+
+    } else if (PRIMTIVES.isDataVariant(v1) && PRIMTIVES.isDataVariant(v2)) {
+      if(v1.$brand && v1.$brand === v2.$brand) {
+        if ("_equals" in v1) {
+          //   NOTE(alex): CURRENTLY FAILS WITH CYCLIC STRUCTURES
+          var ans = v1["_equals"](v2, equalHelp);
+
+          return ans;
+        }
+
+        var fields1 = v1.$brand.names;
+        var fields2 = v2.$brand.names;
+
+        if(fields1.length !== fields2.length) {
+          // Not the same brand
+          return NotEqual("Object Brands", v1, v2);
+        }
+        for(var i = 0; i < fields1.length; i += 1) {
+          if(fields1[i] != fields2[i]) {
+            // Not the same brand
+            return NotEqual("Field Brands", fields1[i], fields2[i]);
+          }
+          worklist.push([v1[fields1[i]], v2[fields2[i]]]);
+        }
+        return Equal();
+
+      } else {
+        return NotEqual("Variant Brands", v1, v2);
+      }
+    } else if (PRIMTIVES.isRawObject(v1) && PRIMTIVES.isRawObject(v2)) {
+      let keys1 = Object.keys(v1);
+      let keys2 = Object.keys(v2);
+
+      if (keys1.length !== keys2.length) {
+        return NotEqual("Raw Object Field Count", v1, v2);
+      }
+
+      // Check for matching keys and push field to worklist
+      for (var i = 0; i < keys1.length; i++) {
+        let key2Index = keys2.indexOf(keys1[i]);
+        if (key2Index === -1) {
+          // Key in v1 not found in v2
+          return NotEqual(`Raw Object Missing Field '${keys1[i]}'`, v1, v2);
+        } else {
+          // Push common field to worklist
+          worklist.push([v1[keys1[i]], v2[keys2[key2Index]]]);
+        }
+      }
+
+      return Equal();
+    } else {
+      return NotEqual("", left, right);
+    }
+  }
+
+  while (worklist.length > 0) {
+    // Scaffolding to set use equalHelp()
+    const result = equalHelp();
+    if (!isEqual(result)) {
+      return result;
+    }
+  }
+
+  return Equal();
 }
