18b8643a918870e0a158afea59850fd6b66be8c9
Alexander Vo <randomcivvy0121@gmail.com>
2020-09-10 11:07:52 -0700

    Handle spy value messages

--- src/runtime/override.arr.ts
+++ src/runtime/override.arr.ts
@@ -24,5 +24,26 @@ RUNTIME.$setSpyMessageHandler((data: { message: string, loc: string}) => {
 });
 
 RUNTIME.$setSpyValueHandler((data: { key: string, value: any, loc: string}) => {
+  // @ts-ignore
+  if (dispatch) {
+    // @ts-ignore
+    dispatch({
+      type: 'update',
+      key: 'rhs',
+      value: {
+        value: {
+          key: data.key,
+          value: data.value,
+        },
+        key: data.loc,
+        loc: data.loc,
+      },
+    });
+    console.log("Dispatch");
+  } else {
+    console.log("No available dispatch");
+  }
+
+
   console.log("SPY VALUE");
 });
