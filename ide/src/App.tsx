3bdbfec647a985c2ef5022811d9cef8e8a9eaffb
Michael MacLeod <michaelmmacleod@gmail.com>
2020-09-16 18:33:17 -0700

    Add documentation to App.tsx

--- ide/src/App.tsx
+++ ide/src/App.tsx
@@ -1,3 +1,7 @@
+/* This is the toplevel component for the IDE. Not much happens here other than
+   setting up the Redux store through the Provider component. All of the
+   interesting things start to happen in the ConnectedEditor component from
+   Editor.tsx. */
 import React from 'react';
 import { Provider } from 'react-redux';
 import ConnectedEditor from './Editor';
