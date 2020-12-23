662a5a0b1072f181a5e848a454d39fae8fb5a19b
Alexander Vo <randomcivvy0121@gmail.com>
2020-12-22 16:51:41 -0800

    Fully implement input appending for Text Mode

--- tests-ide/util/TextMode.ts
+++ tests-ide/util/TextMode.ts
@@ -2,30 +2,20 @@ const selenium = require("selenium-webdriver");
 
 const OPTION_BUTTON_ID = 'optionsButton';
 const TEXT_MODE_BUTTON_ID = 'textModeButton';
-const CODE_MIRROR_INPUT_CLASS = 'CodeMirror';
 
 const By = selenium.By;
 
 /// Assumes already in Text Mode
-export async function sendInput(driver, input) {
+export async function appendInput(driver, input) {
 
-  // CodeMirror stores code in div elements of class CODE_MIRROR_INPUT_CLASS
-  // In text mode, should only be one `CodeMirror-code` element
-  // let inputArea = await driver.findElement({ className: CODE_MIRROR_INPUT_CLASS });
-  console.log("FOO");
+  let codeMirror = await driver.findElement(By.className("CodeMirror"));
+  let codeLines = await codeMirror.findElements(By.className("CodeMirror-line"));
+  let codeLine = codeLines.pop();
+  await codeLine.click();
 
-  // let codeMirror = await driver.findElement(By.className("CodeMirror-scroll"));
-  let codeMirror = await driver.findElement(By.css('.CodeMirror textarea'));
-  //console.log("BAZ");
-  //await codeMirror.click();
-  //console.log("QUX");
-  console.log(await codeMirror.getAttribute("visible"));
-  await codeMirror.sendKeys("Hello World");
+  let inputArea = await codeMirror.findElement(By.css("textarea"));
 
-  //let value = await inputArea.getAttribute("class");
-  //console.log(value);
-
-  // await inputArea.sendKeys(input);
+  await inputArea.sendKeys(input);
 }
 
 export async function toTextMode(driver) {
