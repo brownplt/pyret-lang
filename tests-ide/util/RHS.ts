a7ee528da9054d5818937cf567fd56a32b288f54
Alexander Vo <randomcivvy0121@gmail.com>
2020-12-22 16:54:17 -0800

    Implement basic RHS result scanning

---
+++ tests-ide/util/RHS.ts
@@ -0,0 +1,25 @@
+const selenium = require("selenium-webdriver");
+
+const By = selenium.By;
+
+const RHS_CLASS: string = "interactions-area-container";
+
+export async function searchFor(driver, input: string, strict?: boolean) {
+  let rhs = await driver.findElement(By.className(RHS_CLASS));
+  let results = await rhs.findElements(By.css("pre"));
+
+  for (let i = 0; i < results.length; i++) {
+    let inner = await results[i].getAttribute("innerHTML");
+    if (strict) {
+      if (inner === input) {
+        return true;
+      }
+    } else {
+      if (inner.includes(input)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
