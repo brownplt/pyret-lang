899a15d1fa6a68049feae7f2f3797c1fc0906ea8
Alexander Vo <randomcivvy0121@gmail.com>
2020-12-22 16:53:29 -0800

    Start a new Chrome instance before each IDE test. Add two basic compile/run tests

--- tests-ide/ide.test.ts
+++ tests-ide/ide.test.ts
@@ -1,6 +1,7 @@
 import { strict as assert } from 'assert';
 import { setup } from './util/setup';
-import * as textMode from './util/TextMode';
+import * as TextMode from './util/TextMode';
+import * as RHS from './util/RHS';
 // const glob = require('glob');
 // const tester = require("./test-util.js");
 
@@ -8,6 +9,10 @@ const TEST_TIMEOUT = 20000;
 const COMPILER_TIMEOUT = 10000; // ms, for each compiler run
 const STARTUP_TIMEOUT = 6000;
 
+
+//
+// NOTE(alex): All tests assume that the editor begins with an ***EMPTY*** file
+//
 describe("Testing browser simple-output programs", () => {
 
   jest.setTimeout(TEST_TIMEOUT);
@@ -16,25 +21,67 @@ describe("Testing browser simple-output programs", () => {
   let ideURL;
   let refreshPagePerTest;
 
-  beforeAll(() => {
+  // TODO(alex): Need to have empty file open before each test
+  //   Easiest way to do that so far is to open a new Chrome instance
+  //     which somehow doesn't persist the file (maybe this is a property
+  //     of the ChromeDriver that does not save localstorage?)
+  //
+  //   If you ever figure out how to reset the IDE, you can swap back to
+  //     a single Chrome instance using the following code
+  //
+  //beforeAll(() => {
+  //  let setupResult = setup();
+  //  driver = setupResult.driver;
+  //  ideURL = setupResult.ideURL;
+  //  refreshPagePerTest = setupResult.refreshPagePerTest;
+
+  //  return driver.get(ideURL);
+  //});
+
+  //afterAll(() => {
+  //  return driver.quit();
+  //});
+
+  beforeEach(() => {
     let setupResult = setup();
     driver = setupResult.driver;
     ideURL = setupResult.ideURL;
     refreshPagePerTest = setupResult.refreshPagePerTest;
 
-    if (refreshPagePerTest === false) {
-      return driver.get(ideURL);
-    }
+    return driver.get(ideURL);
   });
 
-  afterAll(() => {
-    // return driver.quit();
+  afterEach(() => {
+    return driver.quit();
   });
 
-  test("Should load the IDE", async function(done) {
+  test("Append Input 1", async function(done) {
 
-    await textMode.toTextMode(driver);
-    await textMode.sendInput(driver, "FOO");
+    await TextMode.toTextMode(driver);
+    await TextMode.appendInput(driver, "include primitive-types\n");
+    await TextMode.appendInput(driver, "123");
+
+    await driver.sleep(3000);
+
+    let result = await RHS.searchFor(driver, "123", false);
+
+    expect(result).toBeTruthy();
+
+    await done();
+  });
+
+  test("Append Input 2", async function(done) {
+
+    await TextMode.toTextMode(driver);
+    await TextMode.appendInput(driver, "include primitive-types");
+    await TextMode.appendInput(driver, "\n\n\"abc\"");
+    await TextMode.appendInput(driver, "\n\n1234");
+
+    await driver.sleep(3000);
+
+    let result = await RHS.searchFor(driver, "1234", false);
+
+    expect(result).toBeTruthy();
 
     await done();
   });
