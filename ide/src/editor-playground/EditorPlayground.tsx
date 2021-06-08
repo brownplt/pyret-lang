8926b159cf90494ddd1f263086d90135dfccf199
cosine <trash@cosine.online>
2021-06-07 22:02:30 -0400

    Move Enter handling to onBeforeChange (about to undo)
    The trouble with this approach is you lose the power to do
    editor.replaceSelection and other nice little features - when it comes down to
    it, CodeMirror wasn't meant to be used as a controlled component, i see now why
    we previously used uncontrolled

--- ide/src/editor-playground/EditorPlayground.tsx
+++ ide/src/editor-playground/EditorPlayground.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 
@@ -7,16 +9,15 @@ export default function EditorPlayground() {
   const [value, setValue] = React.useState<string>('');
   const [chunks, setChunks] = React.useState<Set<number>>(new Set([0]));
   function onBeforeChange(editor: any, data: any, text: string) {
-    setValue(text);
-  }
-  function onKeyDown(editor: any, event: any) {
-    if (event.key === 'Enter') {
-      // from DefChunk.tsx: handleEnter
+    if (data.text.length === 2) {
+      // Remove old prompts
+      text = text.replace(/ ⏎ Press Enter to run/g, '');
       const pos = (editor as any).getCursor();
       const token = editor.getTokenAt(pos);
-      if ((event as any).shiftKey) {
-        // nothing?
-      } else if (token.state.lineState.tokens.length === 0) {
+      const line = editor.getLine(pos.line);
+      if (line.replace(/ /g, '') === '') {
+        // Double enter
+        console.log('RUNNING THE CHUNKS (theoretically)');
         // add chunk marker
         // In Uncontrolled (apparently), onKeyDown fires after onBeforeChange,
         // but on Controlled it fires before onBeforeChange. There are probably
@@ -25,9 +26,22 @@ export default function EditorPlayground() {
         // Does changing chunks before simply re-setting it violate react state
         // invariants? There's no functional union on Set...
         chunks.add(pos.line + 1);
-        setChunks(chunks);
+      // from DefChunk.tsx: handleEnter
+      } else if (token.state.lineState.tokens.length === 0) {
+        console.log('THE FINAL COUNTDOWN');
+        editor.replaceSelection(' ⏎ Press Enter to run', 'start');
+      } else {
+        console.log('continue');
+        // Easy way to drop text into this thing
+        // My design instinct is to show nothing here: in the happy case, the
+        // person is just writing a multiline expression and having a ball
+        //editor.replaceSelection(' ⏎ Press Enter to show errors in unclosed block');
       }
     }
+    setValue(text);
+    setChunks(chunks);
+  }
+  function onKeyDown(editor: any, event: any) {
   }
   console.log(chunks);
   return (
@@ -38,7 +52,7 @@ export default function EditorPlayground() {
         lineNumbers: true,
       }}
       onBeforeChange={onBeforeChange}
-      onKeyDown={onKeyDown}
+      onKeyUp={onKeyDown}
     />
   );
 }
