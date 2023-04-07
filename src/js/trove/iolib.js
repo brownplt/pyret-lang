6a033426dd40c757209074ad8f4862dba3158bdf
Robert Durst <me@robdurst.com>
2023-04-07 18:54:25 -0400

    abstract to other OS's

--- src/js/trove/iolib.js
+++ src/js/trove/iolib.js
@@ -9,22 +9,33 @@
     theModule: function(RUNTIME, NAMESPACE, uri, fs) {
       // https://github.com/nodejs/node/issues/28243#issuecomment-502402453
       function PromptIO(msg) {
-        // only works on linux
-        const stdin = fs.openSync("/dev/stdin","rs");
-  
         fs.writeSync(process.stdout.fd, msg);
         let s = '';
-        let buf = Buffer.alloc(1);
-        fs.readSync(stdin, buf, 0, 1, null);
-        while((buf[0] != 10) && (buf[0] != 13)) {
-          s += buf;
-          fs.readSync(stdin, buf, 0, 1, null);
+        while (true) {
+          let buf = Buffer.alloc(1);
+
+          // TODO: figure out why this needs to be within a try-catch
+          try {
+            // TODO: make sure this line works on non-unix OS
+            fs.readSync(process.stdin.fd, buf, 0, 1, null);
+          } catch (e) {
+            if (e.code !== "EAGAIN") {
+              throw e;
+            }
+          }
+
+          // TODO: elaborate on more exit conditions
+          if (buf.toString("utf-8") === "\n") {
+            break;
+          }
+
+          s += buf.toString("utf-8");
         }
-  
-        return s;
+        return s.trim();
       };
   
       var vals = {
+      // TODO: write a test like an actual software engineer
           "prompt": RUNTIME.makeFunction(function(input) {
             RUNTIME.ffi.checkArity(1, arguments, "prompt", false);
             RUNTIME.checkString(input);
