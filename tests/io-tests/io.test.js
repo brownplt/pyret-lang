613b9dd153a77900ac884b069c4e8459d7b71ce1
Joe Politz <joe.politz@gmail.com>
2025-03-20 15:31:22 -0700

    unconditionally unref for Travis

--- tests/io-tests/io.test.js
+++ tests/io-tests/io.test.js
@@ -72,6 +72,7 @@ describe("IO Tests", () => {
   afterAll(() => {
     const result = server.kill('SIGTERM');
     console.log("Kill result: ", result, server.killed);
+    server.unref();
     if(!result) {
       console.error("Server did not kill properly. Unref-ing that process so the process will exit.");
       server.unref();
